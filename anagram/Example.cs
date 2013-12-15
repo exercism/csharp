d0c130fb25ac2121193dbcaa0c60189d0bfd067c
Franklin Webber <franklin.webber@gmail.com>
2013-12-14 23:54:42 -0600

    Add anagram in C#

---
+++ anagram/Example.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Anagram
+{
+    private string baseWord;
+
+    public Anagram (string baseWord)
+    {
+        this.baseWord = baseWord;
+    }
+
+    public string[] Match (string[] potentialMatches)
+    {
+        List<string> matches = new List<string>();
+
+        foreach (string word in potentialMatches)
+        {
+            if (IsWordAnagramOfBaseWord(word))
+            {
+                matches.Add(word);
+            }
+        }
+
+        return matches.OrderBy(word => word).ToArray();
+    }
+
+    private bool IsWordAnagramOfBaseWord (string word)
+    {
+        return (IsNotTheSameWordAsBaseWord(word) && HasSameLettersAsBaseWord(word));
+    }
+
+    private bool IsNotTheSameWordAsBaseWord (string word)
+    {
+        return ! baseWord.Equals(word,StringComparison.OrdinalIgnoreCase);
+    }
+
+    private bool HasSameLettersAsBaseWord (string word)
+    {
+        return SortedCharArrayForString(baseWord).Equals(SortedCharArrayForString(word));
+    }
+
+    private string SortedCharArrayForString (string word)
+    {
+        return String.Concat(word.ToLower().OrderBy(letter => letter));
+    }
+}
