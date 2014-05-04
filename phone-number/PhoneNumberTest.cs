b7e7d83c92d9e8a2babb22a123acf58621d0eafd
Bressain Dinkelman <dinkelburt@gmail.com>
2014-05-03 20:53:21 -0600

    Add phone-number in C#

---
+++ phone-number/PhoneNumberTest.cs
@@ -0,0 +1,54 @@
+using NUnit.Framework;
+
+[TestFixture]
+public class PhoneNumberTest
+{
+    [Test]
+    public void CleansParensSpacesAndDashes()
+    {
+        var phone = new PhoneNumber("(123) 456-7890");
+        Assert.That(phone.Number, Is.EqualTo("1234567890"));
+    }
+
+    [Test]
+    public void CleansNumbersWithDots()
+    {
+        var phone = new PhoneNumber("123.456.7890");
+        Assert.That(phone.Number, Is.EqualTo("1234567890"));
+    }
+
+    [Test]
+    public void AllowsUsCountryCode()
+    {
+        var phone = new PhoneNumber("11234567890");
+        Assert.That(phone.Number, Is.EqualTo("1234567890"));
+    }
+
+    [Test]
+    public void InvalidWhen11Digits()
+    {
+        var phone = new PhoneNumber("21234567890");
+        Assert.That(phone.Number, Is.EqualTo("0000000000"));
+    }
+
+    [Test]
+    public void InvalidWhen9Digits()
+    {
+        var phone = new PhoneNumber("123456789");
+        Assert.That(phone.Number, Is.EqualTo("0000000000"));
+    }
+
+    [Test]
+    public void HasAnAreaCode()
+    {
+        var phone = new PhoneNumber("1234567890");
+        Assert.That(phone.AreaCode, Is.EqualTo("123"));
+    }
+
+    [Test]
+    public void FormatsANumber()
+    {
+        var phone = new PhoneNumber("1234567890");
+        Assert.That(phone.ToString(), Is.EqualTo("(123) 456-7890"));
+    }
+}
