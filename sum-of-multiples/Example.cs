689104f3db7f600885ef97368ede8f3eae2f755c
Jon <jwood803@users.noreply.github.com>
2014-05-06 17:48:26 -0400

    Added sum of multiples exercise

---
+++ sum-of-multiples/Example.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ExercismCSharp.sum_of_multiples
+{
+    public class SumOfMultiples
+    {
+        private List<int> multiples;
+
+        public SumOfMultiples()
+        {
+            multiples = new List<int> { 5, 3 };
+        }
+
+        public SumOfMultiples(IEnumerable<int> multiplesToCheck)
+        {
+            multiples = multiplesToCheck.ToList();
+        }
+
+        public int To(int limit)
+        {
+            int sum = 0;
+
+            for (int i = 1; i < limit; i++)
+            {
+                if (IsMultiple(i))
+                {
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
+
+        private bool IsMultiple(int input)
+        {
+            bool isMultiple = false;
+
+            if (multiples.Any())
+            {
+                multiples.ForEach(multiple =>
+                {
+                    if (input % multiple == 0)
+                    {
+                        isMultiple = true;
+                    }
+                });
+            }
+
+            return isMultiple;
+        }
+    }
+}
