77fb31ac7405574df97b787d6f0832c83d7d0f05
Jon <jwood803@users.noreply.github.com>
2014-05-07 10:15:20 -0400

    Added some tweaks

--- sum-of-multiples/SumOfMultiplesTest.cs
+++ sum-of-multiples/SumOfMultiplesTest.cs
@@ -1,64 +1,55 @@
 ﻿using NUnit.Framework;
 using System.Collections.Generic;
 
-namespace ExercismCSharp.sum_of_multiples
+[TestFixture]
+public class SumOfMultiplesTest
 {
-    [TestFixture]
-    public class SumOfMultiplesTest
+    private SumOfMultiples sumOfMultiples;
+
+    [SetUp]
+    public void FixtureSetup()
     {
-        private SumOfMultiples sumOfMultiples;
+        sumOfMultiples = new SumOfMultiples();
+    }
 
-        [TestFixtureSetUp]
-        public void FixtureSetup()
-        {
-            sumOfMultiples = new SumOfMultiples();
-        }
+    [Test]
+    public void Sum_To_1()
+    {
+        Assert.That(0, Is.EqualTo(sumOfMultiples.To(1)));
+    }
 
-        [TestFixtureTearDown]
-        public void FixtureTearDown()
-        {
-            sumOfMultiples = null;
-        }
+    [Test]
+    [Ignore]
+    public void Sum_To_3()
+    {
+        Assert.That(3, Is.EqualTo(sumOfMultiples.To(4)));
+    }
 
-        [Test]
-        public void TestSumTo1()
-        {
-            Assert.That(0, Is.EqualTo(sumOfMultiples.To(1)));
-        }
+    [Test]
+    [Ignore]
+    public void Sum_To_10()
+    {
+        Assert.That(23, Is.EqualTo(sumOfMultiples.To(10)));
+    }
 
-        [Test]
-        //[Ignore]
-        public void TestSumTo3()
-        {
-            Assert.That(3, Is.EqualTo(sumOfMultiples.To(4)));
-        }
+    [Test]
+    [Ignore]
+    public void Sum_To_1000()
+    {
+        Assert.That(233168, Is.EqualTo(sumOfMultiples.To(1000)));
+    }
 
-        [Test]
-        //[Ignore]
-        public void TestSumTo10()
-        {
-            Assert.That(23, Is.EqualTo(sumOfMultiples.To(10)));
-        }
+    [Test]
+    [Ignore]
+    public void Configurable_7_13_17_To_20()
+    {
+        Assert.That(51, Is.EqualTo(new SumOfMultiples(new List<int> { 7, 13, 17 }).To(20)));
+    }
 
-        [Test]
-        //[Ignore]
-        public void TestSumTo1000()
-        {
-            Assert.That(233168, Is.EqualTo(sumOfMultiples.To(1000)));
-        }
-
-        [Test]
-        //[Ignore]
-        public void TestConfigurable7_13_17To20()
-        {
-            Assert.That(51, Is.EqualTo(new SumOfMultiples(new List<int> { 7, 13, 17 }).To(20)));
-        }
-
-        [Test]
-        //[Ignore]
-        public void TestConfigurable43_47To10000()
-        {
-            Assert.That(2203160, Is.EqualTo(new SumOfMultiples(new List<int> { 43, 47 }).To(10000)));
-        }
+    [Test]
+    [Ignore]
+    public void Configurable_43_47_To_10000()
+    {
+        Assert.That(2203160, Is.EqualTo(new SumOfMultiples(new List<int> { 43, 47 }).To(10000)));
     }
 }
