45fb17ce54b3ee381ab9995c7f9a4310ef2f4b3a
Bressain Dinkelman <dinkelburt@gmail.com>
2014-05-24 15:27:42 -0600

    Added binary exercise

---
+++ binary/BinaryTest.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+
+[TestFixture]
+public class BinaryTest
+{
+    // change Ignore to false to run test case or just remove 'Ignore = true'
+    [TestCase("1", Result = 1)]
+    [TestCase("10", Result = 2, Ignore = true)]
+    [TestCase("11", Result = 3, Ignore = true)]
+    [TestCase("100", Result = 4, Ignore = true)]
+    [TestCase("1001", Result = 9, Ignore = true)]
+    [TestCase("11010", Result = 26, Ignore = true)]
+    [TestCase("10001101000", Result = 1128, Ignore = true)]
+    public int Binary_converts_to_decimal(string binary)
+    {
+        return new Binary(binary).ToDecimal();
+    }
+
+    [Test]
+    public void Invalid_binary_is_decimal_0()
+    {
+        Assert.That(new Binary("carrot123").ToDecimal(), Is.EqualTo(0));
+    }
+}
