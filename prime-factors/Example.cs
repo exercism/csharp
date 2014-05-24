97b13d497c40f653a1595613345b5c3c3da7bdc4
Bressain Dinkelman <dinkelburt@gmail.com>
2014-05-24 15:57:10 -0600

    Added prime-factors exercise

---
+++ prime-factors/Example.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+public class PrimeFactors
+{
+    public static int[] For(long number)
+    {
+        var primes = new List<int>();
+        int divisor = 2;
+        while (number > 1)
+        {
+            while (number % divisor == 0)
+            {
+                primes.Add(divisor);
+                number /= divisor;
+            }
+            divisor++;
+        }
+        return primes.ToArray();
+    }
+}
