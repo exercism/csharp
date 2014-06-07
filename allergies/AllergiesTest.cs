3b5550149e07bbf363a52b625fd213c3e22840b0
Bressain Dinkelman <dinkelburt@gmail.com>
2014-06-07 16:44:59 -0600

    Added allergies exercise

---
+++ allergies/AllergiesTest.cs
@@ -0,0 +1,111 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+
+[TestFixture]
+public class AllergiesTest
+{
+    [Test]
+    public void No_allergies_means_not_allergic()
+    {
+        var allergies = new Allergies(0);
+        Assert.That(allergies.AllergicTo("peanuts"), Is.False);
+        Assert.That(allergies.AllergicTo("cats"), Is.False);
+        Assert.That(allergies.AllergicTo("strawberries"), Is.False);
+    }
+
+    [Ignore]
+    [Test]
+    public void Allergic_to_eggs()
+    {
+        var allergies = new Allergies(1);
+        Assert.That(allergies.AllergicTo("eggs"), Is.True);
+    }
+
+    [Ignore]
+    [Test]
+    public void Allergic_to_eggs_in_addition_to_other_stuff()
+    {
+        var allergies = new Allergies(5);
+        Assert.That(allergies.AllergicTo("eggs"), Is.True);
+        Assert.That(allergies.AllergicTo("shellfish"), Is.True);
+        Assert.That(allergies.AllergicTo("strawberries"), Is.False);
+    }
+
+    [Ignore]
+    [Test]
+    public void No_allergies_at_all()
+    {
+        var allergies = new Allergies(0);
+        Assert.That(allergies.List(), Is.Empty);
+    }
+
+    [Ignore]
+    [Test]
+    public void Allergic_to_just_eggs()
+    {
+        var allergies = new Allergies(1);
+        Assert.That(allergies.List(), Is.EqualTo(new List<string> { "eggs" }));
+    }
+
+    [Ignore]
+    [Test]
+    public void Allergic_to_just_peanuts()
+    {
+        var allergies = new Allergies(2);
+        Assert.That(allergies.List(), Is.EqualTo(new List<string> { "peanuts" }));
+    }
+
+    [Ignore]
+    [Test]
+    public void Allergic_to_eggs_and_peanuts()
+    {
+        var allergies = new Allergies(3);
+        Assert.That(allergies.List(), Is.EqualTo(new List<string> { "eggs", "peanuts" }));
+    }
+
+    [Ignore]
+    [Test]
+    public void Allergic_to_lots_of_stuff()
+    {
+        var allergies = new Allergies(248);
+        Assert.That(allergies.List(),
+            Is.EqualTo(new List<string> { "strawberries", "tomatoes", "chocolate", "pollen", "cats" }));
+    }
+
+    [Ignore]
+    [Test]
+    public void Allergic_to_everything()
+    {
+        var allergies = new Allergies(255);
+        Assert.That(allergies.List(),
+            Is.EqualTo(new List<string>
+                {
+                    "eggs",
+                    "peanuts",
+                    "shellfish",
+                    "strawberries",
+                    "tomatoes",
+                    "chocolate",
+                    "pollen",
+                    "cats"
+                }));
+    }
+
+    [Ignore]
+    [Test]
+    public void Ignore_non_allergen_score_parts()
+    {
+        var allergies = new Allergies(509);
+        Assert.That(allergies.List(),
+            Is.EqualTo(new List<string>
+                {
+                    "eggs",
+                    "shellfish",
+                    "strawberries",
+                    "tomatoes",
+                    "chocolate",
+                    "pollen",
+                    "cats"
+                }));
+    }
+}
---
+++ allergies/Example.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
+
+public class Allergies
+{
+    private readonly int score;
+    private static readonly Dictionary<string, int> AvailableAllergies = new Dictionary<string, int>
+        {
+            { "eggs", 1 },
+            { "peanuts", 2 },
+            { "shellfish", 4 },
+            { "strawberries", 8 },
+            { "tomatoes", 16 },
+            { "chocolate", 32 },
+            { "pollen", 64 },
+            { "cats", 128 }
+        };
+
+    public Allergies(int score)
+    {
+        this.score = score;
+    }
+
+    public bool AllergicTo(string allergy)
+    {
+        return IsInAllergyScore(AvailableAllergies[allergy]);
+    }
+
+    public IList<string> List()
+    {
+        return AvailableAllergies.Where(x => IsInAllergyScore(x.Value)).Select(x => x.Key).ToList();
+    }
+
+    private bool IsInAllergyScore(int allergyValue)
+    {
+        return (score & allergyValue) == allergyValue;
+    }
+}
