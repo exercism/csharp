fa56b30f2894a695ad7f064b52a5995a328eedd0
Bressain Dinkelman <dinkelburt@gmail.com>
2014-06-07 15:13:15 -0600

    Added raindrops exercise

---
+++ raindrops/Example.cs
@@ -0,0 +1,16 @@
+public class Raindrops
+{
+    public static string Convert(int number)
+    {
+        string result = "";
+        if (number % 3 == 0)
+            result += "Pling";
+        if (number % 5 == 0)
+            result += "Plang";
+        if (number % 7 == 0)
+            result += "Plong";
+        if (string.IsNullOrEmpty(result))
+            result = number.ToString();
+        return result;
+    }
+}
