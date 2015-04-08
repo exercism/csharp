4b29d71571f38ea76b0ee838f56a149bc11a695e
Erik Schierboom <erik_schierboom@hotmail.com>
2015-04-08 13:27:24 +0200

    Use time comparison for gigasecond. Fixes #20

--- gigasecond/GigasecondTest.cs
+++ gigasecond/GigasecondTest.cs
@@ -7,23 +7,23 @@ public class GigasecondTest
     [Test]
     public void First_date()
     {
-        var gs = new Gigasecond(new DateTime(2011, 4, 25));
-        Assert.That(gs.Date(), Is.EqualTo(new DateTime(2043, 1, 1)));
+        var gs = new Gigasecond(new DateTime(2011, 4, 25, 0, 0, 0, DateTimeKind.Utc));
+        Assert.That(gs.Date(), Is.EqualTo(new DateTime(2043, 1, 1, 1, 46, 40, DateTimeKind.Utc)));
     }
 
-    [Ignore]
     [Test]
+    [Ignore]
     public void Another_date()
     {
-        var gs = new Gigasecond(new DateTime(1977, 6, 13));
-        Assert.That(gs.Date(), Is.EqualTo(new DateTime(2009, 2, 19)));
+        var gs = new Gigasecond(new DateTime(1977, 6, 13, 0, 0, 0, DateTimeKind.Utc));
+        Assert.That(gs.Date(), Is.EqualTo(new DateTime(2009, 2, 19, 1, 46, 40, DateTimeKind.Utc)));
     }
 
-    [Ignore]
     [Test]
+    [Ignore]
     public void Yet_another_date()
     {
-        var gs = new Gigasecond(new DateTime(1959, 7, 19));
-        Assert.That(gs.Date(), Is.EqualTo(new DateTime(1991, 3, 27)));
+        var gs = new Gigasecond(new DateTime(1959, 7, 19, 0, 0, 0, DateTimeKind.Utc));
+        Assert.That(gs.Date(), Is.EqualTo(new DateTime(1991, 3, 27, 1, 46, 40, DateTimeKind.Utc)));
     }
 }
