ce135955ab4f1de1e8f6d93d6058993b68ee920f
Erik Schierboom <erik_schierboom@hotmail.com>
2016-02-24 13:08:10 +0100

    Add queen-attack exercise

---
+++ exercises/queen-attack/Example.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Queen
+{
+    public Queen(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
+
+    public int Row { get; }
+    public int Column { get; }
+}
+
+public class Queens
+{
+    private readonly Queen _white;
+    private readonly Queen _black;
+
+    public Queens(Queen white, Queen black)
+    {
+        if (white.Row == black.Row && white.Column == black.Column)
+        {
+            throw new ArgumentException("The queens cannot be positioned at the same place.");
+        }
+
+        _black = black;
+        _white = white;
+    }
+
+    public bool CanAttack()
+    {
+        return _black.Row == _white.Row ||
+               _black.Column == _white.Column ||
+               Math.Abs(_black.Row - _white.Row) == Math.Abs(_black.Column - _white.Column);
+    }
+}
---
+++ exercises/queen-attack/QueenAttackTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using NUnit.Framework;
+
+public class QueenAttackTest
+{
+    [Test]
+    public void Cannot_occupy_same_space()
+    {
+        var white = new Queen(2, 4);
+        var black = new Queen(2, 4);
+        Assert.Throws<ArgumentException>(() => new Queens(white, black));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Cannot_attack()
+    {
+        var queens = new Queens(new Queen(2, 3), new Queen(4, 7));
+        Assert.False(queens.CanAttack());
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Can_attack_on_same_row()
+    {
+        var queens = new Queens(new Queen(2, 4), new Queen(2, 7));
+        Assert.True(queens.CanAttack());
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Can_attack_on_same_column()
+    {
+        var queens = new Queens(new Queen(5, 4), new Queen(2, 4));
+        Assert.True(queens.CanAttack());
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Can_attack_on_diagonal()
+    {
+        var queens = new Queens(new Queen(1, 1), new Queen(6, 6));
+        Assert.True(queens.CanAttack());
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Can_attack_on_other_diagonal()
+    {
+        var queens = new Queens(new Queen(0, 6), new Queen(1, 7));
+        Assert.True(queens.CanAttack());
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Can_attack_on_yet_another_diagonal()
+    {
+        var queens = new Queens(new Queen(4, 1), new Queen(6, 3));
+        Assert.True(queens.CanAttack());
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Can_attack_on_a_diagonal_slanted_the_other_way()
+    {
+        var queens = new Queens(new Queen(6, 1), new Queen(1, 6));
+        Assert.True(queens.CanAttack());
+    }
+}
