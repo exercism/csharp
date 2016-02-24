548d87e1feadc540a1b8ab920bdc8827f05ff39b
Erik Schierboom <erik_schierboom@hotmail.com>
2016-02-24 09:02:04 +0100

    Add matrix exercise

---
+++ exercises/matrix/Example.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+
+public class Matrix
+{
+    private readonly int[][] _rows;
+    private readonly int[][] _cols;
+
+    public Matrix(string input)
+    {
+        _rows = ParseRows(input);
+        _cols = ParseCols(_rows);
+    }
+
+    public int Rows => _rows.Length;
+    public int Cols => _cols.Length;
+
+    public int[] Row(int row) => _rows[row];
+    public int[] Col(int col) => _cols[col];
+
+    private static int[][] ParseRows(string input)
+    {
+        return input.Split('\n')
+            .Select(ParseRow)
+            .ToArray();
+    }
+
+    private static int[] ParseRow(string row)
+    {
+        return row.Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+    }
+
+    private static int[][] ParseCols(int[][] rows)
+    {
+        return Enumerable.Range(0, rows[0].Length)
+            .Select(y => ParseCol(rows, y))
+            .ToArray();
+    }
+
+    private static int[] ParseCol(int[][] rows, int y)
+    {
+        return Enumerable.Range(0, rows.Length)
+            .Select(x => rows[x][y])
+            .ToArray();
+    }
+}
