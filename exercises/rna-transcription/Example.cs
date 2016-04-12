670feffdd155201e916142aa88b94cbb422f4892
Erik Schierboom <erik_schierboom@hotmail.com>
2016-04-12 15:17:26 +0200

    Fix rna-transcription exercise. Closes #72

--- exercises/rna-transcription/Example.cs
+++ exercises/rna-transcription/Example.cs
@@ -7,18 +7,9 @@ public class Complement
         {
             { 'G', 'C' }, { 'C', 'G' }, { 'T', 'A' }, { 'A', 'U' }
         };
-    private static readonly Dictionary<char, char> RnaToDna = new Dictionary<char, char>
-        {
-            { 'C', 'G' }, { 'G', 'C' }, { 'A', 'T' }, { 'U', 'A' }
-        };
 
     public static string OfDna(string nucleotide)
     {
         return string.Concat(nucleotide.Select(x => DnaToRna[x]));
-    }
-
-    public static string OfRna(string nucleotide)
-    {
-        return string.Concat(nucleotide.Select(x => RnaToDna[x]));
     }
 }
