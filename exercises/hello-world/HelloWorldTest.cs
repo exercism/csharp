96d3eb3d8be060e639e6d023eee6c406acc0d27d
Rob Keim <robkeim@gmail.com>
2016-08-31 00:59:51 +0200

    Add HelloWorld solution and tests

---
+++ exercises/hello-world/HelloWorldTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class HelloWorldTest
+{
+    [Test]
+    public void No_name()
+    {
+        Assert.That(HelloWorld.Hello(null), Is.EqualTo("Hello, World!"));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Sample_name()
+    {
+        Assert.That(HelloWorld.Hello("Alice"), Is.EqualTo("Hello, Alice!"));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Other_sample_name()
+    {
+        Assert.That(HelloWorld.Hello("Bob"), Is.EqualTo("Hello, Bob!"));
+    }
+}
