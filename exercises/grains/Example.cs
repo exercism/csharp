54cc69eaa8afff0d29ec8052346461679e73c60c
Rob Keim <robkeim@gmail.com>
2016-08-31 09:08:46 +0200

    Add Grains solution

---
+++ exercises/grains/Example.cs
@@ -0,0 +1,21 @@
+﻿public class Grains
+{
+    public static ulong Square(int n)
+    {
+        return n == 1
+            ? 1
+            : 2 * Square(n - 1);
+    }
+
+    public static ulong Total()
+    {
+        ulong total = 0;
+
+        for (int i = 1; i <= 64; i++)
+        {
+            total += Square(i);
+        }
+
+        return total;
+    }
+}
