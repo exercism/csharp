cb5757380fecd8ce304ec2d797673d8c8b819f6c
Rob Keim <robkeim@gmail.com>
2016-09-02 10:51:42 +0200

    Add Minesweeper exercise (#95)
    * Add Minesweeper exercise

---
+++ exercises/minesweeper/Example.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Minesweeper
+{
+    public static string Annotate(string input)
+    {
+        var results = new List<string>();
+
+        var board = input.Split("\n".ToCharArray());
+
+        for (int i = 0; i < board.Length; i++)
+        {
+            var result = string.Empty;
+
+            for (int j = 0; j < board[0].Length; j++)
+            {
+                if (board[i][j] == '*')
+                {
+                    result += '*';
+                }
+                else
+                {
+                    var numMines = GetCountForSquare(board, i, j);
+                    result += numMines == 0
+                        ? " "
+                        : numMines.ToString();
+                }
+            }
+
+            results.Add(result);
+        }
+
+        return string.Join("\n", results);
+    }
+
+    private static int GetCountForSquare(string[] board, int x, int y)
+    {
+        var result = 0;
+
+        for (int i = -1; i <= 1; i++)
+        {
+            for (int j = -1; j <= 1; j++)
+            {
+                try
+                {
+                    if (board[x + i][y + j] == '*')
+                    {
+                        result++;
+                    }
+                }
+                catch (IndexOutOfRangeException)
+                {
+                }
+            }
+        }
+
+        return result;
+    }
+}
