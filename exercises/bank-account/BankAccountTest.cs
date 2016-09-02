0f20c01019f465db7b1c3c3249f5bec6c79e639f
Rob Keim <robkeim@gmail.com>
2016-09-02 09:51:45 +0200

    Add Bank Account exercise

---
+++ exercises/bank-account/BankAccountTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using NUnit.Framework;
+
+public class BankAccountTest
+{
+    [Test]
+    public void Returns_empty_balance_after_opening()
+    {
+        var account = new BankAccount();
+        account.Open();
+
+        Assert.That(account.GetBalance(), Is.EqualTo(0));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Check_basic_balance()
+    {
+        var account = new BankAccount();
+        account.Open();
+
+        var openingBalance = account.GetBalance();
+
+        account.UpdateBalance(10);
+        var updatedBalance = account.GetBalance();
+
+        Assert.That(openingBalance, Is.EqualTo(0));
+        Assert.That(updatedBalance, Is.EqualTo(10));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Balance_can_increment_and_decrement()
+    {
+        var account = new BankAccount();
+        account.Open();
+        var openingBalance = account.GetBalance();
+
+        account.UpdateBalance(10);
+        var addedBalance = account.GetBalance();
+
+        account.UpdateBalance(-15);
+        var subtractedBalance = account.GetBalance();
+
+        Assert.That(openingBalance, Is.EqualTo(0));
+        Assert.That(addedBalance, Is.EqualTo(10));
+        Assert.That(subtractedBalance, Is.EqualTo(-5));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Closed_account_throws_exception_when_checking_balance()
+    {
+        var account = new BankAccount();
+        account.Open();
+        account.Close();
+
+        Assert.Throws<InvalidOperationException>(() => account.GetBalance());
+    }
+}
---
+++ exercises/bank-account/Example.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class BankAccount
+{
+    private float balance;
+    private bool isOpen;
+
+    public void Open()
+    {
+        isOpen = true;
+    }
+
+    public void Close()
+    {
+        isOpen = false;
+    }
+
+    public float GetBalance()
+    {
+        if (!isOpen)
+        {
+            throw new InvalidOperationException("Cannot get balance on an account that isn't open");
+        }
+
+        return balance;
+    }
+
+    public void UpdateBalance(float change)
+    {
+        if (!isOpen)
+        {
+            throw new InvalidOperationException("Cannot update balance on an account that isn't open");
+        }
+
+        balance += change;
+    }
+}
