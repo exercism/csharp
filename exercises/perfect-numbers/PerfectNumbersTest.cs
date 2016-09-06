28fbf69de0003be3df90baa6c25aec6c2314d8ad
Rob Keim <robkeim@gmail.com>
2016-09-07 00:08:15 +0200

    Add perfect numbers solution

---
+++ exercises/perfect-numbers/PerfectNumbersTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class PerfectNumbersTest
+{
+    [TestCase(3)]
+    [TestCase(7, Ignore = "Remove to run test case")]
+    [TestCase(13, Ignore = "Remove to run test case")]
+    public void Can_classify_deficient_numbers(int number)
+    {
+        Assert.That(PerfectNumbers.Classify(number), Is.EqualTo(NumberType.Deficient));
+    }
+    
+    [TestCase(6, Ignore = "Remove to run test case")]
+    [TestCase(28, Ignore = "Remove to run test case")]
+    [TestCase(496, Ignore = "Remove to run test case")]
+    public void Can_classify_perfect_numbers(int number)
+    {
+        Assert.That(PerfectNumbers.Classify(number), Is.EqualTo(NumberType.Perfect));
+    }
+    
+    [TestCase(12, Ignore = "Remove to run test case")]
+    [TestCase(18, Ignore = "Remove to run test case")]
+    [TestCase(20, Ignore = "Remove to run test case")]
+    public void Can_classify_abundant_numbers(int number)
+    {
+        Assert.That(PerfectNumbers.Classify(number), Is.EqualTo(NumberType.Abundant));
+    }
+}
