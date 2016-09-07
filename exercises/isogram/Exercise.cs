cd41ad59cc31e4a692bf21a4477ee0597c9c5ff3
Rob Keim <robkeim@gmail.com>
2016-09-07 08:27:54 +0200

    Add Isogram solution (#102)

---
+++ exercises/isogram/Exercise.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        var letters = new HashSet<char>();
+
+        foreach (var c in word.ToLowerInvariant())
+        {
+            if (Char.IsLetter(c) && !letters.Add(c))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ exercises/isogram/IsogramTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class IsogramTest
+{
+    [TestCase("duplicates", ExpectedResult = true)]
+    [TestCase("eleven", ExpectedResult = false, Ignore = "Remove to run test case")]
+    [TestCase("subdermatoglyphic", ExpectedResult = true, Ignore = "Remove to run test case")]
+    [TestCase("Alphabet", ExpectedResult = false, Ignore = "Remove to run test case")]
+    [TestCase("thumbscrew-japingly", ExpectedResult = true, Ignore = "Remove to run test case")]
+    [TestCase("Hjelmqvist-Gryb-Zock-Pfund-Wax", ExpectedResult = true, Ignore = "Remove to run test case")]
+    [TestCase("Heizölrückstoßabdämpfung", ExpectedResult = true, Ignore = "Remove to run test case")]
+    [TestCase("the quick brown fox", ExpectedResult = false, Ignore = "Remove to run test case")]
+    [TestCase("Emily Jung Schwartzkopf", ExpectedResult = true, Ignore = "Remove to run test case")]
+    [TestCase("éléphant", ExpectedResult = false, Ignore = "Remove to run test case")]
+    public bool Isogram_correctly_detects_isograms(string input)
+    {
+        return Isogram.IsIsogram(input);
+    }
+}
