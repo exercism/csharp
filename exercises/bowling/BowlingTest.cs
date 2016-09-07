a7184e6ec71b063f401aaa91b2f74855ef36c7c5
Erik Schierboom <erik_schierboom@hotmail.com>
2016-09-07 09:52:48 +0200

    Add bowling exercise (#103)

---
+++ exercises/bowling/BowlingTest.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+
+public class BowlingTest
+{
+    [Test]
+    public void Gutter_game()
+    {
+        var game = new BowlingGame();
+        RollMany(0, 20, game);
+        Assert.That(game.Score(), Is.EqualTo(0));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void All_ones_game()
+    {
+        var game = new BowlingGame();
+        RollMany(1, 20, game);
+        Assert.That(game.Score(), Is.EqualTo(20));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void One_spare_game()
+    {
+        var game = new BowlingGame();
+        RollSpare(game);
+        game.Roll(3);
+        RollMany(0, 17, game);
+
+        Assert.That(game.Score(), Is.EqualTo(16));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void One_strike_game()
+    {
+        var game = new BowlingGame();
+        RollStrike(game);
+        game.Roll(3);
+        game.Roll(4);
+        RollMany(0, 16, game);
+
+        Assert.That(game.Score(), Is.EqualTo(24));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Perfect_game()
+    {
+        var game = new BowlingGame();
+        RollMany(10, 12, game);
+
+        Assert.That(game.Score(), Is.EqualTo(300));
+    }
+
+    private static void RollMany(int pins, int count, BowlingGame game)
+    {
+        for (var i = 0; i < count; i++)
+        {
+            game.Roll(pins);
+        }
+    }
+
+    private static void RollSpare(BowlingGame game)
+    {
+        game.Roll(5);
+        game.Roll(5);
+    }
+
+    private static void RollStrike(BowlingGame game)
+    {
+        game.Roll(10);
+    }
+}
---
+++ exercises/bowling/Example.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+public class BowlingGame
+{
+    private const int NumberOfFrames = 10;
+    private const int MaximumFrameScore = 10;
+
+    private readonly List<int> rolls = new List<int>();
+
+    public void Roll(int pins) => rolls.Add(pins);
+
+    public int Score()
+    {
+        var score = 0;
+        var frameIndex = 0;
+
+        for (int i = 0; i < NumberOfFrames; i++)
+        {
+            if (IsStrike(frameIndex))
+            {
+                score += 10 + StrikeBonus(frameIndex);
+                frameIndex += 1;
+            }
+            else if (IsSpare(frameIndex))
+            {
+                score += 10 + SpareBonus(frameIndex);
+                frameIndex += 2;
+            }
+            else
+            {
+                score += SumOfPinsInFrame(frameIndex);
+                frameIndex += 2;
+            }
+        }
+
+        return score;
+    }
+
+    private bool IsStrike(int frameIndex) => rolls[frameIndex] == MaximumFrameScore;
+    private bool IsSpare(int frameIndex) => rolls[frameIndex] + rolls[frameIndex + 1] == MaximumFrameScore;
+
+    private int StrikeBonus(int frameIndex) => rolls[frameIndex + 1] + rolls[frameIndex + 2];
+    private int SpareBonus(int frameIndex) => rolls[frameIndex + 2];
+
+    private int SumOfPinsInFrame(int frameIndex) => rolls[frameIndex] + rolls[frameIndex + 1];
+}
