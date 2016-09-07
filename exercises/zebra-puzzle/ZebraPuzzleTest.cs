8ef928ddf28a910dd714046719cdd5dfa0f704a3
Erik Schierboom <erik_schierboom@hotmail.com>
2016-09-07 11:40:24 +0200

    Add zebra-puzzle exercise (#107)

---
+++ exercises/zebra-puzzle/ZebraPuzzleTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+public class ZebraPuzzleTest
+{
+    [Test]
+    public void Who_drinks_water()
+    {
+        Assert.That(ZebraPuzzle.WhoDrinks(Drink.Water), Is.EqualTo(Nationality.Norwegian));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Who_owns_the_zebra()
+    {
+        Assert.That(ZebraPuzzle.WhoOwns(Pet.Zebra), Is.EqualTo(Nationality.Japanese));
+    }
+}
