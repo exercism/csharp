dbeb6078bece5639099d054462b25afd59c2df20
Rob Keim <robkeim@gmail.com>
2016-09-07 08:26:54 +0200

    Add perfect numbers solution (#101)

---
+++ exercises/perfect-numbers/Example.cs
@@ -0,0 +1,35 @@
+﻿public enum NumberType
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public class PerfectNumbers
+{
+    public static NumberType Classify(int number)
+    {
+        int sumOfFactors = 0;
+
+        for (int i = 1; i < number; i++)
+        {
+            if (number % i == 0)
+            {
+                sumOfFactors += i;
+            }
+        }
+
+        if (sumOfFactors < number)
+        {
+            return NumberType.Deficient;
+        }
+        else if (sumOfFactors == number)
+        {
+            return NumberType.Perfect;
+        }
+        else
+        {
+            return NumberType.Abundant;
+        }
+    }
+}
