eff6c018ed2fb7496ff5aa79a299e7e660e73bfe
Erik Schierboom <erik_schierboom@hotmail.com>
2016-09-12 16:01:35 +0200

    Add dominoes exercise (#113)

---
+++ exercises/dominoes/DominoesTest.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using NUnit.Framework;
+
+public class DominoesTest
+{
+    [Test]
+    public void Empty_input_equals_empty_output()
+    {
+        var actual = new Tuple<int, int>[0];
+        Assert.That(Dominoes.CanChain(actual), Is.True);
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Singleton_input_equals_singleton_output()
+    {
+        var actual = new[] { Tuple.Create(1, 1) };
+        Assert.That(Dominoes.CanChain(actual), Is.True);
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Singleton_that_cant_be_chained()
+    {
+        var actual = new[] { Tuple.Create(1, 2) };
+        Assert.That(Dominoes.CanChain(actual), Is.False);
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Three_elements()
+    {
+        var actual = new[] { Tuple.Create(1, 2), Tuple.Create(3, 1), Tuple.Create(2, 3) };
+        Assert.That(Dominoes.CanChain(actual), Is.True);
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Can_reverse_dominoes()
+    {
+        var actual = new[] { Tuple.Create(1, 2), Tuple.Create(1, 3), Tuple.Create(2, 3) };
+        Assert.That(Dominoes.CanChain(actual), Is.True);
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Cant_be_chained()
+    {
+        var actual = new[] { Tuple.Create(1, 2), Tuple.Create(4, 1), Tuple.Create(2, 3) };
+        Assert.That(Dominoes.CanChain(actual), Is.False);
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Disconnected_simple()
+    {
+        var actual = new[] { Tuple.Create(1, 1), Tuple.Create(2, 2) };
+        Assert.That(Dominoes.CanChain(actual), Is.False);
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Disconnected_double_loop()
+    {
+        var actual = new[] { Tuple.Create(1, 2), Tuple.Create(2, 1), Tuple.Create(3, 4), Tuple.Create(4, 3) };
+        Assert.That(Dominoes.CanChain(actual), Is.False);
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Disconnected_single_isolated()
+    {
+        var actual = new[] { Tuple.Create(1, 2), Tuple.Create(2, 3), Tuple.Create(3, 1), Tuple.Create(4, 4) };
+        Assert.That(Dominoes.CanChain(actual), Is.False);
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Need_backtrack()
+    {
+        var actual = new[] { Tuple.Create(1, 2), Tuple.Create(2, 3), Tuple.Create(3, 1), Tuple.Create(2, 4), Tuple.Create(2, 4) };
+        Assert.That(Dominoes.CanChain(actual), Is.True);
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Separate_loops()
+    {
+        var actual = new[]
+        {
+            Tuple.Create(1, 2), Tuple.Create(2, 3), Tuple.Create(3, 1), Tuple.Create(1, 1), Tuple.Create(2, 2), Tuple.Create(3, 3)
+        };
+        Assert.That(Dominoes.CanChain(actual), Is.True);
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Ten_elements()
+    {
+        var actual = new[]
+        {
+            Tuple.Create(1, 2), Tuple.Create(5, 3), Tuple.Create(3, 1), Tuple.Create(1, 2), Tuple.Create(2, 4),
+            Tuple.Create(1, 6), Tuple.Create(2, 3), Tuple.Create(3, 4), Tuple.Create(5, 6)
+        };
+        Assert.That(Dominoes.CanChain(actual), Is.True);
+    }
+}
---
+++ exercises/dominoes/Example.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Dominoes
+{
+    public static bool CanChain(IEnumerable<Tuple<int, int>> dominoes)
+    {
+        if (!dominoes.Any())
+        {
+            return true;
+        }
+
+        var domino = dominoes.First();
+
+        if (dominoes.Count() == 1)
+        {
+            return domino.Item1 == domino.Item2;
+        }
+
+        return dominoes.Skip(1).Rotate().Any(sublist => PossibleChains(domino, sublist).Any(CanChain));
+    }
+
+    public static IEnumerable<Tuple<int, int>[]> PossibleChains(Tuple<int, int> domino, IEnumerable<Tuple<int, int>> remainder)
+    {
+        var head = remainder.First();
+
+        if (domino.Item2 == head.Item1)
+        {
+            yield return new[] { Tuple.Create(domino.Item1, head.Item2) }.Concat(remainder.Skip(1)).ToArray();
+        }
+        else if (domino.Item2 == head.Item2)
+        {
+            yield return new[] { Tuple.Create(domino.Item1, head.Item1) }.Concat(remainder.Skip(1)).ToArray();
+        }
+    }
+
+    private static IEnumerable<IEnumerable<T>> Rotate<T>(this IEnumerable<T> input)
+    {
+        return Enumerable.Range(0, input.Count()).Select(i => input.Skip(i).Take(input.Count() - i).Concat(input.Take(i)));
+    }
+}
