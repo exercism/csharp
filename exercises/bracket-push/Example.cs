18a1dfbfcf457b3d5f910b79e1c6258a9a2216c1
Erik Schierboom <erik_schierboom@hotmail.com>
2016-09-12 09:26:50 +0200

    Add bracket-push exercise

---
+++ exercises/bracket-push/Example.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+public static class BracketPush
+{
+    public static bool Matched(string input)
+    {
+        var brackets = new string(input.Where(c => "[]{}()".Contains(c)).ToArray());
+        var previousLength = brackets.Length;
+
+        while (brackets.Length > 0)
+        {
+            brackets = brackets.Replace("[]", "").Replace("{}", "").Replace("()", "");
+
+            if (brackets.Length == previousLength)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
