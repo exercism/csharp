23d0d488df1eeaa6e98399a3d919d4bc48dfc59c
Erik Schierboom <erik_schierboom@hotmail.com>
2016-09-12 18:43:29 +0200

    Add diamond exercise (#115)

---
+++ exercises/diamond/DiamondTest.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+public class DiamondTest
+{
+    private static readonly char[] Letters = GetLetterRange('A', 'Z');
+    private static char[] GetLetterRange(char min, char max) => Enumerable.Range(min, max - min + 1).Select(i => (char) i).ToArray();
+    private static string[] Rows(string x) => x.Split(new[] { '\n' }, StringSplitOptions.None);
+
+    private static string LeadingSpaces(string x) => x.Substring(0, x.IndexOfAny(Letters));
+    private static string TrailingSpaces(string x) => x.Substring(x.LastIndexOfAny(Letters) + 1);
+
+    [TestCaseSource(nameof(Letters))]
+    public void First_row_contains_A(char letter)
+    {
+        var actual = Diamond.Make(letter);
+        var rows = Rows(actual);
+        var firstRowCharacters = rows.First().Trim();
+
+        Assert.That(firstRowCharacters, Is.EqualTo("A"));
+    }
+
+    [Ignore("Remove to run test")]
+    [TestCaseSource(nameof(Letters))]
+    public void All_rows_must_have_symmetric_contour(char letter)
+    {
+        var actual = Diamond.Make(letter);
+        var rows = Rows(actual);
+
+        Assert.That(rows, Is.All.Matches<string>(row => LeadingSpaces(row) == TrailingSpaces(row)));
+    }
+
+    [Ignore("Remove to run test")]
+    [TestCaseSource(nameof(Letters))]
+    public void Top_of_figure_has_letters_in_correct_order(char letter)
+    {
+        var actual = Diamond.Make(letter);
+        var rows = Rows(actual);
+
+        var expected = GetLetterRange('A', letter);
+        var firstNonSpaceLetters = rows.Take(expected.Length).Select(row => row.Trim()[0]);
+        Assert.That(expected, Is.EqualTo(firstNonSpaceLetters));
+    }
+
+    [Ignore("Remove to run test")]
+    [TestCaseSource(nameof(Letters))]
+    public void Figure_is_symmetric_around_the_horizontal_axis(char letter)
+    {
+        var actual = Diamond.Make(letter);
+
+        var rows = Rows(actual);
+        var top = rows.TakeWhile(row => !row.Contains(letter));
+        var bottom = rows.Reverse().TakeWhile(row => !row.Contains(letter));
+
+        Assert.That(top, Is.EqualTo(bottom));
+    }
+
+    [Ignore("Remove to run test")]
+    [TestCaseSource(nameof(Letters))]
+    public void Diamond_has_square_shape(char letter)
+    {
+        var actual = Diamond.Make(letter);
+
+        var rows = Rows(actual);
+        var expected = rows.Length;
+
+        Assert.That(rows, Is.All.Matches<string>(row => row.Length == expected));
+    }
+
+    [Ignore("Remove to run test")]
+    [TestCaseSource(nameof(Letters))]
+    public void All_rows_except_top_and_bottom_have_two_identical_letters(char letter)
+    {
+        var actual = Diamond.Make(letter);
+
+        var rows = Rows(actual).Where(row => !row.Contains('A'));
+
+        Assert.That(rows, Is.All.Matches<string>(row =>
+        {
+            var twoCharacters = row.Replace(" ", "").Length == 2;
+            var identicalCharacters = row.Replace(" ", "").Distinct().Count() == 1;
+            return twoCharacters && identicalCharacters;
+        }));
+    }
+
+    [Ignore("Remove to run test")]
+    [TestCaseSource(nameof(Letters))]
+    public void Bottom_left_corner_spaces_are_triangle(char letter)
+    {
+        var actual = Diamond.Make(letter);
+
+        var rows = Rows(actual);
+
+        var cornerSpaces = rows.Reverse().SkipWhile(row => !row.Contains(letter)).Select(LeadingSpaces);
+        var spaceCounts = cornerSpaces.Select(row => row.Length).ToList();
+        var expected = Enumerable.Range(0, spaceCounts.Count).Select(i => i).ToList();
+
+        Assert.That(spaceCounts, Is.EqualTo(expected));
+    }
+}
---
+++ exercises/diamond/Example.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+public static class Diamond
+{
+    public static string Make(char target)
+    {
+        var letters = GetLetters(target);
+        var diamondLetters = letters.Concat(letters.Reverse().Skip(1)).ToArray();
+
+        return string.Join("\n", diamondLetters.Select(diamondLetter => MakeLine(letters.Length, diamondLetter)));
+    }
+
+    private static Tuple<char, int>[] GetLetters(char target) =>
+        Enumerable
+            .Range('A', target - 'A' + 1)
+            .Select((c, i) => Tuple.Create((char)c, i))
+            .ToArray();
+
+    private static string MakeLine(int letterCount, Tuple<char, int> rowLetter)
+    {
+        var letter = rowLetter.Item1;
+        var row = rowLetter.Item2;
+        var outerSpaces = "".PadRight(letterCount - row - 1);
+        var innerSpaces = "".PadRight(row == 0 ? 0 : row*2 - 1);
+
+        return letter == 'A' ? $"{outerSpaces}{letter}{outerSpaces}" : $"{outerSpaces}{letter}{innerSpaces}{letter}{outerSpaces}";
+    }
+}
