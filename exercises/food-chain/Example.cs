24a2f7be46ae5b8f7ab22f30fda225661ccf0b31
Erik Schierboom <erik_schierboom@hotmail.com>
2016-09-13 09:06:55 +0200

    Add food-chain exercise (#117)

---
+++ exercises/food-chain/Example.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq;
+
+public static class FoodChain
+{
+    private const int Verses = 8;
+
+    private static readonly string[] Subjects =
+    {
+        "spider",
+        "bird",
+        "cat",
+        "dog",
+        "goat",
+        "cow"
+    };
+
+    private static readonly string[] FollowUps =
+    {
+        "It wriggled and jiggled and tickled inside her.",
+        "How absurd to swallow a bird!",
+        "Imagine that, to swallow a cat!",
+        "What a hog, to swallow a dog!",
+        "Just opened her throat and swallowed a goat!",
+        "I don't know how she swallowed a cow!"
+    };
+
+    private static readonly string[] History =
+    {
+        "I don't know how she swallowed a cow!",
+         "She swallowed the cow to catch the goat.",
+         "She swallowed the goat to catch the dog.",
+         "She swallowed the dog to catch the cat.",
+         "She swallowed the cat to catch the bird.",
+         "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.",
+         "She swallowed the spider to catch the fly.",
+         "I don't know why she swallowed the fly. Perhaps she'll die."
+    };
+
+    public static string Song() => string.Join("\n\n", Enumerable.Range(1, Verses).Select(Verse));
+
+    public static string Verse(int number) => $"{VerseBegin(number)}\n{VerseEnd(number)}";
+
+    private static string VerseBegin(int number)
+    {
+        if (number == 1)
+        {
+            return "I know an old lady who swallowed a fly.";
+        }
+
+        if (number == 8)
+        {
+            return "I know an old lady who swallowed a horse.";
+        }
+
+        var subject = Subjects[number - 2];
+        var followUp = FollowUps[number - 2];
+        return $"I know an old lady who swallowed a {subject}.\n{followUp}";
+    }
+
+    private static string VerseEnd(int number)
+    {
+        if (number == 8)
+        {
+            return "She's dead, of course!";
+        }
+
+        return string.Join("\n", History.Skip(History.Length - number).Take(number));
+    }
+}
---
+++ exercises/food-chain/FoodChainTest.cs
@@ -0,0 +1,107 @@
+﻿using NUnit.Framework;
+
+public class FoodChainTest
+{
+    [Test]
+    public void Verse_one()
+    {
+        const string expected = "I know an old lady who swallowed a fly.\n" +
+                                "I don't know why she swallowed the fly. Perhaps she'll die.";
+
+        Assert.That(FoodChain.Verse(1), Is.EqualTo(expected));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Verse_two()
+    {
+        const string expected = "I know an old lady who swallowed a spider.\n" +
+                                "It wriggled and jiggled and tickled inside her.\n" +
+                                "She swallowed the spider to catch the fly.\n" +
+                                "I don't know why she swallowed the fly. Perhaps she'll die.";
+
+        Assert.That(FoodChain.Verse(2), Is.EqualTo(expected));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Verse_four()
+    {
+        const string expected = "I know an old lady who swallowed a cat.\n" +
+                                "Imagine that, to swallow a cat!\n" +
+                                "She swallowed the cat to catch the bird.\n" +
+                                "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" +
+                                "She swallowed the spider to catch the fly.\n" +
+                                "I don't know why she swallowed the fly. Perhaps she'll die.";
+
+        Assert.That(FoodChain.Verse(4), Is.EqualTo(expected));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Verse_eight()
+    {
+        const string expected = "I know an old lady who swallowed a horse.\n" +
+                                "She's dead, of course!";
+
+        Assert.That(FoodChain.Verse(8), Is.EqualTo(expected));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Complete_song()
+    {
+        const string expected = "I know an old lady who swallowed a fly.\n" +
+                                "I don't know why she swallowed the fly. Perhaps she'll die.\n" +
+                                "\n" +
+                                "I know an old lady who swallowed a spider.\n" +
+                                "It wriggled and jiggled and tickled inside her.\n" +
+                                "She swallowed the spider to catch the fly.\n" +
+                                "I don't know why she swallowed the fly. Perhaps she'll die.\n" +
+                                "\n" +
+                                "I know an old lady who swallowed a bird.\n" +
+                                "How absurd to swallow a bird!\n" +
+                                "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" +
+                                "She swallowed the spider to catch the fly.\n" +
+                                "I don't know why she swallowed the fly. Perhaps she'll die.\n" +
+                                "\n" +
+                                "I know an old lady who swallowed a cat.\n" +
+                                "Imagine that, to swallow a cat!\n" +
+                                "She swallowed the cat to catch the bird.\n" +
+                                "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" +
+                                "She swallowed the spider to catch the fly.\n" +
+                                "I don't know why she swallowed the fly. Perhaps she'll die.\n" +
+                                "\n" +
+                                "I know an old lady who swallowed a dog.\n" +
+                                "What a hog, to swallow a dog!\n" +
+                                "She swallowed the dog to catch the cat.\n" +
+                                "She swallowed the cat to catch the bird.\n" +
+                                "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" +
+                                "She swallowed the spider to catch the fly.\n" +
+                                "I don't know why she swallowed the fly. Perhaps she'll die.\n" +
+                                "\n" +
+                                "I know an old lady who swallowed a goat.\n" +
+                                "Just opened her throat and swallowed a goat!\n" +
+                                "She swallowed the goat to catch the dog.\n" +
+                                "She swallowed the dog to catch the cat.\n" +
+                                "She swallowed the cat to catch the bird.\n" +
+                                "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" +
+                                "She swallowed the spider to catch the fly.\n" +
+                                "I don't know why she swallowed the fly. Perhaps she'll die.\n" +
+                                "\n" +
+                                "I know an old lady who swallowed a cow.\n" +
+                                "I don't know how she swallowed a cow!\n" +
+                                "She swallowed the cow to catch the goat.\n" +
+                                "She swallowed the goat to catch the dog.\n" +
+                                "She swallowed the dog to catch the cat.\n" +
+                                "She swallowed the cat to catch the bird.\n" +
+                                "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" +
+                                "She swallowed the spider to catch the fly.\n" +
+                                "I don't know why she swallowed the fly. Perhaps she'll die.\n" +
+                                "\n" +
+                                "I know an old lady who swallowed a horse.\n" +
+                                "She's dead, of course!";
+
+        Assert.That(FoodChain.Song(), Is.EqualTo(expected));
+    }
+}
