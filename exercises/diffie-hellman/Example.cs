5cfcf593028103d9c1af05324527ca84deb88910
Erik Schierboom <erik_schierboom@hotmail.com>
2016-09-13 10:49:31 +0200

    Add diffie-hellman exercise

---
+++ exercises/diffie-hellman/Example.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Numerics;
+
+public static class DiffieHellman
+{
+    private static readonly Random Random = new Random();
+
+    public static BigInteger PrivateKey(BigInteger primeP) => new BigInteger(Random.Next(1, (int) primeP - 1));
+
+    public static BigInteger PublicKey(BigInteger primeP, BigInteger primeG, BigInteger privateKey) => BigInteger.ModPow(primeG, privateKey, primeP);
+
+    public static BigInteger Secret(BigInteger primeP, BigInteger publicKey, BigInteger privateKey) => BigInteger.ModPow(publicKey, privateKey, primeP);
+}
