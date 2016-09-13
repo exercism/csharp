a69cffd0907a180c9eadff0ea665851452482850
Erik Schierboom <erik_schierboom@hotmail.com>
2016-09-13 10:42:58 +0200

    Add proverb exercise (#120)

---
+++ exercises/proverb/Example.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+public static class Proverb
+{
+    private static readonly string[] Subjects = { "nail", "shoe", "horse", "rider", "message", "battle", "kingdom" };
+
+    public static string Line(int line)
+    {
+        if (line == 7)
+        { 
+            return "And all for the want of a horseshoe nail.";
+        }
+
+        return $"For want of a {Subjects[line - 1]} the {Subjects[line]} was lost.";
+    }
+
+    public static string AllLines() => string.Join("\n", Enumerable.Range(1, 7).Select(Line));
+}
---
+++ exercises/proverb/ProverbTest.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+public class ProverbTest
+{
+    [Test]
+    public void Line_one()
+    {
+        Assert.That(Proverb.Line(1), Is.EqualTo("For want of a nail the shoe was lost."));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Line_four()
+    {
+        Assert.That(Proverb.Line(4), Is.EqualTo("For want of a rider the message was lost."));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Line_seven()
+    {
+        Assert.That(Proverb.Line(7), Is.EqualTo("And all for the want of a horseshoe nail."));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void All_lines()
+    {
+        const string expected = "For want of a nail the shoe was lost.\n" +
+                                "For want of a shoe the horse was lost.\n" +
+                                "For want of a horse the rider was lost.\n" +
+                                "For want of a rider the message was lost.\n" +
+                                "For want of a message the battle was lost.\n" +
+                                "For want of a battle the kingdom was lost.\n" +
+                                "And all for the want of a horseshoe nail.";
+
+        Assert.That(Proverb.AllLines(), Is.EqualTo(expected));
+    }
+}
