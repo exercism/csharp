0e9736bab2e8f47bb6bfbc14850a63d175092f30
Erik Schierboom <erik_schierboom@hotmail.com>
2016-09-14 09:19:03 +0200

    Add run-length-encoding exercise (#125)

---
+++ exercises/run-length-encoding/Example.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public static class RunLengthEncoding
+{
+    public static string Encode(string input) => string.Join("", GroupConsecutive(input).Select(EncodeConsecutive));
+
+    private static string EncodeConsecutive(Tuple<char, int> group) => group.Item2 > 1 ? $"{group.Item2}{group.Item1}" : group.Item1.ToString();
+
+    private static IEnumerable<Tuple<char, int>> GroupConsecutive(string input)
+    {
+        if (input.Length == 0)
+        {
+            yield break;
+        }
+
+        var current = input[0];
+        var count = 0;
+
+        foreach (var character in input)
+        {
+            if (character == current)
+            {
+                count++;
+            }
+            else
+            {
+                yield return Tuple.Create(current, count);
+                current = character;
+                count = 1;
+            }
+        }
+
+        yield return Tuple.Create(current, count);
+    }
+
+    public static string Decode(string input) => string.Join("", Regex.Matches(input, @"(\d+[^\d]|[^\d])").Cast<Match>().Select(Decode));
+
+    private static string Decode(Match match)
+    {
+        if (EncodedSingleCharacter(match))
+        {
+            return match.Value;
+        }
+
+        return DecodeConsecutiveCharacters(match);
+    }
+
+    private static bool EncodedSingleCharacter(Match match) => match.Value.Length == 1;
+
+    private static string DecodeConsecutiveCharacters(Match match)
+    {
+        var character = match.Value[match.Value.Length - 1];
+        var count = int.Parse(match.Value.Substring(0, match.Value.Length - 1));
+        return new string(character, count);
+    }
+}
---
+++ exercises/run-length-encoding/RunLengthEncodingTest.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+
+public class RunLengthEncodingTest
+{
+    [Test]
+    public void Encode_simple()
+    {
+        const string input = "AABBBCCCC";
+        const string expected = "2A3B4C";
+        Assert.That(RunLengthEncoding.Encode(input), Is.EqualTo(expected));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Decode_simple()
+    {
+        const string input = "2A3B4C";
+        const string expected = "AABBBCCCC";
+        Assert.That(RunLengthEncoding.Decode(input), Is.EqualTo(expected));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Encode_with_single_values()
+    {
+        const string input = "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB";
+        const string expected = "12WB12W3B24WB";
+        Assert.That(RunLengthEncoding.Encode(input), Is.EqualTo(expected));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Decode_with_single_values()
+    {
+        const string input = "12WB12W3B24WB";
+        const string expected = "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB";
+        Assert.That(RunLengthEncoding.Decode(input), Is.EqualTo(expected));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Encode_and_then_decode()
+    {
+        const string input = "zzz ZZ  zZ";
+        const string expected = "zzz ZZ  zZ";
+        Assert.That(RunLengthEncoding.Decode(RunLengthEncoding.Encode(input)), Is.EqualTo(expected));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Encode_unicode()
+    {
+        const string input = "⏰⚽⚽⚽⭐⭐⏰";
+        const string expected = "⏰3⚽2⭐⏰";
+        Assert.That(RunLengthEncoding.Encode(input), Is.EqualTo(expected));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Decode_unicode()
+    {
+        const string input = "⏰3⚽2⭐⏰";
+        const string expected = "⏰⚽⚽⚽⭐⭐⏰";
+        Assert.That(RunLengthEncoding.Decode(input), Is.EqualTo(expected));
+    }
+}
