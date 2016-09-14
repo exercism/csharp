6a74a55e51377df5cc143680f8ac54fe29e2b196
Erik Schierboom <erik_schierboom@hotmail.com>
2016-09-14 08:29:26 +0200

    Add flatten-array exercise

---
+++ exercises/flatten-array/Example.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+public static class Flattener
+{
+    public static IEnumerable Flatten(IEnumerable input)
+    {
+        foreach (var element in input)
+        {
+            var enumerable = element as IEnumerable;
+            if (enumerable != null)
+            {
+                foreach (var flattenedElement in Flatten(enumerable))
+                {
+                    yield return flattenedElement;
+                }
+            }
+            else if (element != null)
+            {
+                yield return element;
+            }
+        }
+    }
+}
---
+++ exercises/flatten-array/FlattenArrayTest.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+public class FlattenArrayTest
+{
+    [Test]
+    public void Flattens_A_Nested_List()
+    {
+        var nestedList = new List<object> { new List<object>() };
+        Assert.That(Flattener.Flatten(nestedList), Is.Empty);
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Flattens_2_Level_Nested_List()
+    {
+        var nestedList = new List<object> { 1, new List<object> { 2, 3, 4 }, 5 };
+        Assert.That(Flattener.Flatten(nestedList), Is.EquivalentTo(new List<object> { 1, 2, 3, 4, 5 }));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Flattens_3_Level_Nested_List()
+    {
+        var nestedList = new List<object>
+        {
+            1,
+            new List<object> { 2, 3, 4 },
+            5,
+            new List<object> { 6, new List<object> { 7, 8 } }
+        };
+        Assert.That(Flattener.Flatten(nestedList), Is.EquivalentTo(new List<object> { 1, 2, 3, 4, 5, 6, 7, 8 }));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Flattens_5_Level_Nested_List()
+    {
+        var nestedList = new List<object>
+        {
+            0,
+            2,
+            new List<object>
+            {
+                new List<object> { 2, 3 },
+                8,
+                100,
+                4,
+                new List<object> { new List<object> { new List<object> { 50 } } },
+                -2
+            }
+        };
+        Assert.That(Flattener.Flatten(nestedList), Is.EquivalentTo(new List<object> { 0, 2, 2, 3, 8, 100, 4, 50, -2 }));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Flattens_6_Level_Nested_List()
+    {
+        var nestedList = new List<object>
+        {
+            1,
+            new List<object>
+            {
+                2,
+                new List<object> { new List<object> { 3 } },
+                new List<object> { 4, new List<object> { new List<object> { 5 } } },
+                6,
+                7
+            },
+            8
+        };
+        Assert.That(Flattener.Flatten(nestedList), Is.EquivalentTo(new List<object> { 1, 2, 3, 4, 5, 6, 7, 8 }));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Flattens_6_Level_Nested_List_With_Nulls()
+    {
+        var nestedList = new List<object>
+        {
+            1,
+            new List<object>
+            {
+                2,
+                null,
+                new List<object> { new List<object> { 3 }, null },
+                new List<object> { 4, new List<object> { new List<object> { 5 } } },
+                6,
+                7,
+                new List<object> { new List<object> { null } }
+            },
+            8,
+            null
+        };
+        Assert.That(Flattener.Flatten(nestedList), Is.EquivalentTo(new List<object> { 1, 2, 3, 4, 5, 6, 7, 8 }));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void All_Null_Nested_List_Returns_Empty_List()
+    {
+        var nestedList = new List<object>
+        {
+            null,
+            new List<object>
+            {
+                null,
+                new List<object> { null },
+                new List<object> { new List<object> { new List<object> { null } } }
+            },
+            null
+        };
+        Assert.That(Flattener.Flatten(nestedList), Is.Empty);
+    }
+}
