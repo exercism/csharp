e8e09e344f0918088e64bb379684e5810565f840
Erik Schierboom <erik_schierboom@hotmail.com>
2016-12-23 23:38:08 +0100

    Add markdown exercise (#152)

---
+++ exercises/markdown/MarkdownTest.cs
@@ -0,0 +1,76 @@
+﻿using NUnit.Framework;
+
+public class MarkdownTest
+{
+    [Test]
+    public void Parses_normal_text_as_a_paragraph()
+    {
+        var input = "This will be a paragraph";
+        var expected = "<p>This will be a paragraph</p>";
+        Assert.That(Markdown.Parse(input), Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Parsing_italics()
+    {
+        var input = "_This will be italic_";
+        var expected = "<p><i>This will be italic</i></p>";
+        Assert.That(Markdown.Parse(input), Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Parsing_bold_text()
+    {
+        var input = "__This will be bold__";
+        var expected = "<p><em>This will be bold</em></p>";
+        Assert.That(Markdown.Parse(input), Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Mixed_normal_italics_and_bold_text()
+    {
+        var input = "This will _be_ __mixed__";
+        var expected = "<p>This will <i>be</i> <em>mixed</em></p>";
+        Assert.That(Markdown.Parse(input), Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void With_h1_header_level()
+    {
+        var input = "# This will be an h1";
+        var expected = "<h1>This will be an h1</h1>";
+        Assert.That(Markdown.Parse(input), Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void With_h2_header_level()
+    {
+        var input = "## This will be an h2";
+        var expected = "<h2>This will be an h2</h2>";
+        Assert.That(Markdown.Parse(input), Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void With_h6_header_level()
+    {
+        var input = "###### This will be an h6";
+        var expected = "<h6>This will be an h6</h6>";
+        Assert.That(Markdown.Parse(input), Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Unordered_lists()
+    {
+        var input = "* Item 1\n* Item 2";
+        var expected = "<ul><li>Item 1</li><li>Item 2</li></ul>";
+        Assert.That(Markdown.Parse(input), Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void With_a_little_bit_of_everything()
+    {
+        var input = "# Header!\n* __Bold Item__\n* _Italic Item_";
+        var expected = "<h1>Header!</h1><ul><li><em>Bold Item</em></li><li><i>Italic Item</i></li></ul>";
+        Assert.That(Markdown.Parse(input), Is.EqualTo(expected));
+    }
+}
