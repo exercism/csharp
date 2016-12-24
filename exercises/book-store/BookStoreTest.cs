a7a998c6d0544e698ac88d7c3648f2db5211ca90
Rob Keim <robkeim@gmail.com>
2016-12-24 02:55:13 +0100

    Add book-store exercise

---
+++ exercises/book-store/BookStoreTest.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+[TestFixture]
+public class BookStoreTest
+{
+    [Test]
+    public void Basket_with_single_book()
+    {
+        Assert.That(BookStore.CalculateTotalCost(MakeList(1)), Is.EqualTo(8));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Basket_with_two_of_same_book()
+    {
+        Assert.That(BookStore.CalculateTotalCost(MakeList(2, 2)), Is.EqualTo(16));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Empty_basket()
+    {
+        Assert.That(BookStore.CalculateTotalCost(MakeList()), Is.EqualTo(0));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Basket_with_two_different_books()
+    {
+        Assert.That(BookStore.CalculateTotalCost(MakeList(1, 2)), Is.EqualTo(15.2));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Basket_with_three_different_books()
+    {
+        Assert.That(BookStore.CalculateTotalCost(MakeList(1, 2, 3)), Is.EqualTo(21.6));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Basket_with_four_different_books()
+    {
+        Assert.That(BookStore.CalculateTotalCost(MakeList(1, 2, 3, 4)), Is.EqualTo(25.6));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Basket_with_five_different_books()
+    {
+        Assert.That(BookStore.CalculateTotalCost(MakeList(1, 2, 3, 4, 5)), Is.EqualTo(30));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Basket_with_eight_books()
+    {
+        Assert.That(BookStore.CalculateTotalCost(MakeList(1, 1, 2, 2, 3, 3, 4, 5)), Is.EqualTo(51.20));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Basket_with_nine_books()
+    {
+        Assert.That(BookStore.CalculateTotalCost(MakeList(1, 1, 2, 2, 3, 3, 4, 4, 5)), Is.EqualTo(55.60));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Basket_with_ten_books()
+    {
+        Assert.That(BookStore.CalculateTotalCost(MakeList(1, 1, 2, 2, 3, 3, 4, 4, 5, 5)), Is.EqualTo(60));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Basket_with_eleven_books()
+    {
+        Assert.That(BookStore.CalculateTotalCost(MakeList(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 1)), Is.EqualTo(68));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Basket_with_twelve_books()
+    {
+        Assert.That(BookStore.CalculateTotalCost(MakeList(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 1, 2)), Is.EqualTo(75.20));
+    }
+
+    private static List<int> MakeList(params int[] values)
+    {
+        return values.ToList();
+    }
+}
---
+++ exercises/book-store/Example.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class BookStore
+{
+    public static double CalculateTotalCost(List<int> books)
+    {
+        return CalculateTotalCost(books, 0);
+    }
+
+    private static double CalculateTotalCost(List<int> books, double priceSoFar)
+    {
+        if (books.Count == 0)
+        {
+            return priceSoFar;
+        }
+
+        var groups = books
+            .GroupBy(b => b)
+            .Select(g => g.Key)
+            .ToList();
+
+        var minPrice = double.MaxValue;
+
+        for (int i = groups.Count; i >= 1; i--)
+        {
+            var itemsToRemove = groups.Take(i).ToList();
+            var remaining = books.ToList();
+
+            foreach (var item in itemsToRemove)
+            {
+                remaining.Remove(item);
+            }
+
+            var price = CalculateTotalCost(remaining.ToList(), priceSoFar + CostPerGroup(i));
+            minPrice = Math.Min(minPrice, price);
+        }
+
+        return minPrice;
+    }
+
+    private static double CostPerGroup(int groupSize)
+    {
+        double discountPercentage;
+
+        switch (groupSize)
+        {
+            case 1:
+                discountPercentage = 0;
+                break;
+            case 2:
+                discountPercentage = 5;
+                break;
+            case 3:
+                discountPercentage = 10;
+                break;
+            case 4:
+                discountPercentage = 20;
+                break;
+            case 5:
+                discountPercentage = 25;
+                break;
+            default:
+                throw new InvalidOperationException($"Invalid group size: {groupSize}");
+        }
+        
+        return 8 * groupSize * (100 - discountPercentage) / 100;
+    }
+}
