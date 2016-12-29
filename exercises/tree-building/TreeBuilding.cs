0ba76fd4e8275caeaebd4dbbf9ba31849de6064c
Erik Schierboom <erik_schierboom@hotmail.com>
2016-12-29 19:01:00 +0100

    Add tree-building exercise

---
+++ exercises/tree-building/TreeBuilding.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class TreeBuildingRecord
+{
+    public int ParentId { get; set; }
+    public int RecordId { get; set; }
+}
+
+public class Tree
+{
+    public int Id { get; set; }
+    public int ParentId { get; set; }
+
+    public List<Tree> Children { get; set; }
+
+    public bool IsLeaf => Children.Count == 0;
+}
+
+public static class TreeBuilder
+{
+    public static Tree BuildTree(IEnumerable<TreeBuildingRecord> records)
+    {
+        var ordered = new SortedList<int, TreeBuildingRecord>();
+
+        foreach (var record in records)
+        {
+            ordered.Add(record.RecordId, record);
+        }
+
+        records = ordered.Values;
+
+        var trees = new List<Tree>();
+        var previousRecordId = -1;
+
+        foreach (var record in records)
+        {   
+            var t = new Tree { Children = new List<Tree>(), Id = record.RecordId, ParentId = record.ParentId };
+            trees.Add(t);
+
+            if ((t.Id == 0 && t.ParentId != 0) ||
+                (t.Id != 0 && t.ParentId >= t.Id) ||
+                (t.Id != 0 && t.Id != previousRecordId + 1))
+            {
+                throw new ArgumentException();
+            }
+
+            ++previousRecordId;
+        }
+        
+        if (trees.Count == 0)
+        {
+            throw new ArgumentException();
+        }
+
+        for (int i = 1; i < trees.Count; i++)
+        {
+            var t = trees.First(x => x.Id == i);
+            var parent = trees.First(x => x.Id == t.ParentId);
+            parent.Children.Add(t);
+        }
+
+        var r = trees.First(t => t.Id == 0);
+        return r;
+    }
+}
---
+++ exercises/tree-building/TreeBuildingTest.cs
@@ -0,0 +1,228 @@
+﻿using NUnit.Framework;
+
+public class TreeBuildingTest
+{
+    [Test]
+    public void One_node()
+    {
+        var records = new[]
+        {
+            new TreeBuildingRecord { RecordId = 0, ParentId = 0 }
+        };
+
+        var tree = TreeBuilder.BuildTree(records);
+
+        AssertTreeIsLeaf(tree, id: 0);
+    }
+
+    [Test]
+    public void Three_nodes_in_order()
+    {
+        var records = new[]
+        {
+            new TreeBuildingRecord { RecordId = 0, ParentId = 0 },
+            new TreeBuildingRecord { RecordId = 1, ParentId = 0 },
+            new TreeBuildingRecord { RecordId = 2, ParentId = 0 }
+        };
+
+        var tree = TreeBuilder.BuildTree(records);
+
+        AssertTreeIsBranch(tree, id: 0, childCount: 2);
+        AssertTreeIsLeaf(tree.Children[0], id: 1);
+        AssertTreeIsLeaf(tree.Children[1], id: 2);
+    }
+
+    [Test]
+    public void Three_nodes_in_reverse_order()
+    {
+        var records = new[]
+        {
+            new TreeBuildingRecord { RecordId = 2, ParentId = 0 },
+            new TreeBuildingRecord { RecordId = 1, ParentId = 0 },
+            new TreeBuildingRecord { RecordId = 0, ParentId = 0 }
+        };
+
+        var tree = TreeBuilder.BuildTree(records);
+
+        AssertTreeIsBranch(tree, id: 0, childCount: 2);
+        AssertTreeIsLeaf(tree.Children[0], id: 1);
+        AssertTreeIsLeaf(tree.Children[1], id: 2);
+    }
+
+    [Test]
+    public void More_than_two_children()
+    {
+        var records = new[]
+        {
+            new TreeBuildingRecord { RecordId = 3, ParentId = 0 },
+            new TreeBuildingRecord { RecordId = 2, ParentId = 0 },
+            new TreeBuildingRecord { RecordId = 1, ParentId = 0 },
+            new TreeBuildingRecord { RecordId = 0, ParentId = 0 }
+        };
+
+        var tree = TreeBuilder.BuildTree(records);
+
+        AssertTreeIsBranch(tree, id: 0, childCount: 3);
+        AssertTreeIsLeaf(tree.Children[0], id: 1);
+        AssertTreeIsLeaf(tree.Children[1], id: 2);
+        AssertTreeIsLeaf(tree.Children[2], id: 3);
+    }
+
+    [Test]
+    public void Binary_tree()
+    {
+        var records = new[]
+        {
+            new TreeBuildingRecord { RecordId = 5, ParentId = 1 },
+            new TreeBuildingRecord { RecordId = 3, ParentId = 2 },
+            new TreeBuildingRecord { RecordId = 2, ParentId = 0 },
+            new TreeBuildingRecord { RecordId = 4, ParentId = 1 },
+            new TreeBuildingRecord { RecordId = 1, ParentId = 0 },
+            new TreeBuildingRecord { RecordId = 0, ParentId = 0 },
+            new TreeBuildingRecord { RecordId = 6, ParentId = 2 }
+        };
+
+        var tree = TreeBuilder.BuildTree(records);
+
+        AssertTreeIsBranch(tree, id: 0, childCount: 2);
+        AssertTreeIsBranch(tree.Children[0], id: 1, childCount: 2);
+        AssertTreeIsBranch(tree.Children[1], id: 2, childCount: 2);
+
+        AssertTreeIsLeaf(tree.Children[0].Children[0], id: 4);
+        AssertTreeIsLeaf(tree.Children[0].Children[1], id: 5);
+        AssertTreeIsLeaf(tree.Children[1].Children[0], id: 3);
+        AssertTreeIsLeaf(tree.Children[1].Children[1], id: 6);
+    }
+
+    [Test]
+    public void Unbalanced_tree()
+    {
+        var records = new[]
+        {
+            new TreeBuildingRecord { RecordId = 5, ParentId = 2 },
+            new TreeBuildingRecord { RecordId = 3, ParentId = 2 },
+            new TreeBuildingRecord { RecordId = 2, ParentId = 0 },
+            new TreeBuildingRecord { RecordId = 4, ParentId = 1 },
+            new TreeBuildingRecord { RecordId = 1, ParentId = 0 },
+            new TreeBuildingRecord { RecordId = 0, ParentId = 0 },
+            new TreeBuildingRecord { RecordId = 6, ParentId = 2 }
+        };
+
+        var tree = TreeBuilder.BuildTree(records);
+
+        AssertTreeIsBranch(tree, id: 0, childCount: 2);
+        AssertTreeIsBranch(tree.Children[0], id: 1, childCount: 1);
+        AssertTreeIsBranch(tree.Children[1], id: 2, childCount: 3);
+
+        AssertTreeIsLeaf(tree.Children[0].Children[0], id: 4);
+        AssertTreeIsLeaf(tree.Children[1].Children[0], id: 3);
+        AssertTreeIsLeaf(tree.Children[1].Children[1], id: 5);
+        AssertTreeIsLeaf(tree.Children[1].Children[2], id: 6);
+    }
+
+    [Test]
+    public void Empty_input()
+    {
+        var records = new TreeBuildingRecord[0];
+
+        Assert.That(() => TreeBuilder.BuildTree(records), Throws.Exception);
+    }
+
+    [Test]
+    public void Root_node_has_parent()
+    {
+        var records = new[]
+        {
+            new TreeBuildingRecord { RecordId = 0, ParentId = 1 },
+            new TreeBuildingRecord { RecordId = 1, ParentId = 0 }
+        };
+
+        Assert.That(() => TreeBuilder.BuildTree(records), Throws.Exception);
+    }
+
+    [Test]
+    public void No_root_node()
+    {
+        var records = new[]
+        {
+            new TreeBuildingRecord { RecordId = 1, ParentId = 0 }
+        };
+
+        Assert.That(() => TreeBuilder.BuildTree(records), Throws.Exception);
+    }
+
+
+    [Test]
+    public void Non_continuous()
+    {
+        var records = new[]
+        {
+            new TreeBuildingRecord { RecordId = 2, ParentId = 0 },
+            new TreeBuildingRecord { RecordId = 4, ParentId = 2 },
+            new TreeBuildingRecord { RecordId = 1, ParentId = 0 },
+            new TreeBuildingRecord { RecordId = 0, ParentId = 0 }
+        };
+
+        Assert.That(() => TreeBuilder.BuildTree(records), Throws.Exception);
+    }
+
+    [Test]
+    public void Cycle_directly()
+    {
+        var records = new[]
+        {
+            new TreeBuildingRecord { RecordId = 5, ParentId = 2 },
+            new TreeBuildingRecord { RecordId = 3, ParentId = 2 },
+            new TreeBuildingRecord { RecordId = 2, ParentId = 2 },
+            new TreeBuildingRecord { RecordId = 4, ParentId = 1 },
+            new TreeBuildingRecord { RecordId = 1, ParentId = 0 },
+            new TreeBuildingRecord { RecordId = 0, ParentId = 0 },
+            new TreeBuildingRecord { RecordId = 6, ParentId = 3 }
+        };
+
+        Assert.That(() => TreeBuilder.BuildTree(records), Throws.Exception);
+    }
+
+    [Test]
+    public void Cycle_indirectly()
+    {
+        var records = new[]
+        {
+            new TreeBuildingRecord { RecordId = 5, ParentId = 2 },
+            new TreeBuildingRecord { RecordId = 3, ParentId = 2 },
+            new TreeBuildingRecord { RecordId = 2, ParentId = 6 },
+            new TreeBuildingRecord { RecordId = 4, ParentId = 1 },
+            new TreeBuildingRecord { RecordId = 1, ParentId = 0 },
+            new TreeBuildingRecord { RecordId = 0, ParentId = 0 },
+            new TreeBuildingRecord { RecordId = 6, ParentId = 3 }
+        };
+
+        Assert.That(() => TreeBuilder.BuildTree(records), Throws.Exception);
+    }
+
+    [Test]
+    public void Higher_id_parent_of_lower_id()
+    {
+        var records = new[]
+        {
+            new TreeBuildingRecord { RecordId = 0, ParentId = 0 },
+            new TreeBuildingRecord { RecordId = 2, ParentId = 0 },
+            new TreeBuildingRecord { RecordId = 1, ParentId = 2 }
+        };
+
+        Assert.That(() => TreeBuilder.BuildTree(records), Throws.Exception);
+    }
+
+    private static void AssertTreeIsBranch(Tree tree, int id, int childCount)
+    {
+        Assert.That(tree.Id, Is.EqualTo(id));
+        Assert.That(tree.IsLeaf, Is.False);
+        Assert.That(tree.Children.Count, Is.EqualTo(childCount));
+    }
+
+    private static void AssertTreeIsLeaf(Tree tree, int id)
+    {
+        Assert.That(tree.Id, Is.EqualTo(id));
+        Assert.That(tree.IsLeaf, Is.True);
+    }
+}
