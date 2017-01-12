98a1cc67da0724ba61935eb4e59c0a22a3262243
Erik Schierboom <erik_schierboom@hotmail.com>
2017-01-12 19:22:51 +0100

    Fix Luhn tests not running

--- exercises/luhn/LuhnTest.cs
+++ exercises/luhn/LuhnTest.cs
@@ -4,11 +4,11 @@ using NUnit.Framework;
 public class LuhnTest
 {
     [TestCase("1", ExpectedResult = false)] // single digit strings can not be valid
-    [TestCase("0", ExpectedResult = false, Ignore = "Remove to run test")] // a single zero is invalid
-    [TestCase("046 454 286", ExpectedResult = true, Ignore = "Remove to run test")] // valid Canadian SIN
-    [TestCase("046 454 287", ExpectedResult = false, Ignore = "Remove to run test")] // invalid Canadian SIN
-    [TestCase("8273 1232 7352 0569", ExpectedResult = false, Ignore = "Remove to run test")] // invalid credit card
-    [TestCase("827a 1232 7352 0569", ExpectedResult = false, Ignore = "Remove to run test")] // strings that contain non-digits are not valid
+    [TestCase("0", ExpectedResult = false, Ignore = "Remove to run test case")] // a single zero is invalid
+    [TestCase("046 454 286", ExpectedResult = true, Ignore = "Remove to run test case")] // valid Canadian SIN
+    [TestCase("046 454 287", ExpectedResult = false, Ignore = "Remove to run test case")] // invalid Canadian SIN
+    [TestCase("8273 1232 7352 0569", ExpectedResult = false, Ignore = "Remove to run test case")] // invalid credit card
+    [TestCase("827a 1232 7352 0569", ExpectedResult = false, Ignore = "Remove to run test case")] // strings that contain non-digits are not valid
     public bool ValidateChecksum(string number)
     {
         return Luhn.IsValid(number);
