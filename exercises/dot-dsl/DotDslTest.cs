3ab5983affef2b2768682d09594c5c6da78402a6
Erik Schierboom <erik_schierboom@hotmail.com>
2017-01-12 18:35:52 +0100

    Add dot-dsl exercise

---
+++ exercises/dot-dsl/DotDslTest.cs
@@ -0,0 +1,91 @@
+﻿using NUnit.Framework;
+
+public class DotDslTest
+{
+    [Test]
+    public void Empty_graph()
+    {
+        var g = new Graph();
+
+        Assert.That(g.Nodes, Is.Empty);
+        Assert.That(g.Edges, Is.Empty);
+        Assert.That(g.Attrs, Is.Empty);
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Graph_with_one_node()
+    {
+        var g = new Graph
+        {
+            new Node("a")
+        };
+
+        Assert.That(g.Nodes, Is.EquivalentTo(new[] { new Node("a") }));
+        Assert.That(g.Edges, Is.Empty);
+        Assert.That(g.Attrs, Is.Empty);
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Graph_with_one_node_with_keywords()
+    {
+        var g = new Graph
+        {
+            new Node("a") { { "color", "green" } }
+        };
+
+        Assert.That(g.Nodes, Is.EquivalentTo(new[] { new Node("a") { { "color", "green" } } }));
+        Assert.That(g.Edges, Is.Empty);
+        Assert.That(g.Attrs, Is.Empty);
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Graph_with_one_edge()
+    {
+        var g = new Graph
+        {
+            new Edge("a", "b")
+        };
+
+        Assert.That(g.Nodes, Is.Empty);
+        Assert.That(g.Edges, Is.EquivalentTo(new[] { new Edge("a", "b") }));
+        Assert.That(g.Attrs, Is.Empty);
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Graph_with_one_attribute()
+    {
+        var g = new Graph
+        {
+            { "foo", "1" }
+        };
+
+        Assert.That(g.Nodes, Is.Empty);
+        Assert.That(g.Edges, Is.Empty);
+        Assert.That(g.Attrs, Is.EquivalentTo(new[] { new Attr("foo", "1") }));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Graph_with_attributes()
+    {
+        var g = new Graph
+        {
+            { "foo", "1" },
+            { "title", "Testing Attrs" },
+            new Node("a") { { "color", "green" } },
+            new Node("c"),
+            new Node("b") { {  "label", "Beta!" } },
+            new Edge("b", "c"),
+            new Edge("a", "b") { { "color", "blue" } },
+            { "bar", "true" }
+        };
+
+        Assert.That(g.Nodes, Is.EquivalentTo(new[] { new Node("a") { { "color", "green" } }, new Node("b") { { "label", "Beta!" } }, new Node("c") }));
+        Assert.That(g.Edges, Is.EquivalentTo(new[] { new Edge("a", "b") { { "color", "blue" } }, new Edge("b", "c") }));
+        Assert.That(g.Attrs, Is.EquivalentTo(new[] { new Attr("bar", "true"), new Attr("foo", "1"), new Attr("title", "Testing Attrs") }));
+    }
+}
---
+++ exercises/dot-dsl/Example.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Node : Element
+{
+    public Node(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+            return false;
+
+        return ((Node)obj).Name == Name;
+    }
+
+    public override int GetHashCode() => Name.GetHashCode();
+}
+
+public class Edge : Element
+{
+    public Edge(string node1, string node2)
+    {
+        Node1 = node1;
+        Node2 = node2;
+    }
+
+    public string Node1 { get; }
+    public string Node2 { get; }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+            return false;
+
+        return ((Edge)obj).Node1 == Node1 && ((Edge)obj).Node2 == Node2;
+    }
+
+    public override int GetHashCode() => Node1.GetHashCode() ^ Node2.GetHashCode();
+}
+
+
+public class Attr
+{
+    public Attr(string key, string value)
+    {
+        Key = key;
+        Value = value;
+    }
+
+    public string Key { get; }
+    public string Value { get; }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+            return false;
+
+        return ((Attr)obj).Key == Key && ((Attr)obj).Value == Value;
+    }
+
+    public override int GetHashCode() => Key.GetHashCode() ^ Value.GetHashCode();
+}
+
+public class Graph : Element
+{
+    private List<Node> nodes = new List<Node>();
+    private List<Edge> edges = new List<Edge>();
+
+    public IEnumerable<Node> Nodes => nodes;
+    public IEnumerable<Edge> Edges => edges;
+    public IEnumerable<Attr> Attrs => attrs;
+
+    public void Add(Node node) => nodes.Add(node);
+    public void Add(Edge edge) => edges.Add(edge);
+}
+
+public abstract class Element : IEnumerable<Attr>
+{
+    protected List<Attr> attrs = new List<Attr>();
+
+    public void Add(string key, string value) => attrs.Add(new Attr(key, value));
+
+    public IEnumerator<Attr> GetEnumerator() => attrs.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
