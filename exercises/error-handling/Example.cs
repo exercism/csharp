2849365e61d92c0932583ee13b281d31d34e10e4
Rob Keim <robkeim@gmail.com>
2017-01-16 22:43:14 +0100

    Add error-handling exercise

---
+++ exercises/error-handling/Example.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public static class ErrorHandling
+{
+    public static void HandleErrorByThrowingException()
+    {
+        throw new Exception("An error occured.");
+    }
+
+    public static int? HandleErrorByReturningNullableType(string input)
+    {
+        int result;
+
+        if (int.TryParse(input, out result))
+        {
+            return result;
+        }
+
+        return null;
+    }
+
+    public static bool HandleErrorWithOutParam(string input, out int result)
+    {
+        return int.TryParse(input, out result);
+    }
+
+    public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(IDisposable disposableObject)
+    {
+        using (disposableObject)
+        {
+            throw new Exception("An error occured.");
+        }
+    }
+}
