ece9c87f1b9229302ab3c8d3e394ea43628601ae
Frida Tveit <Frida.Tveit@softwire.com>
2017-01-17 19:34:30 +0000

    Added ignore to SumOfMultiplesTest so that only the first test is run when the user first tries to run them.

--- exercises/sum-of-multiples/SumOfMultiplesTest.cs
+++ exercises/sum-of-multiples/SumOfMultiplesTest.cs
@@ -8,37 +8,43 @@ public class SumOfMultiplesTest
     {
         Assert.That(SumOfMultiples.To(new[] { 3, 5 }, 1), Is.EqualTo(0));
     }
-    
+
+    [Ignore("Remove to run test")]
     [Test]
     public void Sum_to_3()
     {
         Assert.That(SumOfMultiples.To(new[] { 3, 5 }, 4), Is.EqualTo(3));
     }
-    
+
+    [Ignore("Remove to run test")]
     [Test]
     public void Sum_to_10()
     {
         Assert.That(SumOfMultiples.To(new[] { 3, 5 }, 10), Is.EqualTo(23));
     }
-    
+
+    [Ignore("Remove to run test")]
     [Test]
     public void Sum_to_100()
     {
        Assert.That(SumOfMultiples.To(new[] { 3, 5 }, 100), Is.EqualTo(2318));
     }
-    
+
+    [Ignore("Remove to run test")]
     [Test]
     public void Sum_to_1000()
     {
         Assert.That(SumOfMultiples.To(new[] { 3, 5 }, 1000), Is.EqualTo(233168));
     }
-    
+
+    [Ignore("Remove to run test")]
     [Test]
     public void Sum_to_20()
     {
         Assert.That(SumOfMultiples.To(new [] { 7, 13, 17 }, 20), Is.EqualTo(51));
     }
-    
+
+    [Ignore("Remove to run test")]
     [Test]
     public void Sum_to_10000()
     {
