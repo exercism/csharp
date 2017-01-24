6c36af4c32e929eb682ab654d71254e82db7d5b9
Erik Schierboom <erik_schierboom@hotmail.com>
2017-01-24 19:46:41 +0100

    Add forth exercise

---
+++ exercises/forth/Example.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Sprache;
+
+public enum ForthError
+{
+    DivisionByZero,
+    StackUnderflow,
+    InvalidWord,
+    UnknownWord
+}
+
+public class ForthException : Exception
+{
+    public ForthException(ForthError error)
+    {
+        Error = error;
+    }
+
+    public ForthError Error { get; }
+}
+
+public class ForthState
+{
+    public Stack<int> Stack { get; } = new Stack<int>();
+    public Dictionary<string, IEnumerable<ForthDefinition>> Mapping { get; } = new Dictionary<string, IEnumerable<ForthDefinition>>();
+
+    public override string ToString() => string.Join(" ", Stack.Reverse());
+}
+
+public abstract class ForthDefinition
+{
+    public abstract void Evaluate(ForthState state);    
+}
+
+public abstract class TermDefinition : ForthDefinition
+{
+    public TermDefinition(string term)
+    {
+        Term = term;
+    }
+
+    public string Term { get; }
+
+    public override void Evaluate(ForthState state)
+    {
+        if (IsUserTerm(state))
+            EvaluateUserTerm(state);
+        else
+            EvaluateDefaultTerm(state);
+    }
+
+    private bool IsUserTerm(ForthState state) => state.Mapping.ContainsKey(Term);
+
+    public virtual void EvaluateUserTerm(ForthState state)
+    {
+        foreach (var definition in state.Mapping[Term])
+            definition.Evaluate(state);
+    }
+
+    public abstract void EvaluateDefaultTerm(ForthState state);
+}
+
+public abstract class UnaryOperation: TermDefinition
+{
+    public UnaryOperation(string term) : base(term)
+    {
+    }
+
+    public override void EvaluateDefaultTerm(ForthState state)
+    {        
+        if (state.Stack.Count < 1)
+            throw new ForthException(ForthError.StackUnderflow);
+
+        var operand = state.Stack.Pop();
+
+        foreach (var value in operation(operand))
+            state.Stack.Push(value);
+    }
+
+    public abstract List<int> operation(int x);
+}
+
+public abstract class BinaryOperation : TermDefinition
+{
+    public BinaryOperation(string term) : base(term)
+    {
+    }
+
+    public override void EvaluateDefaultTerm(ForthState state)
+    {
+        if (state.Stack.Count <= 1)
+            throw new ForthException(ForthError.StackUnderflow);
+
+        var operand2 = state.Stack.Pop();
+        var operand1 = state.Stack.Pop();
+
+        foreach (var value in operation(operand1, operand2))
+            state.Stack.Push(value);
+    }
+
+    public abstract List<int> operation(int x, int y);
+}
+
+public class Constant : ForthDefinition
+{
+    private readonly int n;
+
+    public Constant(int n)
+    {
+        this.n = n;
+    }
+
+    public override void Evaluate(ForthState state) => state.Stack.Push(n);
+}
+
+public class Word : TermDefinition
+{
+    public Word(string str) : base(str)
+    {
+    }
+
+    public override void EvaluateDefaultTerm(ForthState state)
+    {
+        if (!state.Mapping.ContainsKey(Term))
+            throw new ForthException(ForthError.UnknownWord);
+    }
+}
+
+public class Dup: UnaryOperation
+{
+    public Dup() : base("dup") {}
+    public override List<int> operation(int x) => new List<int> { x, x };
+}
+
+public class Drop : UnaryOperation
+{
+    public Drop() : base("drop") {}
+    public override List<int> operation(int x) => new List<int>();
+}
+
+public class Swap : BinaryOperation
+{
+    public Swap() : base("swap") {}
+    public override List<int> operation(int x, int y) => new List<int> { y, x };
+}
+
+public class Over : BinaryOperation
+{
+    public Over() : base("over") {}
+    public override List<int> operation(int x, int y) => new List<int> { x, y, x };
+}
+
+public class Plus : BinaryOperation
+{
+    public Plus() : base("+") {}
+    public override List<int> operation(int x, int y) => new List<int> { x + y };
+}
+
+public class Minus : BinaryOperation
+{
+    public Minus() : base("-") {}
+    public override List<int> operation(int x, int y) => new List<int> { x - y };
+}
+
+public class Multiply : BinaryOperation
+{
+    public Multiply() : base("*") {}
+    public override List<int> operation(int x, int y) => new List<int> { x * y };
+}
+
+public class Division : BinaryOperation
+{
+    public Division() : base("/") {}
+
+    public override List<int> operation(int x, int y)
+    {
+        if (y == 0)
+            throw new ForthException(ForthError.DivisionByZero);
+
+        return new List<int> { x / y };
+    }
+}
+
+public class CustomTerm : ForthDefinition
+{
+    private readonly string term;
+    private readonly IEnumerable<ForthDefinition> actions;
+
+    public CustomTerm(string term, IEnumerable<ForthDefinition> actions)
+    {
+        this.term = term;
+        this.actions = actions;
+    }
+
+    public override void Evaluate(ForthState state)
+    {
+        int result;
+        if (int.TryParse(term, out result))
+            throw new ForthException(ForthError.InvalidWord);
+
+        state.Mapping[term] = actions;
+    }
+}
+
+public static class Forth
+{
+    public static string Eval(string input)
+    {
+        var expression = Expression.Parse(input);
+        var state = new ForthState();
+
+        foreach (var definition in expression)
+            definition.Evaluate(state);
+
+        return state.ToString();
+    }
+    
+    private static readonly Parser<Constant> Constant =
+        from number in Parse.Number
+        from whitespace in Parse.WhiteSpace.Optional()
+        select new Constant(int.Parse(number));
+
+    private static readonly Parser<string> TermIdentifier =
+        from term in Parse.Regex(@"[^\s;]+")
+        from whitespace in Parse.WhiteSpace.Optional()
+        select term.ToLowerInvariant();
+
+    private static readonly Parser<ForthDefinition> Term = TermIdentifier.Select<string, ForthDefinition>(term =>
+    {
+        switch (term)
+        {
+            case "+": return new Plus();
+            case "-": return new Minus();
+            case "*": return new Multiply();
+            case "/": return new Division();
+            case "dup": return new Dup();
+            case "drop": return new Drop();
+            case "swap": return new Swap();
+            case "over": return new Over();
+            default: return new Word(term);
+        }
+    });
+
+    private static readonly Parser<ForthDefinition> CustomTerm =
+        from a in Parse.Char(':')
+        from b in Parse.WhiteSpace
+        from customTerm in TermIdentifier
+        from definitions in Constant.XOr(Term).AtLeastOnce()            
+        from d in Parse.Char(';')
+        from e in Parse.WhiteSpace.Optional()
+        select new CustomTerm(customTerm, definitions);
+
+    private static readonly Parser<IEnumerable<ForthDefinition>> Expression =
+        (CustomTerm.XOr(Constant).XOr(Term)).Many();
+}
---
+++ exercises/forth/ForthTest.cs
@@ -0,0 +1,134 @@
+﻿using NUnit.Framework;
+
+public class ForthTest
+{
+    [Test]
+    public void No_input()
+    {
+        Assert.That(Forth.Eval(""), Is.EqualTo(""));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Numbers_just_get_pushed_onto_the_stack()
+    {
+        Assert.That(Forth.Eval("1 2 3 4 5"), Is.EqualTo("1 2 3 4 5"));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Non_word_characters_are_separators()
+    {
+        Assert.That(Forth.Eval("1\v2\t3\n4\r5 6\t7"), Is.EqualTo("1 2 3 4 5 6 7"));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Basic_arithmetic()
+    {
+        Assert.That(Forth.Eval("1 2 + 4 -"), Is.EqualTo("-1"));
+        Assert.That(Forth.Eval("2 4 * 3 /"), Is.EqualTo("2"));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Division_by_zero()
+    {
+        var exception = Assert.Throws<ForthException>(() => Forth.Eval("4 2 2 - /"));
+        Assert.That(exception.Error, Is.EqualTo(ForthError.DivisionByZero));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void dup()
+    {
+        Assert.That(Forth.Eval("1 DUP"), Is.EqualTo("1 1"));
+        Assert.That(Forth.Eval("1 2 Dup"), Is.EqualTo("1 2 2"));
+
+        var exception = Assert.Throws<ForthException>(() => Forth.Eval("dup"));
+        Assert.That(exception.Error, Is.EqualTo(ForthError.StackUnderflow));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void drop()
+    {
+        Assert.That(Forth.Eval("1 drop"), Is.EqualTo(""));
+        Assert.That(Forth.Eval("1 2 drop"), Is.EqualTo("1"));
+
+        var exception = Assert.Throws<ForthException>(() => Forth.Eval("drop"));
+        Assert.That(exception.Error, Is.EqualTo(ForthError.StackUnderflow));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void swap()
+    {
+        Assert.That(Forth.Eval("1 2 swap"), Is.EqualTo("2 1"));
+        Assert.That(Forth.Eval("1 2 3 swap"), Is.EqualTo("1 3 2"));
+
+        var exception1 = Assert.Throws<ForthException>(() => Forth.Eval("1 swap"));
+        Assert.That(exception1.Error, Is.EqualTo(ForthError.StackUnderflow));
+
+        var exception2 = Assert.Throws<ForthException>(() => Forth.Eval("swap"));
+        Assert.That(exception2.Error, Is.EqualTo(ForthError.StackUnderflow));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void over()
+    {
+        Assert.That(Forth.Eval("1 2 over"), Is.EqualTo("1 2 1"));
+        Assert.That(Forth.Eval("1 2 3 over"), Is.EqualTo("1 2 3 2"));
+
+        var exception1 = Assert.Throws<ForthException>(() => Forth.Eval("1 over"));
+        Assert.That(exception1.Error, Is.EqualTo(ForthError.StackUnderflow));
+
+        var exception2 = Assert.Throws<ForthException>(() => Forth.Eval("over"));
+        Assert.That(exception2.Error, Is.EqualTo(ForthError.StackUnderflow));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Defining_a_new_word()
+    {
+        Assert.That(Forth.Eval(": dup-twice dup dup ; 1 dup-twice"), Is.EqualTo("1 1 1"));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Redefining_an_existing_word()
+    {
+        Assert.That(Forth.Eval(": foo dup ; : foo dup dup ; 1 foo"), Is.EqualTo("1 1 1"));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Redefining_an_existing_built_in_word()
+    {
+        Assert.That(Forth.Eval(": swap dup ; 1 swap"), Is.EqualTo("1 1"));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Defining_words_with_odd_characters()
+    {
+        Assert.That(Forth.Eval(": € 220371 ; €"), Is.EqualTo("220371"));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Defining_a_number()
+    {
+        var exception = Assert.Throws<ForthException>(() => Forth.Eval(": 1 2 ;"));
+        Assert.That(exception.Error, Is.EqualTo(ForthError.InvalidWord));
+    }
+
+    [Ignore("Remove to run test")]
+    [Test]
+    public void Calling_a_non_existing_word()
+    {
+        var exception = Assert.Throws<ForthException>(() => Forth.Eval("1 foo"));
+        Assert.That(exception.Error, Is.EqualTo(ForthError.UnknownWord));
+    }
+}
