8ea9c569f02f4f30b1b36eefb3a33f57d0cdb67c
Erik Schierboom <erik_schierboom@hotmail.com>
2017-02-20 20:15:56 +0100

    Update hello-world exercise (#206)

--- exercises/hello-world/HelloWorldTest.cs
+++ exercises/hello-world/HelloWorldTest.cs
@@ -4,22 +4,8 @@
 public class HelloWorldTest
 {
     [Test]
-    public void No_name()
+    public void Say_hi()
     {
-        Assert.That(HelloWorld.Hello(null), Is.EqualTo("Hello, World!"));
-    }
-
-    [Ignore("Remove to run test")]
-    [Test]
-    public void Sample_name()
-    {
-        Assert.That(HelloWorld.Hello("Alice"), Is.EqualTo("Hello, Alice!"));
-    }
-
-    [Ignore("Remove to run test")]
-    [Test]
-    public void Other_sample_name()
-    {
-        Assert.That(HelloWorld.Hello("Bob"), Is.EqualTo("Hello, Bob!"));
+        Assert.That(HelloWorld.Hello(), Is.EqualTo("Hello, World!"));
     }
 }
