3b1f1051ff5193585572dd31d40a54bdf8da90fa
Erik Schierboom <erik_schierboom@hotmail.com>
2017-03-12 14:49:01 +0100

    Support nested canonical data cases

--- generators/CanonicalDataCase.cs
+++ generators/CanonicalDataCase.cs
@@ -1,26 +1,18 @@
 ﻿using Newtonsoft.Json;
 using System.ComponentModel.DataAnnotations;
-using System.Collections.Generic;
 
 namespace Generators
 {
-    public class CanonicalDataCase : IValidatableObject
+    public class CanonicalDataCase
     {
         [Required]
         public string Description { get; set; }
-        
+
+        [Required]
         public string Property { get; set; }
 
         public string[] Comments { get; set; }
 
         public object Expected { get; set; }
-
-        public CanonicalDataCase[] Cases { get; set; }
-
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if (Cases == null && string.IsNullOrWhiteSpace(Property))
-                yield return new ValidationResult("This field is required.", new[] { nameof(Property) });
-        }
     }
 }
---
+++ generators/CanonicalDataCasesJsonConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Generators
+{
+    public class CanonicalDataCasesJsonConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(IEnumerable<CanonicalData>).GetTypeInfo().IsAssignableFrom(objectType);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var casesToken = JToken.ReadFrom(reader);
+            var caseTokens = casesToken.SelectTokens("$..*[?(@.property)]");
+            
+            return new JArray(caseTokens).ToObject(objectType);            
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
