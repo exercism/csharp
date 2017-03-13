785d1752ed5c5dddc2a16745e7631c2dcc8e43e4
Erik Schierboom <erik_schierboom@hotmail.com>
2017-03-13 16:14:14 +0100

    Add logging and use correct test filename

--- exercises/leap/LeapTest.cs
+++ exercises/leap/LeapTest.cs
@@ -3,26 +3,26 @@ using Xunit;
 public class LeapTest
 {
     [Fact]
-    public void Year_not_divisible_by_4_is_common_year()
+    public void Valid_leap_year()
     {
-        Assert.False(Year.IsLeap(2015));
+        Assert.True(Year.IsLeap(1996));
     }
 
     [Fact(Skip = "Remove to run test")]
-    public void Year_divisible_by_4_not_divisible_by_100_is_leap_year()
+    public void Invalid_leap_year()
     {
-        Assert.True(Year.IsLeap(2016));
+        Assert.False(Year.IsLeap(1997));
     }
 
     [Fact(Skip = "Remove to run test")]
-    public void Year_divisible_by_100_not_divisible_by_400_is_common_year()
+    public void Turn_of_the_20th_century_is_not_a_leap_year()
     {
-        Assert.False(Year.IsLeap(2100));
+        Assert.False(Year.IsLeap(1900));
     }
 
     [Fact(Skip = "Remove to run test")]
-    public void Year_divisible_by_400_is_leap_year()
+    public void Turn_of_the_25th_century_is_a_leap_year()
     {
-        Assert.True(Year.IsLeap(2000));
+        Assert.True(Year.IsLeap(2400));
     }
 }
