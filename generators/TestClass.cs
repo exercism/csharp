15d42b238a22787ef548048eb0f37fd818f6df01
Erik Schierboom <erik_schierboom@hotmail.com>
2017-03-14 18:00:41 +0100

    Add support for specifying namespaces and before- and after test methods code

--- generators/TestClass.cs
+++ generators/TestClass.cs
@@ -1,8 +1,13 @@
-﻿namespace Generators
+﻿using System.Collections.Generic;
+
+namespace Generators
 {
     public class TestClass
     {
+        public ISet<string> UsingNamespaces { get; set; } = new HashSet<string> { "Xunit" };
         public string ClassName { get; set; }
+        public string BeforeTestMethods { get; set; }
         public TestMethod[] TestMethods { get; set; }
+        public string AfterTestMethods { get; set; }
     }    
 }
--- generators/TestClassRenderer.cs
+++ generators/TestClassRenderer.cs
@@ -1,20 +1,36 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Generators
 {
     public static class TestClassRenderer
     {
         private const string TestClassTemplate =
-@"using Xunit;
+@"{UsingNamespaces}
 
 public class {ClassName}Test
 {
-{TestMethods}
+{Body}
 }";
 
         public static string Render(TestClass testClass) =>
             TestClassTemplate
+                .Replace("{UsingNamespaces}", RenderUsingNamespaces(testClass))
                 .Replace("{ClassName}", testClass.ClassName)
-                .Replace("{TestMethods}", string.Join("\n\n", testClass.TestMethods.Select(TestMethodRenderer.Render)));
+                .Replace("{Body}", RenderBody(testClass));
+
+        private static string RenderUsingNamespaces(TestClass testClass) =>
+           string.Join("\n", testClass.UsingNamespaces.Select(usingNamespace => $"using {usingNamespace};"));
+
+        private static string RenderBody(TestClass testClass) =>
+           string.Join("\n\n", GetBodyParts(testClass));
+
+        private static IEnumerable<string> GetBodyParts(TestClass testClass) =>
+            from bodyPart in new [] { testClass.BeforeTestMethods, RenderTestMethods(testClass), testClass.AfterTestMethods }
+            where !string.IsNullOrWhiteSpace(bodyPart)
+            select bodyPart;
+
+        private static string RenderTestMethods(TestClass testClass) =>
+           string.Join("\n\n", testClass.TestMethods.Select(TestMethodRenderer.Render));
     }
 }
