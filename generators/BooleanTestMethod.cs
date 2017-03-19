3a3618437ec06d8b7bad62d27614e0d578580de9
Erik Schierboom <erik_schierboom@hotmail.com>
2017-03-19 15:30:58 +0100

    Add test method generator base

--- generators/BooleanTestMethod.cs
+++ generators/BooleanTestMethod.cs
@@ -1,21 +1,15 @@
 using System;
-using Humanizer;
 
 namespace Generators
 {
-    public class BooleanTestMethod : TestMethod
+    public class BooleanTestMethod : TestMethodBase
     {
         public BooleanTestMethod(CanonicalData canonicalData, CanonicalDataCase canonicalDataCase, int index)
+            : base(canonicalData, canonicalDataCase, index)
         {
-            Index = index;
-            MethodName = GetMethodName(canonicalDataCase);
-            Body = GetBody(canonicalData, canonicalDataCase);
         }
 
-        private static string GetMethodName(CanonicalDataCase canonicalDataCase)
-            => canonicalDataCase.Description.Replace(":", " is").Transform(To.TestMethodName);
-
-        private static string GetBody(CanonicalData canonicalData, CanonicalDataCase canonicalDataCase)
+        protected override string GetBody(CanonicalData canonicalData, CanonicalDataCase canonicalDataCase)
         {
             var isTrue = Convert.ToBoolean(canonicalDataCase.Expected);
             var testedClassName = GetTestedClassName(canonicalData);
@@ -24,12 +18,9 @@ namespace Generators
             return $"Assert.{isTrue}({testedClassName}.{testedMethod}({canonicalDataCase.Input}));";
         }
 
-        private static string GetTestedClassName(CanonicalData canonicalData)
-            => canonicalData.Exercise.Transform(To.TestedClassName);
-
-        private static string GetTestedMethod(CanonicalDataCase canonicalDataCase)
+        protected override string GetTestedMethod(CanonicalDataCase canonicalDataCase)
         {
-            var testedMethod = canonicalDataCase.Property.Transform(To.TestedMethodName);
+            var testedMethod = base.GetTestedMethod(canonicalDataCase);
             return testedMethod.StartsWith("Is") ? testedMethod : "Is" + testedMethod;
         }
     }
