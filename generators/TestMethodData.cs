cbd176ca71f780a51c20cb4d92d7f322b7dd219e
Erik Schierboom <erik_schierboom@hotmail.com>
2017-03-19 15:52:48 +0100

    Refine test method generator and support equality tests

---
+++ generators/TestMethodData.cs
@@ -0,0 +1,10 @@
+namespace Generators
+{
+    public class TestMethodData
+    {
+        public CanonicalData CanonicalData { get; set; }
+        public CanonicalDataCase CanonicalDataCase { get; set;}
+        public int Index { get; set; }
+        public string InputProperty { get; set; }
+    }
+}
