60e0f58c26c7ee8497cb4d713409ea5a2ebb00ec
Erik Schierboom <erik_schierboom@hotmail.com>
2017-03-20 17:14:16 +0100

    Add raindrops generator (#259)
    Refs #194, #195

---
+++ generators/Exercises/RaindropsExercise.cs
@@ -0,0 +1,20 @@
+﻿using Generators.Data;
+using Generators.Methods;
+
+namespace Generators.Exercises
+{
+    public class RaindropsExercise : EqualityExercise
+    {
+        public RaindropsExercise() : base("raindrops")
+        {
+        }
+
+        protected override TestMethodOptions CreateTestMethodOptions(CanonicalData canonicalData, CanonicalDataCase canonicalDataCase, int index)
+        {
+            var testMethodOptions = base.CreateTestMethodOptions(canonicalData, canonicalDataCase, index);
+            testMethodOptions.InputProperty = "number";
+
+            return testMethodOptions;
+        }
+    }
+}
