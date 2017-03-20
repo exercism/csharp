0c7ced5318bcf4b82118722aba621c328fb95ce2
Erik Schierboom <erik_schierboom@hotmail.com>
2017-03-20 17:12:50 +0100

    Add atbash-cipher generator

--- exercises/atbash-cipher/AtbashCipher.cs
+++ exercises/atbash-cipher/AtbashCipher.cs
@@ -2,7 +2,12 @@ using System;
 
 public static class Atbash
 {
-    public static string Encode(string value)
+    public static string Encode(string plainValue)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+
+    public static string Decode(string encodedValue)
     {
         throw new NotImplementedException("You need to implement this function.");
     }
