09f21d8306e6fcb396236d6d014f4aedd7577e67
Erik Schierboom <erik_schierboom@hotmail.com>
2017-03-28 22:07:44 +0200

    Add beer-song generator (#269)
    Refs #194, #195

--- generators/Data/CanonicalDataValue.cs
+++ generators/Data/CanonicalDataValue.cs
@@ -1,10 +1,24 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
 
 namespace Generators.Data
 {
     public static class CanonicalDataValue
     {
-        public static string StringArrayToString(object expected)
-            => string.Join("\\n\"+\n\"", ((JArray) expected).Values<string>());
+        public static string ExpectedToMultiLineString(object expected)
+        {
+            switch (expected)
+            {
+                case IEnumerable<string> enumerable:
+                    return string.Join("\\n\"+\n\"", enumerable);
+                case JArray jarray:
+                    return ExpectedToMultiLineString(((JArray) expected).Values<string>());
+                case string str:
+                    return ExpectedToMultiLineString(str.Split('\n'));
+                default:
+                    throw new ArgumentException("Cannot convert expected value to multil-ine string.");
+            }
+        }
     }
 }
