9d845679227afa61e45e231f6438a58ef18a70c1
rwobben <r.wobben@home.nl>
2017-03-31 12:49:25 +0200

    Change roster so roster displays all students sorted (#273)
    * Change roster so roster displays all students sorted
    
    * updated example.cs so it works with the new Roster function
    
    * repaired indentation

--- exercises/grade-school/GradeSchoolTest.cs
+++ exercises/grade-school/GradeSchoolTest.cs
@@ -3,46 +3,42 @@ using Xunit;
 public class GradeSchoolTest
 {
     [Fact]
-    public void Adding_a_student_adds_them_to_the_roster_for_the_given_grade()
+    public void Adding_a_student_adds_them_to_the_sorted_roster()
     {
         var school = new School();
         school.Add("Aimee", 2);
 
-        var actual = school.Roster(2);
+        var actual = school.Roster();
 
         var expected = new[] { "Aimee" };
         Assert.Equal(expected, actual);
     }
 
     [Fact(Skip = "Remove to run test")]
-    public void Adding_more_students_to_the_same_grade_adds_them_to_the_roster()
+    public void Adding_more_students_adds_them_to_the_sorted_roster()
     {
         var school = new School();
         school.Add("Blair", 2);
         school.Add("James", 2);
         school.Add("Paul", 2);
        
-        var actual = school.Roster(2);
+        var actual = school.Roster();
 
         var expected = new[] { "Blair", "James", "Paul" };
         Assert.Equal(expected, actual );
     }
 
     [Fact(Skip = "Remove to run test")]
-    public void Adding_students_to_different_grades_adds_them_to_the_roster()
+    public void Adding_students_to_different_grades_adds_them_to_the_same_sorted_roster()
     {
         var school = new School();
         school.Add("Chelsea", 3);
         school.Add("Logan", 7);
-        
-        var rosterThree = school.Roster(3);
-        var rosterSeven = school.Roster(7);
 
-        var expectedThree = new[] { "Chelsea" };
-        Assert.Equal(expectedThree, rosterThree);
-        
-        var expectedSeven = new[] { "Logan" };
-        Assert.Equal(expectedSeven, rosterSeven);
+        var actual = school.Roster();
+
+        var expected = new[] { "Chelsea", "Logan"};
+        Assert.Equal(expected, actual);
     }
 
     [Fact(Skip = "Remove to run test")]
@@ -70,7 +66,7 @@ public class GradeSchoolTest
     }
 
     [Fact(Skip = "Remove to run test")]
-    public void Student_names_in_each_grade_in_roster_are_sorted()
+    public void Student_names_with_grades_are_displayed_in_the_same_sorted_roster()
     {
         var school = new School();
         school.Add("Jennifer", 4);
@@ -78,17 +74,12 @@ public class GradeSchoolTest
         school.Add("Christopher", 4);
         school.Add("Kyle", 3);
               
-        var roster3 = school.Roster(3);
-        var roster4 = school.Roster(4);
-        var roster6 = school.Roster(6);
+        var actual = school.Roster();
+        
 
-        var expected3 = new[] { "Kyle" };
-        Assert.Equal(expected3, roster3);
+        var expected = new[] { "Christopher", "Jennifer", "Kareem", "Kyle" };
+        Assert.Equal(expected, actual);
 
-        var expected4 = new[] { "Christopher", "Jennifer" };
-        Assert.Equal(expected4, roster4);
-
-        var expected6 = new[] { "Kareem" };
-        Assert.Equal(expected6, roster6) ;
+        
     }
 }
