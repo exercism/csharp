3946fbc244938eab7af70de5f00e0abf2d5b2c4a
Erik Schierboom <erik_schierboom@hotmail.com>
2017-06-20 21:03:13 +0200

    Prefix Luhn property with "Is"

--- exercises/luhn/LuhnTest.cs
+++ exercises/luhn/LuhnTest.cs
@@ -7,78 +7,78 @@ public class LuhnTest
     [Fact]
     public void Single_digit_strings_can_not_be_valid()
     {
-        Assert.False(Luhn.Valid("1"));
+        Assert.False(Luhn.IsValid("1"));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void A_single_zero_is_invalid()
     {
-        Assert.False(Luhn.Valid("0"));
+        Assert.False(Luhn.IsValid("0"));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void A_simple_valid_sin_that_remains_valid_if_reversed()
     {
-        Assert.True(Luhn.Valid("059"));
+        Assert.True(Luhn.IsValid("059"));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void A_simple_valid_sin_that_becomes_invalid_if_reversed()
     {
-        Assert.True(Luhn.Valid("59"));
+        Assert.True(Luhn.IsValid("59"));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void A_valid_canadian_sin()
     {
-        Assert.True(Luhn.Valid("055 444 285"));
+        Assert.True(Luhn.IsValid("055 444 285"));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Invalid_canadian_sin()
     {
-        Assert.False(Luhn.Valid("055 444 286"));
+        Assert.False(Luhn.IsValid("055 444 286"));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Invalid_credit_card()
     {
-        Assert.False(Luhn.Valid("8273 1232 7352 0569"));
+        Assert.False(Luhn.IsValid("8273 1232 7352 0569"));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Valid_strings_with_a_non_digit_included_become_invalid()
     {
-        Assert.False(Luhn.Valid("055a 444 285"));
+        Assert.False(Luhn.IsValid("055a 444 285"));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Valid_strings_with_punctuation_included_become_invalid()
     {
-        Assert.False(Luhn.Valid("055-444-285"));
+        Assert.False(Luhn.IsValid("055-444-285"));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Valid_strings_with_symbols_included_become_invalid()
     {
-        Assert.False(Luhn.Valid("055£ 444$ 285"));
+        Assert.False(Luhn.IsValid("055£ 444$ 285"));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Single_zero_with_space_is_invalid()
     {
-        Assert.False(Luhn.Valid(" 0"));
+        Assert.False(Luhn.IsValid(" 0"));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void More_than_a_single_zero_is_valid()
     {
-        Assert.True(Luhn.Valid("0000 0"));
+        Assert.True(Luhn.IsValid("0000 0"));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Input_digit_9_is_correctly_converted_to_output_digit_9()
     {
-        Assert.True(Luhn.Valid("091"));
+        Assert.True(Luhn.IsValid("091"));
     }
 }
--- generators/Exercises/Luhn.cs
+++ generators/Exercises/Luhn.cs
@@ -2,5 +2,10 @@ namespace Generators.Exercises
 {
     public class Luhn : Exercise
     {
+        public Luhn()
+        {
+            foreach (var canonicalDataCase in CanonicalData.Cases)
+                canonicalDataCase.Property = "IsValid";
+        }
     }
 }
