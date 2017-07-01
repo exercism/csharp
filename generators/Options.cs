b7fc4fab85ecb869ab599630389374ad7445393f
Erik Schierboom <erik_schierboom@hotmail.com>
2017-07-01 20:35:10 +0200

    Parse command line options through CommandLine library

---
+++ generators/Options.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using CommandLine;
+
+namespace Generators
+{
+    public class Options
+    {
+        [Option('e', "exercises", Required = false, 
+            HelpText = "Exercises to generate (if not specified, defaults to all exercises).")]
+        public IEnumerable<string> Exercises { get; set; }
+    }
+}
