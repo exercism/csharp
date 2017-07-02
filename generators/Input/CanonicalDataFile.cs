f46dd850c1a58e05de7098ee831e7fae43de156c
Erik Schierboom <erik_schierboom@hotmail.com>
2017-07-02 11:45:18 +0200

    Use local git repository for canonical data files

---
+++ generators/Input/CanonicalDataFile.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Linq;
+using LibGit2Sharp;
+
+namespace Generators.Input
+{
+    public static class CanonicalDataFile
+    {
+        private const string CacheDir = "./.cache";
+        private const string ProblemSpecificationsGitUrl = "https://github.com/exercism/problem-specifications.git";
+        private const string RemoteName = "origin";
+
+        static CanonicalDataFile()
+        {
+            if (!Directory.Exists(CacheDir))
+                Repository.Clone(ProblemSpecificationsGitUrl, CacheDir);
+
+            using (var repository = new Repository(CacheDir))
+                Commands.Fetch(repository, RemoteName, Enumerable.Empty<string>(), new FetchOptions(), null);
+        }
+
+        public static string Contents(string exercise)
+        {
+            var exerciseCanonicalDataPath = Path.Combine(CacheDir, "exercises", exercise, "canonical-data.json");
+            return File.ReadAllText(exerciseCanonicalDataPath);
+        }
+    }
+}
