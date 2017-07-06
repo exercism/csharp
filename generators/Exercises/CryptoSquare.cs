d77e58b0e56da3a80dad753dc98c8c82d81af4c6
Erik Schierboom <erik_schierboom@hotmail.com>
2017-07-06 20:14:38 +0200

    Add crypto-square test generator

---
+++ generators/Exercises/CryptoSquare.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Generators.Exercises
+{
+    public class CryptoSquare : Exercise
+    {
+        public CryptoSquare()
+        {
+            foreach (var canonicalDataCase in CanonicalData.Cases)
+            {
+                canonicalDataCase.UseVariablesForInput = true;
+                canonicalDataCase.UseVariableForExpected = true;
+
+                if (canonicalDataCase.Expected is JArray expected)
+                    canonicalDataCase.Expected = expected.Values<string>();
+            }
+        }
+    }
+}
