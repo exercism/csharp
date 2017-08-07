ecf0081e2706626c3d120f283c5b85f72a20b3b0
John Reese <jpreese@mtu.edu>
2017-08-07 17:58:22 -0400

    Initial commit

---
+++ exercises/spiral-matrix/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpiralMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
