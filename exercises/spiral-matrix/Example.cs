d86ae6c253c79fbb37885a90e9d54053b50da9a0
John Reese <jpreese@mtu.edu>
2017-08-07 22:24:35 -0400

    Implement tests and example

---
+++ exercises/spiral-matrix/Example.cs
@@ -0,0 +1,42 @@
+﻿namespace SpiralMatrix
+{
+    public static class Example
+    {
+        public static int[,] GetMatrix(int size)
+        {
+            var spiral = new int[size, size];
+            int numbersToPlace = size * size;
+
+            int currentSpiralValue = 1;
+            int firstPivot = 0, secondPivot = size - 1;
+
+            while (currentSpiralValue <= numbersToPlace)
+            {
+                for (int i = firstPivot; i <= secondPivot; i++)
+                {
+                    spiral[firstPivot, i] = currentSpiralValue++;
+                }
+
+                for (int j = firstPivot + 1; j <= secondPivot; j++)
+                {
+                    spiral[j, secondPivot] = currentSpiralValue++;
+                }
+
+                for (int i = secondPivot - 1; i >= firstPivot; i--)
+                {
+                    spiral[secondPivot, i] = currentSpiralValue++;
+                }
+
+                for (int j = secondPivot - 1; j >= firstPivot + 1; j--)
+                {
+                    spiral[j, firstPivot] = currentSpiralValue++;
+                }
+
+                firstPivot++;
+                secondPivot--;
+            }
+
+            return spiral;
+
+        }
+}
