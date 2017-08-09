7ff43a6678c43077168ad3b5b58884f1e9d41524
John Reese <john@reesespieces.io>
2017-08-09 10:38:37 -0400

    Implement Collatz Conjecture (#329)
    * Initial commit
    
    * Implement Collatz Conjecture exercise
    
    * Add Collatz Conjecture to config
    
    * Make implementation and example classes static

---
+++ exercises/collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CollatzConjecture
+{
+    public static class CollatzConjecture
+    {
+        public static int GetSteps(int input)
+        {
+            throw new NotImplementedException("You need to implement this function");
+        }
+    }
+}
---
+++ exercises/collatz-conjecture/Example.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CollatzConjecture
+{
+    public static class CollatzConjecture
+    {
+        public static int GetSteps(int input)
+        {
+            if(input <= 0)
+            {
+                throw new ArgumentException("Only positive numbers are allowed");
+            }
+
+            int stepCount = 0;
+
+            while(input != 1)
+            {
+                if(input % 2 == 0)
+                {
+                    input /= 2;
+                }
+                else
+                {
+                    input = (input * 3) + 1;
+                }
+
+                stepCount++;
+            }
+
+            return stepCount;
+        }
+    }
+}
