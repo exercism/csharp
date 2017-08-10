a291d55e3dc852f3ded750720a32d8e74bd2269d
John Reese <jpreese@mtu.edu>
2017-08-10 10:09:00 -0400

    Remove namespace from exercises and test suite

--- exercises/diamond/Diamond.cs
+++ exercises/diamond/Diamond.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 
-namespace Diamond
+public static class Diamond
 {
-    public static class Diamond
+    public static string Make(char target)
     {
-        public static string Make(char target)
-        {
-            throw new NotImplementedException("Please implement this function");
-        }
+        throw new NotImplementedException("Please implement this function");
     }
 }
