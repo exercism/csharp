81588e673c86157bc12a076b07c43dd3952cd7ed
Keim, Rob (Agoda) <Rob.Keim@agoda.com>
2017-08-17 23:45:46 +0700

    Add test geneator for Hamming exercise

--- exercises/hamming/Hamming.cs
+++ exercises/hamming/Hamming.cs
@@ -2,7 +2,7 @@ using System;
 
 public static class Hamming
 {
-    public static int Compute(string firstStrand, string secondStrand)
+    public static int Distance(string firstStrand, string secondStrand)
     {
         throw new NotImplementedException("You need to implement this function.");
     }
