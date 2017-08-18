fb99c8e3086d5bcb29527986bda0c246267e337f
Rob Keim <robkeim@gmail.com>
2017-08-19 02:01:55 +0700

    Add test generator for Pangram exercise (#349)

---
+++ generators/Exercises/Pangram.cs
@@ -0,0 +1,17 @@
+﻿using Generators.Input;
+
+namespace Generators.Exercises
+{
+    public class Pangram : Exercise
+    {
+        protected override void UpdateCanonicalData(CanonicalData canonicalData)
+        {
+            foreach (var canonicalDataCase in CanonicalData.Cases)
+            {
+                canonicalDataCase.Description = canonicalDataCase.Description.Replace("'", string.Empty);
+                canonicalDataCase.UseVariablesForInput = true;
+                canonicalDataCase.Input["input"] = ((string)canonicalDataCase.Input["input"]).Replace("\"", "\\\"");
+            }
+        }
+    }
+}
