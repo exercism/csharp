bd66c0546245418d4af3a59742521aecac03bec8
John Reese <john@reesespieces.io>
2017-08-18 14:25:32 -0400

    Add TwoFer exercise test generator (#354)

--- exercises/two-fer/TwoFer.cs
+++ exercises/two-fer/TwoFer.cs
@@ -2,7 +2,7 @@
 
   public static class TwoFer
   {
-      public static string GetResponse(string input = null)
+      public static string Name(string input = null)
       {
           throw new NotImplementedException("You need to implement this function.");
       }
--- exercises/two-fer/TwoFerTest.cs
+++ exercises/two-fer/TwoFerTest.cs
@@ -1,24 +1,24 @@
-﻿// This file was auto-generated based on version 1.0.0 of the canonical data.
+﻿// This file was auto-generated based on version 1.1.0 of the canonical data.
 
 using Xunit;
 
-  public class TwoFerTest
-  {
-      [Fact]
-      public void No_name_given()
-      {
-          Assert.Equal("One for you, one for me.", TwoFer.GetResponse());
-      }
+public class TwoFerTest
+{
+    [Fact]
+    public void No_name_given()
+    {
+        Assert.Equal("One for you, one for me.", TwoFer.Name());
+    }
 
-      [Fact(Skip = "Remove to run test")]
-      public void Name_is_given()
-      {
-          Assert.Equal("One for Alice, one for me.", TwoFer.GetResponse("Alice"));
-      }
+    [Fact(Skip = "Remove to run test")]
+    public void A_name_given()
+    {
+        Assert.Equal("One for Alice, one for me.", TwoFer.Name("Alice"));
+    }
 
-      [Fact(Skip = "Remove to run test")]
-      public void Another_name_is_given()
-      {
-          Assert.Equal("One for Bob, one for me.", TwoFer.GetResponse("Bob"));
-      }
-  }
+    [Fact(Skip = "Remove to run test")]
+    public void Another_name_given()
+    {
+        Assert.Equal("One for Bob, one for me.", TwoFer.Name("Bob"));
+    }
+}
