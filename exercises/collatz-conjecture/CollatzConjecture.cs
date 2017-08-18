16d0ac7d767bb2d6288ddb879abcabb4ec6121d2
John Reese <john@reesespieces.io>
2017-08-18 02:31:01 -0400

    Add CollatzConjecture exercise test generator (#344)

--- exercises/collatz-conjecture/CollatzConjecture.cs
+++ exercises/collatz-conjecture/CollatzConjecture.cs
@@ -2,7 +2,7 @@
 
 public static class CollatzConjecture
 {
-    public static int GetSteps(int input)
+    public static int Steps(int number)
     {
         throw new NotImplementedException("You need to implement this function");
     }
