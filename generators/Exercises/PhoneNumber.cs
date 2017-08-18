14b1f01c89c68cd4b610f97c7ff41953956e686b
Keim, Rob (Agoda) <Rob.Keim@agoda.com>
2017-08-19 00:15:26 +0700

    Add generator for Phone Number exercise

---
+++ generators/Exercises/PhoneNumber.cs
@@ -0,0 +1,17 @@
+﻿using Generators.Input;
+using Generators.Output;
+
+namespace Generators.Exercises
+{
+    public class PhoneNumber : Exercise
+    {
+        protected override void UpdateCanonicalData(CanonicalData canonicalData)
+        {
+            foreach (var canonicalDataCase in CanonicalData.Cases)
+            {
+                canonicalDataCase.UseVariablesForInput = true;
+                canonicalDataCase.Expected = canonicalDataCase.Expected ?? new UnescapedValue("null");
+            }
+        }
+    }
+}
