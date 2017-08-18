6b9f7d965068e142a8d8c50ab88dda68bdbd1b17
Keim, Rob (Agoda) <Rob.Keim@agoda.com>
2017-08-18 23:15:19 +0700

    [WIP] Add Run Length Encoding test generator

---
+++ generators/Exercises/RunLengthEncoding.cs
@@ -0,0 +1,16 @@
+﻿using Generators.Input;
+
+namespace Generators.Exercises
+{
+    public class RunLengthEncoding : Exercise
+    {
+        protected override void UpdateCanonicalData(CanonicalData canonicalData)
+        {
+            foreach (var canonicalDataCase in CanonicalData.Cases)
+            {
+                // Prefix the test name with encode/decode because both functions are tested with the same cases
+                canonicalDataCase.Description = $"{canonicalDataCase.Property} {canonicalDataCase.Description}";
+            }
+        }
+    }
+}
