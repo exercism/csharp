54d2c1796cf1981a235b96422dc57cc55ab6d3ae
Rob Keim <robkeim@gmail.com>
2017-08-19 15:06:28 +0700

    Add test generator for rna transcription exercise (#353)
    * Add test generator for rna transcription exercise
    
    * Fix build error

--- exercises/rail-fence-cipher/RailFenceCipherTest.cs
+++ exercises/rail-fence-cipher/RailFenceCipherTest.cs
@@ -1,4 +1,4 @@
-﻿// This file was auto-generated based on version 1.0.1 of the canonical data.
+// This file was auto-generated based on version 1.0.1 of the canonical data.
 
 using Xunit;
 
--- exercises/rna-transcription/RnaTranscriptionTest.cs
+++ exercises/rna-transcription/RnaTranscriptionTest.cs
@@ -1,34 +1,54 @@
+// This file was auto-generated based on version 1.0.1 of the canonical data.
+
 using Xunit;
 
-public class ComplementTest
+public class RnaTranscriptionTest
 {
     [Fact]
     public void Rna_complement_of_cytosine_is_guanine()
     {
-        Assert.Equal("G", Complement.OfDna("C"));
+        Assert.Equal("G", RnaTranscription.ToRna("C"));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Rna_complement_of_guanine_is_cytosine()
     {
-        Assert.Equal("C", Complement.OfDna("G"));
+        Assert.Equal("C", RnaTranscription.ToRna("G"));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Rna_complement_of_thymine_is_adenine()
     {
-        Assert.Equal("A", Complement.OfDna("T"));
+        Assert.Equal("A", RnaTranscription.ToRna("T"));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Rna_complement_of_adenine_is_uracil()
     {
-        Assert.Equal("U", Complement.OfDna("A"));
+        Assert.Equal("U", RnaTranscription.ToRna("A"));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Rna_complement()
     {
-        Assert.Equal("UGCACCAGAAUU", Complement.OfDna("ACGTGGTCTTAA"));
+        Assert.Equal("UGCACCAGAAUU", RnaTranscription.ToRna("ACGTGGTCTTAA"));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Correctly_handles_invalid_input_rna_instead_of_dna_()
+    {
+        Assert.Null(RnaTranscription.ToRna("U"));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Correctly_handles_completely_invalid_dna_input()
+    {
+        Assert.Null(RnaTranscription.ToRna("XXX"));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Correctly_handles_partially_invalid_dna_input()
+    {
+        Assert.Null(RnaTranscription.ToRna("ACGTXXXCTTAA"));
     }
 }
