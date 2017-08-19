1715515e530e83c2801c105b903b5bcf57374a6b
Keim, Rob (Agoda) <Rob.Keim@agoda.com>
2017-08-19 15:05:49 +0700

    Add test generator for secret handshake exercise

---
+++ generators/Exercises/SecretHandshake.cs
@@ -0,0 +1,15 @@
+﻿using Generators.Input;
+
+namespace Generators.Exercises
+{
+    public class SecretHandshake : Exercise
+    {
+        protected override void UpdateCanonicalData(CanonicalData canonicalData)
+        {
+            foreach (var canonicalDataCase in CanonicalData.Cases)
+            {
+                canonicalDataCase.Expected = canonicalDataCase.Expected.ConvertToEnumerable<string>();
+            }
+        }
+    }
+}
