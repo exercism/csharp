6b7305349554ae5b432f716ec086d0cd9162f0b9
Rob Keim <robkeim@gmail.com>
2017-08-20 02:52:48 +0700

    Add test generator for space age exercise (#373)

---
+++ generators/Exercises/SpaceAge.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Generators.Input;
+
+namespace Generators.Exercises
+{
+    public class SpaceAge : Exercise
+    {
+        protected override void UpdateCanonicalData(CanonicalData canonicalData)
+        {
+            foreach (var canonicalDataCase in CanonicalData.Cases)
+            {
+                canonicalDataCase.TestedMethodType = TestedMethodType.Instance;
+
+                canonicalDataCase.ConstructorInput = new Dictionary<string, object>
+                {
+                    ["seconds"] = canonicalDataCase.Properties["seconds"]
+                };
+                canonicalDataCase.Input.Remove("seconds");
+
+                canonicalDataCase.Property = $"On_{canonicalDataCase.Input["planet"]}";
+                canonicalDataCase.Input.Remove("planet");
+            }
+        }
+    }
+}
