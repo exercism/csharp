a31198b2ec3abbdfbb08d2d2ef76bb45df09f80b
Rob Keim <robkeim@gmail.com>
2017-08-20 02:29:28 +0700

    Add test generator for say exercise (#362)
    * Add test generator for say exercise
    
    * Update error cases to throw exceptions

--- exercises/say/SayTest.cs
+++ exercises/say/SayTest.cs
@@ -1,101 +1,97 @@
-﻿using System;
+// This file was auto-generated based on version 1.0.0 of the canonical data.
+
 using Xunit;
+using System;
 
 public class SayTest
 {
     [Fact]
     public void Zero()
     {
-        Assert.Equal("zero", Say.InEnglish(0L));
+        Assert.Equal("zero", Say.InEnglish(0));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void One()
     {
-        Assert.Equal("one", Say.InEnglish(1L));
+        Assert.Equal("one", Say.InEnglish(1));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Fourteen()
     {
-        Assert.Equal("fourteen", Say.InEnglish(14L));
+        Assert.Equal("fourteen", Say.InEnglish(14));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Twenty()
     {
-        Assert.Equal("twenty", Say.InEnglish(20L));
+        Assert.Equal("twenty", Say.InEnglish(20));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Twenty_two()
     {
-        Assert.Equal("twenty-two", Say.InEnglish(22L));
+        Assert.Equal("twenty-two", Say.InEnglish(22));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void One_hundred()
     {
-        Assert.Equal("one hundred", Say.InEnglish(100L));
+        Assert.Equal("one hundred", Say.InEnglish(100));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void One_hundred_twenty_three()
     {
-        Assert.Equal("one hundred twenty-three", Say.InEnglish(123L));
+        Assert.Equal("one hundred twenty-three", Say.InEnglish(123));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void One_thousand()
     {
-        Assert.Equal("one thousand", Say.InEnglish(1000L));
+        Assert.Equal("one thousand", Say.InEnglish(1000));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void One_thousand_two_hundred_thirty_four()
     {
-        Assert.Equal("one thousand two hundred thirty-four", Say.InEnglish(1234L));
+        Assert.Equal("one thousand two hundred thirty-four", Say.InEnglish(1234));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void One_million()
     {
-        Assert.Equal("one million", Say.InEnglish(1000000L));
-    }
-
-    [Fact(Skip = "Remove to run test")]
-    public void One_million_two()
-    {
-        Assert.Equal("one million two", Say.InEnglish(1000002L));
+        Assert.Equal("one million", Say.InEnglish(1000000));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void One_million_two_thousand_three_hundred_forty_five()
     {
-        Assert.Equal("one million two thousand three hundred forty-five", Say.InEnglish(1002345L));
+        Assert.Equal("one million two thousand three hundred forty-five", Say.InEnglish(1002345));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void One_billion()
     {
-        Assert.Equal("one billion", Say.InEnglish(1000000000L));
+        Assert.Equal("one billion", Say.InEnglish(1000000000));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void A_big_number()
     {
-        Assert.Equal("nine hundred eighty-seven billion six hundred fifty-four million three hundred twenty-one thousand one hundred twenty-three", Say.InEnglish(987654321123L));
+        Assert.Equal("nine hundred eighty-seven billion six hundred fifty-four million three hundred twenty-one thousand one hundred twenty-three", Say.InEnglish(987654321123));
     }
 
     [Fact(Skip = "Remove to run test")]
-    public void Lower_bound()
+    public void Numbers_below_zero_are_out_of_range()
     {
-        Assert.Throws<ArgumentOutOfRangeException>(() => Say.InEnglish(-1L));
+        Assert.Throws<ArgumentOutOfRangeException>(() => Say.InEnglish(-1));
     }
 
     [Fact(Skip = "Remove to run test")]
-    public void Upper_bound()
+    public void Numbers_above_999_999_999_999_are_out_of_range()
     {
-        Assert.Throws<ArgumentOutOfRangeException>(() => Say.InEnglish(1000000000000L));
+        Assert.Throws<ArgumentOutOfRangeException>(() => Say.InEnglish(1000000000000));
     }
 }
---
+++ generators/Exercises/Say.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Generators.Input;
+
+namespace Generators.Exercises
+{
+    public class Say : Exercise
+    {
+        protected override void UpdateCanonicalData(CanonicalData canonicalData)
+        {
+            foreach (var canonicalDataCase in CanonicalData.Cases)
+            {
+                // Rename the property to avoid it being considered a constructor
+                if (canonicalDataCase.Property == "say")
+                {
+                    canonicalDataCase.Property = "in_english";
+                }
+
+                canonicalDataCase.ExceptionThrown = canonicalDataCase.Expected is long ? typeof(ArgumentOutOfRangeException) : null;
+            }
+        }
+    }
+}
