d6e23797ff239052426480611ae711a161c22dda
John Reese <jpreese@mtu.edu>
2017-08-22 09:41:34 -0400

    Simplify Pangram test generator

--- exercises/pangram/PangramTest.cs
+++ exercises/pangram/PangramTest.cs
@@ -7,63 +7,54 @@ public class PangramTest
     [Fact]
     public void Sentence_empty()
     {
-        var input = "";
-        Assert.False(Pangram.IsPangram(input));
+        Assert.False(Pangram.IsPangram(""));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Pangram_with_only_lower_case()
     {
-        var input = "the quick brown fox jumps over the lazy dog";
-        Assert.True(Pangram.IsPangram(input));
+        Assert.True(Pangram.IsPangram("the quick brown fox jumps over the lazy dog"));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Missing_character_x()
     {
-        var input = "a quick movement of the enemy will jeopardize five gunboats";
-        Assert.False(Pangram.IsPangram(input));
+        Assert.False(Pangram.IsPangram("a quick movement of the enemy will jeopardize five gunboats"));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Another_missing_character_x()
     {
-        var input = "the quick brown fish jumps over the lazy dog";
-        Assert.False(Pangram.IsPangram(input));
+        Assert.False(Pangram.IsPangram("the quick brown fish jumps over the lazy dog"));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Pangram_with_underscores()
     {
-        var input = "the_quick_brown_fox_jumps_over_the_lazy_dog";
-        Assert.True(Pangram.IsPangram(input));
+        Assert.True(Pangram.IsPangram("the_quick_brown_fox_jumps_over_the_lazy_dog"));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Pangram_with_numbers()
     {
-        var input = "the 1 quick brown fox jumps over the 2 lazy dogs";
-        Assert.True(Pangram.IsPangram(input));
+        Assert.True(Pangram.IsPangram("the 1 quick brown fox jumps over the 2 lazy dogs"));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Missing_letters_replaced_by_numbers()
     {
-        var input = "7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog";
-        Assert.False(Pangram.IsPangram(input));
+        Assert.False(Pangram.IsPangram("7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog"));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Pangram_with_mixed_case_and_punctuation()
     {
-        var input = "\"Five quacking Zephyrs jolt my wax bed.\"";
-        Assert.True(Pangram.IsPangram(input));
+        Assert.True(Pangram.IsPangram("\"Five quacking Zephyrs jolt my wax bed.\""));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Upper_and_lower_case_versions_of_the_same_character_should_not_be_counted_separately()
     {
-        var input = "the quick brown fox jumps over with lazy FX";
-        Assert.False(Pangram.IsPangram(input));
+        Assert.False(Pangram.IsPangram("the quick brown fox jumps over with lazy FX"));
     }
 }
--- generators/Exercises/Pangram.cs
+++ generators/Exercises/Pangram.cs
@@ -1,16 +1,6 @@
-﻿using Generators.Input;
-
-namespace Generators.Exercises
+﻿namespace Generators.Exercises
 {
     public class Pangram : Exercise
     {
-        protected override void UpdateCanonicalData(CanonicalData canonicalData)
-        {
-            foreach (var canonicalDataCase in CanonicalData.Cases)
-            {
-                canonicalDataCase.UseVariablesForInput = true;
-                canonicalDataCase.Input["input"] = ((string)canonicalDataCase.Input["input"]).Replace("\"", "\\\"");
-            }
-        }
     }
 }
