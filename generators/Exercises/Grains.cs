9da0e03338896bb396b5c11659f6deecd4192274
Keim, Rob (Agoda) <Rob.Keim@agoda.com>
2017-08-31 22:12:51 +0700

    Add test generator for grains exercise

---
+++ generators/Exercises/Grains.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Generators.Input;
+using Generators.Output;
+
+namespace Generators.Exercises
+{
+    public class Grains : Exercise
+    {
+        protected override void UpdateCanonicalData(CanonicalData canonicalData)
+        {
+            foreach (var canonicalDataCase in canonicalData.Cases)
+            {
+                if (ShouldThrowException(canonicalDataCase.Expected))
+                {
+                    canonicalDataCase.ExceptionThrown = typeof(ArgumentOutOfRangeException);
+                }
+                else
+                {
+                    canonicalDataCase.Expected =  new UnescapedValue($"{canonicalDataCase.Expected}UL");
+                }
+            }
+        }
+
+        private static bool ShouldThrowException(object value)
+        {
+            return int.TryParse(value.ToString(), out int result)
+                && result == -1;
+        }
+    }
+}
