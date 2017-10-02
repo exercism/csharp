3a6b4ba765319e9a168a011d5907197f82bdcab1
Grzegorz Kotfis <gkotfis@outlook.com>
2017-10-02 22:56:46 +0200

    Modify example to cover: - error cases: invalid lines/columns numbers - multiline input joined by commas

--- exercises/ocr-numbers/Example.cs
+++ exercises/ocr-numbers/Example.cs
@@ -10,8 +10,39 @@ public static class OcrNumbers
     public static string Convert(string input)
     {
         var lines = input.Split('\n');
-        
+
+        if (lines.Length > CharacterHeight)
+            lines = GetTransformedInput(lines);
+
         return Positions(lines).Aggregate("", (str, pos) => str + ConvertCharacter(lines, pos.Item1, pos.Item2));
+    }
+
+    /// <summary>
+    /// Transform multilines input into one line digits separate by comma
+    /// </summary>
+    /// <param name="lines"></param>
+    /// <returns></returns>
+    private static string[] GetTransformedInput(string[] lines)
+    {
+        string[] transformedLines = new string[CharacterHeight];
+
+        for (int x = 0, characterRow = 0; x < lines.Length; x++, characterRow++)
+        {
+            if (characterRow > 3)
+                characterRow = 0;
+
+            for (int y = 0; y < lines[x].Length; y++)
+                transformedLines[characterRow] += lines[x][y];
+            
+            //for empty & last line add comma
+            if (string.IsNullOrWhiteSpace(lines[x]) && lines.Length - 1 != x)
+            {
+                for (int i = 0; i < CharacterHeight; i++)
+                    transformedLines[i] += i == CharacterWidth - 1 ? "  ," : "   ";
+            }
+        }
+
+        return transformedLines;
     }
 
     private static IEnumerable<Tuple<int, int>> Positions(string[] lines)
@@ -21,10 +52,25 @@ public static class OcrNumbers
                select Tuple.Create(x, y);
     }
 
-    private static int Cols(string[] lines) => lines[0].Length / CharacterWidth;
+    private static int Cols(string[] lines)
+    {
+        return lines[0].Length % CharacterWidth == 0
+            ? lines[0].Length / CharacterWidth
+            : throw new ArgumentException();
+    }
 
-    private static int Rows(string[] lines) => lines.Length / CharacterHeight;
-    
+    private static int Rows(string[] lines)
+    {
+        return lines.Length % CharacterHeight == 0
+            ? lines.Length / CharacterHeight
+            : throw new ArgumentException();
+    }
+
+    private static bool IsEmptyLine(string line)
+    {
+        return string.IsNullOrWhiteSpace(line);
+    }
+
     private static char ConvertCharacter(string[] input, int row, int col) => MatchCharacter(Character(input, row, col));
 
     private static string Character(string[] input, int row, int col)
@@ -41,9 +87,9 @@ public static class OcrNumbers
     private static readonly IReadOnlyDictionary<string, char> CharactersMap = new Dictionary<string, char>
     {
         {
-            " _ " + 
-            "| |" + 
-            "|_|" + 
+            " _ " +
+            "| |" +
+            "|_|" +
             "   ",
             '0'
         },
@@ -109,6 +155,13 @@ public static class OcrNumbers
             " _|" +
             "   ",
             '9'
+        },
+        {
+            "   " +
+            "   " +
+            "  ," +
+            "   ",
+            ','
         }
     };
 }
