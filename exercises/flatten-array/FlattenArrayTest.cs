a4634210f169b476a1fb18cd229a668eb9a92885
felix91gr <felix91gr@gmail.com>
2017-10-11 21:54:41 -0300

    Made tests correctly formatted.

--- exercises/flatten-array/FlattenArrayTest.cs
+++ exercises/flatten-array/FlattenArrayTest.cs
@@ -7,36 +7,140 @@ public class FlattenArrayTest
     [Fact]
     public void No_nesting()
     {
-        Assert.Equal(new[] { 0, 1, 2 }, FlattenArray.Flatten(new[] { 0, 1, 2 }));
+        var input = new[] { 0, 1, 2 };
+        var expected = new[] { 0, 1, 2 };
+        Assert.Equal(expected, FlattenArray.Flatten(input));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Flattens_array_with_just_integers_present()
     {
-        Assert.Equal(new[] { 1, 2, 3, 4, 5, 6, 7, 8 }, FlattenArray.Flatten(new[] { 1, new[] { 2, 3, 4, 5, 6, 7 }, 8 }));
+        var input = new object[] {
+          1,
+          new object[] {
+            2,
+            3,
+            4,
+            5,
+            6,
+            7
+          },
+          8
+        };
+        var expected = new[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+        Assert.Equal(expected, FlattenArray.Flatten(input));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Number_5_level_nesting()
     {
-        Assert.Equal(new[] { 0, 2, 2, 3, 8, 100, 4, 50, -2 }, FlattenArray.Flatten(new[] { 0, 2, new[] { new[] { 2, 3 }, 8, 100, 4, new[] { new[] { new[] { 50 } } } }, -2 }));
+        var input = new object[] {
+          0,
+          2,
+          new object[] {
+            new object[] {
+              2,
+              3
+            },
+            8,
+            100,
+            4,
+            new object[] {
+              new object[] {
+                new object[] {
+                  50
+                }
+              }
+            }
+          },
+          -2
+        };
+        var expected = new[] { 0, 2, 2, 3, 8, 100, 4, 50, -2 };
+        Assert.Equal(expected, FlattenArray.Flatten(input));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Number_6_level_nesting()
     {
-        Assert.Equal(new[] { 1, 2, 3, 4, 5, 6, 7, 8 }, FlattenArray.Flatten(new[] { 1, new[] { 2, new[] { new[] { 3 } }, new[] { 4, new[] { new[] { 5 } } }, 6, 7 }, 8 }));
+        var input = new object[] {
+          1,
+          new object[] {
+            2,
+            new object[] {
+              new object[] {
+                3
+              }
+            },
+            new object[] {
+              4,
+              new object[] {
+                new object[] {
+                  5
+                }
+              }
+            },
+            6,
+            7
+          },
+          8
+        };
+        var expected = new[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+        Assert.Equal(expected, FlattenArray.Flatten(input));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Number_6_level_nest_list_with_null_values()
     {
-        Assert.Equal(new[] { 0, 2, 2, 3, 8, 100, -2 }, FlattenArray.Flatten(new[] { 0, 2, new[] { new[] { 2, 3 }, 8, new[] { new[] { 100 } }, , new[] { new[] {  } } }, -2 }));
+        var input = new object[] {
+          0,
+          2,
+          new object[] {
+            new object[] {
+              2,
+              3
+            },
+            8,
+            new object[] {
+              new object[] {
+                100
+              }
+            },
+            null,
+            new object[] {
+              new object[] {
+                null
+              }
+            }
+          },
+          -2
+        };
+        var expected = new[] { 0, 2, 2, 3, 8, 100, -2 };
+        Assert.Equal(expected, FlattenArray.Flatten(input));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void All_values_in_nested_list_are_null()
     {
-        Assert.Empty(FlattenArray.Flatten(new[] { , new[] { new[] { new[] {  } } }, , , new[] { new[] { ,  },  },  }));
+        var input = new object[] {
+          null,
+          new object[] {
+            new object[] {
+              new object[] {
+                null
+              }
+            }
+          },
+          null,
+          null,
+          new object[] {
+            new object[] {
+              null,
+              null
+            },
+            null
+          },
+          null
+        };
+        Assert.Empty(FlattenArray.Flatten(input));
     }
 }
--- generators/Exercises/FlattenArray.cs
+++ generators/Exercises/FlattenArray.cs
@@ -1,4 +1,6 @@
 using Generators.Input;
+using Generators.Output;
+using System.Collections;
 
 namespace Generators.Exercises
 {
@@ -6,6 +8,29 @@ namespace Generators.Exercises
     {
     	protected override void UpdateCanonicalData(CanonicalData canonicalData)
         {
+            foreach (var canonicalDataCase in canonicalData.Cases)
+            {
+                canonicalDataCase.UseVariablesForInput = true;
+                canonicalDataCase.UseVariableForExpected = true;
+
+                string stringInput = canonicalDataCase.Properties["input"].ToString();
+
+                // We skip reformatting of pure int arrays.
+                if(stringInput.Contains("System.Int32"))
+                    continue;
+
+                string properInput = ToProperObjArray(stringInput);
+
+                canonicalDataCase.Properties["input"] = new UnescapedValue(properInput);
+            }
+        }
+
+        private string ToProperObjArray(string input) 
+        {
+            string res = input.Replace("System.Int32", "");
+            res = res.Replace("]", "}");
+            res = res.Replace("[", "new object[] {");
+            return res;
         }
     }
 }
