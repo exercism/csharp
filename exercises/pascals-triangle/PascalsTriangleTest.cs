ed933e2c8e1b46aa1970a82efeecea09e3fb2aa8
Rob Keim <rob.keim@agoda.com>
2017-11-12 19:22:22 +0700

    Update Pascal's Triangle exercise to latest canonical data

--- exercises/pascals-triangle/PascalsTriangleTest.cs
+++ exercises/pascals-triangle/PascalsTriangleTest.cs
@@ -1,4 +1,4 @@
-// This file was auto-generated based on version 1.0.0 of the canonical data.
+// This file was auto-generated based on version 1.2.0 of the canonical data.
 
 using Xunit;
 using System;
@@ -37,6 +37,27 @@ public class PascalsTriangleTest
     {
         var expected = new[] { new[] { 1 }, new[] { 1, 1 }, new[] { 1, 2, 1 }, new[] { 1, 3, 3, 1 } };
         Assert.Equal(expected, PascalsTriangle.Calculate(4));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Five_rows()
+    {
+        var expected = new[] { new[] { 1 }, new[] { 1, 1 }, new[] { 1, 2, 1 }, new[] { 1, 3, 3, 1 }, new[] { 1, 4, 6, 4, 1 } };
+        Assert.Equal(expected, PascalsTriangle.Calculate(5));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Six_rows()
+    {
+        var expected = new[] { new[] { 1 }, new[] { 1, 1 }, new[] { 1, 2, 1 }, new[] { 1, 3, 3, 1 }, new[] { 1, 4, 6, 4, 1 }, new[] { 1, 5, 10, 10, 5, 1 } };
+        Assert.Equal(expected, PascalsTriangle.Calculate(6));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Ten_rows()
+    {
+        var expected = new[] { new[] { 1 }, new[] { 1, 1 }, new[] { 1, 2, 1 }, new[] { 1, 3, 3, 1 }, new[] { 1, 4, 6, 4, 1 }, new[] { 1, 5, 10, 10, 5, 1 }, new[] { 1, 6, 15, 20, 15, 6, 1 }, new[] { 1, 7, 21, 35, 35, 21, 7, 1 }, new[] { 1, 8, 28, 56, 70, 56, 28, 8, 1 }, new[] { 1, 9, 36, 84, 126, 126, 84, 36, 9, 1 } };
+        Assert.Equal(expected, PascalsTriangle.Calculate(10));
     }
 
     [Fact(Skip = "Remove to run test")]
