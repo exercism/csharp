88a5ca64ced124959f22c2641e9d6c54e2a10848
Rob Keim <robkeim@gmail.com>
2017-11-13 14:38:47 +0700

    Add reverse string exercise (#490)

---
+++ exercises/reverse-string/Example.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        return new string(input.ToCharArray().Reverse().ToArray());
+    }
+}
