17cff6b2ea9b10754d908fc4397a0d0bcd1d098b
Erik Schierboom <erik_schierboom@hotmail.com>
2017-11-13 16:18:56 +0100

    Add comment on why the Diamond exercise inherits from CustomExercise

--- generators/Exercises/Diamond.cs
+++ generators/Exercises/Diamond.cs
@@ -1,5 +1,8 @@
 ﻿namespace Generators.Exercises
 {
+    // This class derives from CustomExercise instead of GeneratorExercise
+    // as it has custom (property-based) tests that cannot be generated from
+    // the canonical data.
     public class Diamond : CustomExercise
     {
     }
