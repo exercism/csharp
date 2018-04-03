8c1b6bc1cbdcc0fd1c2bd268c05a2ae1e5b14173
ShamilS <Shamil.Salakhetdinov@gmail.com>
2018-04-03 18:54:42 +0300

    'Yacht' problem solution PR, revision #1

--- exercises/yacht/Yacht.cs
+++ exercises/yacht/Yacht.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 public static class Yacht
 {
-    public static int Score(int[] diceSet, string category)
+    public static int Score(int[] dice, string category)
     {
         throw new NotImplementedException("You need to implement this function.");
     }
--- generators/Exercises/Yacht.cs
+++ generators/Exercises/Yacht.cs
@@ -5,8 +5,5 @@ namespace Generators.Exercises
 {
     public class Yacht : GeneratorExercise
     {
-        protected override void UpdateCanonicalData(CanonicalData canonicalData)
-        {
-        }
     }
 }
