17f59eae373168f6b5ed42691f8713e02c0fb152
ShamilS <Shamil.Salakhetdinov@gmail.com>
2018-04-04 22:42:13 +0300

    Rational Numbers, v.2, still Math.Pow is used in one method

--- exercises/rational-numbers/Example.cs
+++ exercises/rational-numbers/Example.cs
@@ -4,7 +4,7 @@ public static class RationalNumbers
 {
     public static int[] Add(int[] firstAddendum, int[] secondAddendum)
     {
-        var commonDenominator = nok(Math.Abs(firstAddendum[1]), Math.Abs(secondAddendum[1]));
+        var commonDenominator = nok(absValue(firstAddendum[1]), absValue(secondAddendum[1]));
         var numerator1 = firstAddendum[0] * commonDenominator / firstAddendum[1];
         var numerator2 = secondAddendum[0] * commonDenominator / secondAddendum[1];
         return Reduce(new int[]
@@ -14,7 +14,7 @@ public static class RationalNumbers
     }
     public static int[] Sub(int[] decrementable, int[] subtractor)
     {
-        var commonDenominator = nok(Math.Abs(decrementable[1]), Math.Abs(subtractor[1]));
+        var commonDenominator = nok(absValue(decrementable[1]), absValue(subtractor[1]));
         var numerator1 = decrementable[0] * commonDenominator / decrementable[1];
         var numerator2 = subtractor[0] * commonDenominator / subtractor[1];
         return Reduce(new int[]
@@ -25,27 +25,27 @@ public static class RationalNumbers
     public static int[] Mul(int[] multiplicand, int[] multiplier)
     {
         if (multiplier[0] == 0) return new int[] { 0, 1};
-        var sign = Math.Sign(multiplicand[0]) / Math.Sign(multiplicand[1]) /
-                   Math.Sign(multiplier[0]) / Math.Sign(multiplier[1]);
+        var sign = getSign(multiplicand[0]) / getSign(multiplicand[1]) /
+                   getSign(multiplier[0]) / getSign(multiplier[1]);
 
         return Reduce(new int[]
             {
-                sign * Math.Abs( multiplicand[0]*multiplier[0]), Math.Abs(multiplicand[1]*multiplier[1])
+                sign * absValue( multiplicand[0]*multiplier[0]), absValue(multiplicand[1]*multiplier[1])
             });
     }
     public static int[] Div(int[] divisible, int[] divider)
     {
-        var sign = Math.Sign(divisible[0]) / Math.Sign(divisible[1]) /
-                   Math.Sign(divider[0]) / Math.Sign(divider[1]);
+        var sign = getSign(divisible[0]) / getSign(divisible[1]) /
+                   getSign(divider[0]) / getSign(divider[1]);
 
         return Reduce(new int[]
             {
-                sign * Math.Abs( divisible[0]*divider[1]), Math.Abs(divisible[1]*divider[0])
+                sign * absValue( divisible[0]*divider[1]), absValue(divisible[1]*divider[0])
             });
     }
     public static int[] Abs(int[] value)
     {
-        return new[] { Math.Abs(value[0]), Math.Abs(value[1])};
+        return new[] { absValue(value[0]), absValue(value[1])};
     }
 
     public static int[] Reduce(int[] properFraction)
@@ -53,10 +53,10 @@ public static class RationalNumbers
         if (properFraction[1] == 0) return properFraction;
         else if (properFraction[0] == 0) return new int[] { 0, 1};
 
-        var a = Math.Abs(properFraction[0]);
-        var b = Math.Abs(properFraction[1]);
+        var a = absValue(properFraction[0]);
+        var b = absValue(properFraction[1]);
 
-        var sign = Math.Sign(properFraction[0]) * Math.Sign(properFraction[1]);
+        var sign = getSign(properFraction[0]) * getSign(properFraction[1]);
 
         return new int[] { sign * a / nod(a, b), b / nod(a,b) };
     }
@@ -65,12 +65,12 @@ public static class RationalNumbers
     {
         if (power == 0) return new int[] { 1, 1 };
 
-        var sign = Math.Sign(baseNumber[0]) / Math.Sign(baseNumber[1]);
+        var sign = getSign(baseNumber[0]) / getSign(baseNumber[1]);
         return Reduce(new int[]
             {
                 sign *
-                Math.Abs(myPow(baseNumber[0], power)),
-                Math.Abs(myPow(baseNumber[1], power))
+                absValue(pow(baseNumber[0], power)),
+                absValue(pow(baseNumber[1], power))
             });
     }
 
@@ -78,23 +78,13 @@ public static class RationalNumbers
     {
         if (power[0] == 0) return 1;
         else
-            return myPow2(baseNumber, power);
-    }
-
-    private static int myPow(int baseNumber, int power)
-    {
-        var result = 1;
-        for (int i = 1; i <= power; i++)
-        {
-            result *= baseNumber;
-        }
-        return result;
+            return pow2(baseNumber, power);
     }
 
     private static int nod(int a, int b)
     {
-        var x = Math.Abs(a);
-        var y = Math.Abs(b);
+        var x = absValue(a);
+        var y = absValue(b);
 
         while (a != b)
             if (a > b) a -= b; else b -= a;
@@ -106,11 +96,31 @@ public static class RationalNumbers
         return a * (b / nod(a, b));
     }
 
-    private static double myPow2(int baseNumber, int[] power)
+    private static int absValue(int value)
+    {
+        return (value > 0) ? value : -value;
+    }
+
+    private static int getSign(int value)
+    {
+        return (value > 0) ? 1 : -1;
+    }
+
+    private static int pow(int baseNumber, int power)
+    {
+        var result = 1;
+        for (int i = 1; i <= power; i++)
+        {
+            result *= baseNumber;
+        }
+        return result;
+    }
+
+    private static double pow2(int baseNumber, int[] power)
     {
         if (power[0] > 0 && power[1] > 0)
         {
-            var tempResult = myPow(baseNumber, power[0]);
+            var tempResult = pow(baseNumber, power[0]);
             return Math.Pow((double)tempResult, 1 / (double)power[1]);
         }
         else return Math.Pow((double)baseNumber, (double)power[0] / (double)power[1]);
