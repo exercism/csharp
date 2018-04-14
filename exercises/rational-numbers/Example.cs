81c339c2e3007922eb2e51d4fbec3bd9361fe68c
ShamilS <Shamil.Salakhetdinov@gmail.com>
2018-04-15 01:07:08 +0300

    Rational Numbers v.1.2

--- exercises/rational-numbers/Example.cs
+++ exercises/rational-numbers/Example.cs
@@ -1,106 +1,125 @@
 using System;
+using System.Diagnostics;
 
-public static class RationalNumbers
+public static class RealNumberExtension
 {
-    private const int NUMERATOR_INDEX = 0;
-    private const int DENOMINATOR_INDEX = 1;
-
-    #region Main code: Add, Sub, Mul, Div, Abs, Reduce, Exprational, Expreal
-    public static int[] Add(int[] r1, int[] r2)
+    // exponentiate real number to the rational number power
+    public static double Expreal(this int realNumber, RationalNumber r)
     {
-        return Reduce(new int[] { r1[NUMERATOR_INDEX] * r2[DENOMINATOR_INDEX] + r1[DENOMINATOR_INDEX] * r2[NUMERATOR_INDEX], r1[DENOMINATOR_INDEX] * r2[DENOMINATOR_INDEX] });
+        return r.Expreal(realNumber);
+    }
+}
+
+[DebuggerDisplay("{Numerator} / {Denominator}")]
+public struct RationalNumber
+{
+    public RationalNumber(int numerator, int denominator)
+    {
+        this.Numerator = numerator;
+        this.Denominator = denominator;
     }
 
-    public static int[] Sub(int[] r1, int[] r2)
+    public int Numerator { get; }
+    public int Denominator { get; }
+
+    public RationalNumber Add(RationalNumber r)
     {
-        return Reduce(new int[] { r1[NUMERATOR_INDEX] * r2[DENOMINATOR_INDEX] - r1[DENOMINATOR_INDEX] * r2[NUMERATOR_INDEX], r1[DENOMINATOR_INDEX] * r2[DENOMINATOR_INDEX] });
+        return ReducedRationalNumber(this.Numerator * r.Denominator + this.Denominator * r.Numerator, this.Denominator * r.Denominator);
     }
 
-    public static int[] Mul(int[] r1, int[] r2)
+    public RationalNumber Sub(RationalNumber r)
     {
-        if (r1[NUMERATOR_INDEX] == 0) return new int[] { 0, 1 };
-        return Reduce(new int[] { r1[NUMERATOR_INDEX] * r2[NUMERATOR_INDEX], r1[DENOMINATOR_INDEX] * r2[DENOMINATOR_INDEX] });
+        return ReducedRationalNumber(this.Numerator * r.Denominator - this.Denominator * r.Numerator, this.Denominator * r.Denominator);
     }
 
-    public static int[] Div(int[] r1, int[] r2)
+    public RationalNumber Mul(RationalNumber r)
     {
-        return Reduce(new int[] { r1[NUMERATOR_INDEX] * r2[DENOMINATOR_INDEX], r1[DENOMINATOR_INDEX] * r2[NUMERATOR_INDEX] });
+        if (this.Numerator == 0) return new RationalNumber(0, 1);
+        return ReducedRationalNumber(this.Numerator * r.Numerator, this.Denominator * r.Denominator);
     }
 
-    public static int[] Abs(int[] value)
+    public RationalNumber Div(RationalNumber r)
     {
-        return new[] { getAbsValue(value[NUMERATOR_INDEX]), getAbsValue(value[DENOMINATOR_INDEX]) };
+        return ReducedRationalNumber(this.Numerator * r.Denominator, this.Denominator * r.Numerator);
     }
 
-    public static int[] Reduce(int[] properFraction)
+    public RationalNumber Abs()
     {
-        if (properFraction[DENOMINATOR_INDEX] == 0) return properFraction;
-        else if (properFraction[NUMERATOR_INDEX] == 0) return new int[] { 0, 1 };
-
-        var a = getAbsValue(properFraction[NUMERATOR_INDEX]);
-        var b = getAbsValue(properFraction[DENOMINATOR_INDEX]);
-
-        var sign = getSign(properFraction[NUMERATOR_INDEX]) * getSign(properFraction[DENOMINATOR_INDEX]);
-
-        return new int[] { sign * a / greatestCommonDivisor(a, b), b / greatestCommonDivisor(a, b) };
+        return new RationalNumber(Abs(this.Numerator), Abs(this.Denominator));
     }
 
-    public static int[] Exprational(int[] baseNumber, int power)
+    public RationalNumber Reduce()
     {
-        if (power == 0) return new int[] { 1, 1 };
+        if (this.Denominator == 0) return new RationalNumber(this.Numerator, this.Denominator);
+        else if (this.Numerator == 0) return new RationalNumber(0, 1);
 
-        var sign = getSign(baseNumber[NUMERATOR_INDEX]) / getSign(baseNumber[DENOMINATOR_INDEX]);
-        return Reduce(new int[]
-            {
-                sign *
-                getAbsValue((int)pow(baseNumber[NUMERATOR_INDEX], power)),
-                getAbsValue((int)pow(baseNumber[DENOMINATOR_INDEX], power))
-            });
+        var a = Abs(this.Numerator);
+        var b = Abs(this.Denominator);
+
+        var sign = Sign(this.Numerator) * Sign(this.Denominator);
+
+        return new RationalNumber(sign * a / GreatestCommonDenominator(a, b), b / GreatestCommonDenominator(a, b));
     }
 
-    public static double Expreal(int baseNumber, int[] power)
+    public RationalNumber Exprational(int power)
     {
-        if (power[NUMERATOR_INDEX] == 0) return 1;
+        if (power == 0) return new RationalNumber(1, 1);
+
+        return ReducedRationalNumber
+            (
+                (Sign(this.Numerator) / Sign(this.Denominator)) *
+                Abs((int)Pow(this.Numerator, power)),
+                Abs((int)Pow(this.Denominator, power))
+            );
+    }
+
+    public double Expreal(int baseNumber)
+    {
+        if (this.Numerator == 0)
+            return 1;
         else
-            return pow(baseNumber, power);
+            return Pow(baseNumber);
     }
-    #endregion
 
-    #region Utilities
-    private static double pow(int baseNumber, int[] power)
+    private RationalNumber ReducedRationalNumber(int numerator, int denominator)
     {
-        return nthRoot((getSign(power[NUMERATOR_INDEX]) == getSign(power[DENOMINATOR_INDEX]) ?
-                        pow(baseNumber, getAbsValue(power[NUMERATOR_INDEX])) :
-                        1d / pow(baseNumber, getAbsValue(power[NUMERATOR_INDEX]))),
-                        getAbsValue(power[DENOMINATOR_INDEX]));
+        return new RationalNumber(numerator, denominator).Reduce();
     }
 
-    private static int getAbsValue(int value)
+    private double Pow(int baseNumber)
     {
-        return (value > 0) ? value : -value;
+        return NthRoot((Sign(this.Numerator) == Sign(this.Denominator) ?
+                        Pow(baseNumber, Abs(this.Numerator)) :
+                        1d / Pow(baseNumber, Abs(this.Numerator))),
+                        Abs(this.Denominator));
     }
 
-    private static double getAbsValue(double value)
+    private int Abs(int value)
     {
         return (value > 0) ? value : -value;
     }
 
-    private static int getSign(int value)
+    private double Abs(double value)
+    {
+        return (value > 0) ? value : -value;
+    }
+
+    private int Sign(int value)
     {
         return (value > 0) ? 1 : -1;
     }
 
-    private static int greatestCommonDivisor(int a, int b)
+    private int GreatestCommonDenominator(int a, int b)
     {
-        var x = getAbsValue(a);
-        var y = getAbsValue(b);
+        var x = Abs(a);
+        var y = Abs(b);
 
-        while (a != b)
-            if (a > b) a -= b; else b -= a;
-        return a;
+        while (x != 0 && y != 0)
+            if (x > y) x %= y; else y %= x;
+        return x == 0 ? y : x;
     }
 
-    private static double pow(double baseValue, int exp)
+    private double Pow(double baseValue, int exp)
     {
         double result = 1.0;
         while (exp != 0)
@@ -112,19 +131,17 @@ public static class RationalNumbers
         return result;
     }
 
-    private static double nthRoot(double baseValue, int n)
+    private double NthRoot(double baseValue, int n)
     {
         if (n == 1) return baseValue;
         double deltaX;
         double x = 0.1;
         do
         {
-            deltaX = ((double)baseValue / pow(x, (n - 1)) - x) / n;
+            deltaX = ((double)baseValue / Pow(x, (n - 1)) - x) / n;
             x = x + deltaX;
         }
-        while (getAbsValue(deltaX) > 0);
+        while (Abs(deltaX) > 0);
         return x;
     }
-    #endregion
-
 }
--- generators/Exercises/RationalNumbers.cs
+++ generators/Exercises/RationalNumbers.cs
@@ -4,26 +4,75 @@ using System.Linq;
 using Generators.Input;
 using Generators.Output;
 using Newtonsoft.Json.Linq;
+using Humanizer;
 
 namespace Generators.Exercises
 {
+	public struct RationalNumber
+    {
+		public RationalNumber(int[] n)
+		{
+			this.Numerator = n[0];
+			this.Denominator = n[1];
+		}
+		public int Numerator { get; }
+		public int Denominator { get; }
+    }
+	
     public class RationalNumbers : GeneratorExercise
     {
         protected override string RenderTestMethodBodyAssert(TestMethodBody testMethodBody)
         {
-            if (testMethodBody.CanonicalDataCase.Expected.GetType().Name.Equals("Double"))
-			     return RenderRationalNumberAssert(testMethodBody);
-            return base.RenderTestMethodBodyAssert(testMethodBody);
+            var input = testMethodBody.CanonicalDataCase.Properties["input"] as System.Collections.Generic.Dictionary<string, object>;
+            var operation = testMethodBody.CanonicalDataCase.Properties["property"].ToString();
+            var expected = testMethodBody.CanonicalDataCase.Properties["expected"];
+			var operationName = char.ToUpper(operation[0]) + operation.Substring(1);			
+            string template = "Assert.Equal([expression]);";
+			string expression = "";
+			
+            switch (operation.ToLower())
+            {
+                case "add":
+                case "sub":
+                case "mul":
+                case "div":
+				    {
+                       var r1 = new RationalNumber((int[])input["r1"]);
+                       var r2 = new RationalNumber((int[])input["r2"]);
+                       var e = new RationalNumber((int[])expected);
+                       expression = $"new RationalNumber ({e.Numerator},{e.Denominator}), new RationalNumber({r1.Numerator},{r1.Denominator}).{operationName}(new RationalNumber({r2.Numerator},{r2.Denominator}))";
+					}
+                    break;
+                case "abs":
+                case "reduce":
+                    {
+                        var r = new RationalNumber((int[])input["r"]);
+                        var e = new RationalNumber((int[])expected);						
+                        expression = $"new RationalNumber ({e.Numerator},{e.Denominator}), new RationalNumber({r.Numerator},{r.Denominator}).{operationName}()";
+                    }
+                    break;
+                case "exprational":
+                    {
+                        var r = new RationalNumber((int[])input["r"]);
+                        var n = input["n"];
+                        var e = new RationalNumber((int[])expected);						
+                        expression = $"new RationalNumber ({e.Numerator},{e.Denominator}), new RationalNumber({r.Numerator},{r.Denominator}).{operationName}({n})";
+                    }
+                    break;
+                case "expreal":
+                    {
+                        var x = input["x"].ToString();
+                        var r = new RationalNumber((int[])input["r"]);
+                        var e = expected;
+                        var p = precision(e);						
+                        expression = $"{e}, {x}.{operationName}(new RationalNumber({r.Numerator},{r.Denominator})),{p}";						
+                    }
+                    break;
+            }
+			return TemplateRenderer.RenderInline(template.Replace("[expression]", expression), testMethodBody.AssertTemplateParameters);			
         }
 
-        private static string RenderRationalNumberAssert(TestMethodBody testMethodBody)
-        {		
-            const string template = "Assert.Equal({{ ExpectedParameter }}, {{ TestedValue }}, precision: [precision]);";
-            return TemplateRenderer.RenderInline(template.Replace("[precision]", precision(testMethodBody.CanonicalDataCase.Properties["expected"]).ToString()),
-			                 testMethodBody.AssertTemplateParameters);
-        }
-				
-	private static int precision(object rawValue) => 
-             rawValue.ToString().Split(new char[] {'.'}).Length == 1? 0 : rawValue.ToString().Split(new char[] {'.'})[1].Length;
+        private static int precision(object rawValue) =>
+                 rawValue.ToString().Split(new char[] { '.' }).Length == 1 ? 0 : rawValue.ToString().Split(new char[] { '.' })[1].Length;
     }
 }
