3ce038d6f9b1ac747a2e94f5500b86f3cded46c5
Shamil Salakhetdinov <Shamil.Salakhetdinov@gmail.com>
2018-04-16 10:57:01 +0300

    Rational numbers solution (#533)
    * Rational Numbers - tests are generated
    
    * Rational Numbers, V.2
    
    * Rational Numbers, v.2, still Math.Pow is used in one method
    
    * Rational Numbers - ready to merge to master
    
    * RationalNumbers.cs generator changed
    
    * Rational Numbers v. 0.1
    
    * Rational Numbers v.1
    
    * Rational Numbers v.1.1
    
    * Rational Numbers v.1.2
    
    * Rational Numbers v.1.3
    
    * Rational Numbers v.1.44 (fixing indentation issue)
    
    * Rational Numbers v.2
    
    * Rational Numbers v.3 (renamed RationalNumber.cs ->RationalNumbers.cs)
    
    * Rational Numbers v.3.1 ("unlocked_by": "leap")

---
+++ exercises/rational-numbers/Example.cs
@@ -0,0 +1,167 @@
+using System;
+using System.Diagnostics;
+
+public static class RealNumberExtension
+{
+    // exponentiate real number to the rational number power
+    public static double Expreal(this int realNumber, RationalNumber r)
+    {
+        return r.Expreal(realNumber);
+    }
+}
+
+[DebuggerDisplay("{Numerator} / {Denominator}")]
+public struct RationalNumber
+{
+    public RationalNumber(int numerator, int denominator)
+    {
+        this.Numerator = numerator;
+        this.Denominator = denominator;
+    }
+
+    public int Numerator { get; }
+    public int Denominator { get; }
+
+    public RationalNumber Add(RationalNumber r)
+    {
+        return ReducedRationalNumber(this.Numerator * r.Denominator + this.Denominator * r.Numerator, this.Denominator * r.Denominator);
+    }
+
+    public static RationalNumber operator+ (RationalNumber r1, RationalNumber r2)
+    {
+        return r1.Add(r2);
+    }
+
+    public RationalNumber Sub(RationalNumber r)
+    {
+        return ReducedRationalNumber(this.Numerator * r.Denominator - this.Denominator * r.Numerator, this.Denominator * r.Denominator);
+    }
+
+    public static RationalNumber operator -(RationalNumber r1, RationalNumber r2)
+    {
+        return r1.Sub(r2);
+    }
+
+    public RationalNumber Mul(RationalNumber r)
+    {
+        if (this.Numerator == 0) return new RationalNumber(0, 1);
+        return ReducedRationalNumber(this.Numerator * r.Numerator, this.Denominator * r.Denominator);
+    }
+
+    public static RationalNumber operator *(RationalNumber r1, RationalNumber r2)
+    {
+        return r1.Mul(r2);
+    }
+
+    public RationalNumber Div(RationalNumber r)
+    {
+        return ReducedRationalNumber(this.Numerator * r.Denominator, this.Denominator * r.Numerator);
+    }
+
+    public static RationalNumber operator /(RationalNumber r1, RationalNumber r2)
+    {
+        return r1.Div(r2);
+    }
+
+    public RationalNumber Abs()
+    {
+        return new RationalNumber(Abs(this.Numerator), Abs(this.Denominator));
+    }
+
+    public RationalNumber Reduce()
+    {
+        if (this.Denominator == 0) return new RationalNumber(this.Numerator, this.Denominator);
+        else if (this.Numerator == 0) return new RationalNumber(0, 1);
+
+        var a = Abs(this.Numerator);
+        var b = Abs(this.Denominator);
+
+        var sign = Sign(this.Numerator) * Sign(this.Denominator);
+
+        return new RationalNumber(sign * a / GreatestCommonDenominator(a, b), b / GreatestCommonDenominator(a, b));
+    }
+
+    public RationalNumber Exprational(int power)
+    {
+        if (power == 0) return new RationalNumber(1, 1);
+
+        return ReducedRationalNumber
+            (
+                (Sign(this.Numerator) / Sign(this.Denominator)) *
+                Abs((int)Pow(this.Numerator, power)),
+                Abs((int)Pow(this.Denominator, power))
+            );
+    }
+
+    public double Expreal(int baseNumber)
+    {
+        if (this.Numerator == 0)
+            return 1;
+        else
+            return Pow(baseNumber);
+    }
+
+    private static RationalNumber ReducedRationalNumber(int numerator, int denominator)
+    {
+        return new RationalNumber(numerator, denominator).Reduce();
+    }
+
+    private double Pow(int baseNumber)
+    {
+        return NthRoot((Sign(this.Numerator) == Sign(this.Denominator) ?
+                        Pow(baseNumber, Abs(this.Numerator)) :
+                        1d / Pow(baseNumber, Abs(this.Numerator))),
+                        Abs(this.Denominator));
+    }
+
+    private int Abs(int value)
+    {
+        return (value > 0) ? value : -value;
+    }
+
+    private double Abs(double value)
+    {
+        return (value > 0) ? value : -value;
+    }
+
+    private int Sign(int value)
+    {
+        return (value > 0) ? 1 : -1;
+    }
+
+    private int GreatestCommonDenominator(int a, int b)
+    {
+        var x = Abs(a);
+        var y = Abs(b);
+
+        while (x != 0 && y != 0)
+            if (x > y) x %= y; else y %= x;
+        return x == 0 ? y : x;
+    }
+
+    private double Pow(double baseValue, int exp)
+    {
+        double result = 1.0;
+        while (exp != 0)
+        {
+            if ((exp & 1) != 0) result *= baseValue;
+            baseValue *= baseValue;
+            exp >>= 1;
+        }
+        return result;
+    }
+
+    private double NthRoot(double baseValue, int n)
+    {
+        if (n == 1) return baseValue;
+        double deltaX;
+        double x = 0.1;
+        do
+        {
+            deltaX = ((double)baseValue / Pow(x, (n - 1)) - x) / n;
+            x = x + deltaX;
+        }
+        while (Abs(deltaX) > 0);
+        return x;
+    }
+}
---
+++ exercises/rational-numbers/RationalNumbersTest.cs
@@ -0,0 +1,222 @@
+// This file was auto-generated based on version 1.0.0 of the canonical data.
+
+using Xunit;
+
+public class RationalNumbersTest
+{
+    [Fact]
+    public void Add_two_positive_rational_numbers()
+    {
+        Assert.Equal(new RationalNumber (7, 6), new RationalNumber(1, 2) + (new RationalNumber(2, 3)));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Add_a_positive_rational_number_and_a_negative_rational_number()
+    {
+        Assert.Equal(new RationalNumber (-1, 6), new RationalNumber(1, 2) + (new RationalNumber(-2, 3)));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Add_two_negative_rational_numbers()
+    {
+        Assert.Equal(new RationalNumber (-7, 6), new RationalNumber(-1, 2) + (new RationalNumber(-2, 3)));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Add_a_rational_number_to_its_additive_inverse()
+    {
+        Assert.Equal(new RationalNumber (0, 1), new RationalNumber(1, 2) + (new RationalNumber(-1, 2)));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Subtract_two_positive_rational_numbers()
+    {
+        Assert.Equal(new RationalNumber (-1, 6), new RationalNumber(1, 2) - (new RationalNumber(2, 3)));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Subtract_a_positive_rational_number_and_a_negative_rational_number()
+    {
+        Assert.Equal(new RationalNumber (7, 6), new RationalNumber(1, 2) - (new RationalNumber(-2, 3)));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Subtract_two_negative_rational_numbers()
+    {
+        Assert.Equal(new RationalNumber (1, 6), new RationalNumber(-1, 2) - (new RationalNumber(-2, 3)));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Subtract_a_rational_number_from_itself()
+    {
+        Assert.Equal(new RationalNumber (0, 1), new RationalNumber(1, 2) - (new RationalNumber(1, 2)));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Multiply_two_positive_rational_numbers()
+    {
+        Assert.Equal(new RationalNumber (1, 3), new RationalNumber(1, 2) * (new RationalNumber(2, 3)));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Multiply_a_negative_rational_number_by_a_positive_rational_number()
+    {
+        Assert.Equal(new RationalNumber (-1, 3), new RationalNumber(-1, 2) * (new RationalNumber(2, 3)));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Multiply_two_negative_rational_numbers()
+    {
+        Assert.Equal(new RationalNumber (1, 3), new RationalNumber(-1, 2) * (new RationalNumber(-2, 3)));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Multiply_a_rational_number_by_its_reciprocal()
+    {
+        Assert.Equal(new RationalNumber (1, 1), new RationalNumber(1, 2) * (new RationalNumber(2, 1)));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Multiply_a_rational_number_by_1()
+    {
+        Assert.Equal(new RationalNumber (1, 2), new RationalNumber(1, 2) * (new RationalNumber(1, 1)));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Multiply_a_rational_number_by_0()
+    {
+        Assert.Equal(new RationalNumber (0, 1), new RationalNumber(1, 2) * (new RationalNumber(0, 1)));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Divide_two_positive_rational_numbers()
+    {
+        Assert.Equal(new RationalNumber (3, 4), new RationalNumber(1, 2) / (new RationalNumber(2, 3)));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Divide_a_positive_rational_number_by_a_negative_rational_number()
+    {
+        Assert.Equal(new RationalNumber (-3, 4), new RationalNumber(1, 2) / (new RationalNumber(-2, 3)));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Divide_two_negative_rational_numbers()
+    {
+        Assert.Equal(new RationalNumber (3, 4), new RationalNumber(-1, 2) / (new RationalNumber(-2, 3)));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Divide_a_rational_number_by_1()
+    {
+        Assert.Equal(new RationalNumber (1, 2), new RationalNumber(1, 2) / (new RationalNumber(1, 1)));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Absolute_value_of_a_positive_rational_number()
+    {
+        Assert.Equal(new RationalNumber (1, 2), new RationalNumber(1, 2).Abs());
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Absolute_value_of_a_negative_rational_number()
+    {
+        Assert.Equal(new RationalNumber (1, 2), new RationalNumber(-1, 2).Abs());
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Absolute_value_of_zero()
+    {
+        Assert.Equal(new RationalNumber (0, 1), new RationalNumber(0, 1).Abs());
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Raise_a_positive_rational_number_to_a_positive_integer_power()
+    {
+        Assert.Equal(new RationalNumber (1, 8), new RationalNumber(1, 2).Exprational(3));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Raise_a_negative_rational_number_to_a_positive_integer_power()
+    {
+        Assert.Equal(new RationalNumber (-1, 8), new RationalNumber(-1, 2).Exprational(3));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Raise_zero_to_an_integer_power()
+    {
+        Assert.Equal(new RationalNumber (0, 1), new RationalNumber(0, 1).Exprational(5));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Raise_one_to_an_integer_power()
+    {
+        Assert.Equal(new RationalNumber (1, 1), new RationalNumber(1, 1).Exprational(4));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Raise_a_positive_rational_number_to_the_power_of_zero()
+    {
+        Assert.Equal(new RationalNumber (1, 1), new RationalNumber(1, 2).Exprational(0));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Raise_a_negative_rational_number_to_the_power_of_zero()
+    {
+        Assert.Equal(new RationalNumber (1, 1), new RationalNumber(-1, 2).Exprational(0));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Raise_a_real_number_to_a_positive_rational_number()
+    {
+        Assert.Equal(16, 8.Expreal(new RationalNumber(4, 3)),0);
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Raise_a_real_number_to_a_negative_rational_number()
+    {
+        Assert.Equal(0.333333333333333, 9.Expreal(new RationalNumber(-1, 2)),15);
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Raise_a_real_number_to_a_zero_rational_number()
+    {
+        Assert.Equal(1, 2.Expreal(new RationalNumber(0, 1)),0);
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Reduce_a_positive_rational_number_to_lowest_terms()
+    {
+        Assert.Equal(new RationalNumber (1, 2), new RationalNumber(2, 4).Reduce());
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Reduce_a_negative_rational_number_to_lowest_terms()
+    {
+        Assert.Equal(new RationalNumber (-2, 3), new RationalNumber(-4, 6).Reduce());
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Reduce_a_rational_number_with_a_negative_denominator_to_lowest_terms()
+    {
+        Assert.Equal(new RationalNumber (-1, 3), new RationalNumber(3, -9).Reduce());
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Reduce_zero_to_lowest_terms()
+    {
+        Assert.Equal(new RationalNumber (0, 1), new RationalNumber(0, 6).Reduce());
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Reduce_an_integer_to_lowest_terms()
+    {
+        Assert.Equal(new RationalNumber (-2, 1), new RationalNumber(-14, 7).Reduce());
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Reduce_one_to_lowest_terms()
+    {
+        Assert.Equal(new RationalNumber (1, 1), new RationalNumber(13, 13).Reduce());
+    }
+}
