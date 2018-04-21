81e7e1d620c36b50f44b46d6ee6bac78910364e9
ShamilS <Shamil.Salakhetdinov@gmail.com>
2018-04-22 00:48:38 +0300

    Phase 1

--- generators/Input/ConfigFile.cs
+++ generators/Input/ConfigFile.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Linq;
 using Newtonsoft.Json;
+using Generators.Output;
 
 namespace Generators.Input
 {
@@ -9,11 +10,13 @@ namespace Generators.Input
     {
         private const string ConfigFilePath = "../config.json";
 
-        public static IEnumerable<string> GetExerciseNames()
+        //SS: public static IEnumerable<string> GetExerciseNames()
+        public static IEnumerable<ConfigExercise> GetExercises() //SS:Names()
         {
             var jsonContents = File.ReadAllText(ConfigFilePath);
             var config = JsonConvert.DeserializeObject<Config>(jsonContents);
-            return config.Exercises.Select(exercise => exercise.Slug).OrderBy(x => x).ToArray();
+            //SS: return config.Exercises.Select(exercise => exercise.Slug).OrderBy(x => x).ToArray();
+            return config.Exercises.OrderBy(x => x.Slug).ToArray();
         }
 
         private class Config
@@ -21,9 +24,31 @@ namespace Generators.Input
             public ConfigExercise[] Exercises { get; set; }
         }
 
-        private class ConfigExercise
+        //ss:private class ConfigExercise
+        public class ConfigExercise
         {
+            public string Name { get { return this.Slug.ToExerciseName(); } }
             public string Slug { get; set; }
+
+            public bool Core { get; set; }
+            public int Difficulty { get; set; }
+            public string[] Topics { get; set; }
+            public string Unlocked_By { get; set; }
+            public string UUID { get; set; }
+            public bool Deprecated { get; set; }
+            //{
+            //  "core": false,
+            //  "difficulty": 10,
+            //  "slug": "pov",
+            //  "topics": [
+            //    "graphs",
+            //    "recursion",
+            //    "searching"
+            //  ],
+            //  "unlocked_by": "markdown",
+            //  "uuid": "a5794706-58d2-48f7-8aab-78639d7bce77"
+            //},
+
         }
     }
 }
