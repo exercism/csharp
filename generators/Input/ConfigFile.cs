ccb440ca9966cb6ad1007eee4d98ee1489b9e018
ShamilS <Shamil.Salakhetdinov@gmail.com>
2018-04-22 01:01:57 +0300

    Phase 2

--- generators/Input/ConfigFile.cs
+++ generators/Input/ConfigFile.cs
@@ -10,13 +10,11 @@ namespace Generators.Input
     {
         private const string ConfigFilePath = "../config.json";
 
-        //SS: public static IEnumerable<string> GetExerciseNames()
-        public static IEnumerable<ConfigExercise> GetExercises() //SS:Names()
+        public static IEnumerable<ConfigExercise> GetExercises()
         {
             var jsonContents = File.ReadAllText(ConfigFilePath);
             var config = JsonConvert.DeserializeObject<Config>(jsonContents);
-            //SS: return config.Exercises.Select(exercise => exercise.Slug).OrderBy(x => x).ToArray();
-            return config.Exercises.OrderBy(x => x.Slug).ToArray();
+            return config.Exercises.OrderBy(x => x.Name).ToArray();
         }
 
         private class Config
@@ -24,31 +22,16 @@ namespace Generators.Input
             public ConfigExercise[] Exercises { get; set; }
         }
 
-        //ss:private class ConfigExercise
         public class ConfigExercise
         {
             public string Name { get { return this.Slug.ToExerciseName(); } }
             public string Slug { get; set; }
-
             public bool Core { get; set; }
             public int Difficulty { get; set; }
             public string[] Topics { get; set; }
             public string Unlocked_By { get; set; }
             public string UUID { get; set; }
             public bool Deprecated { get; set; }
-            //{
-            //  "core": false,
-            //  "difficulty": 10,
-            //  "slug": "pov",
-            //  "topics": [
-            //    "graphs",
-            //    "recursion",
-            //    "searching"
-            //  ],
-            //  "unlocked_by": "markdown",
-            //  "uuid": "a5794706-58d2-48f7-8aab-78639d7bce77"
-            //},
-
         }
     }
 }
