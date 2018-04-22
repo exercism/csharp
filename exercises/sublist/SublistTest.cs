c2e5fded72a1f8eae9b398ff92b3a35559f4baad
ShamilS <Shamil.Salakhetdinov@gmail.com>
2018-04-23 02:57:35 +0300

    'sublist' problem test generator v.1

--- exercises/sublist/SublistTest.cs
+++ exercises/sublist/SublistTest.cs
@@ -1,150 +1,109 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+// This file was auto-generated based on version 1.1.0 of the canonical data.
+
 using Xunit;
+using System.Collections.Generic;
 
 public class SublistTest
 {
     [Fact]
-    public void Empty_equals_empty()
+    public void Empty_lists()
     {
-        var list1 = new List<int>();
-        var list2 = new List<int>();
-        Assert.Equal(SublistType.Equal, Sublist.Classify(list1, list2));
+        Assert.Equal(SublistType.Equal, Sublist.Classify(new List<int>() { }, new List<int>() { }));
     }
 
     [Fact(Skip = "Remove to run test")]
-    public void Empty_is_a_sublist_of_anything()
+    public void Empty_list_within_non_empty_list()
     {
-        var list1 = new List<int>();
-        var list2 = new List<int> { 1, 2, 3, 4 };
-        Assert.Equal(SublistType.Sublist, Sublist.Classify(list1, list2));
+        Assert.Equal(SublistType.Sublist, Sublist.Classify(new List<int>() { }, new List<int>() {1, 2, 3}));
     }
 
     [Fact(Skip = "Remove to run test")]
-    public void Anything_is_a_superlist_of_empty()
+    public void Non_empty_list_contains_empty_list()
     {
-        var list1 = new List<int> { 1, 2, 3, 4 };
-        var list2 = new List<int>();
-        Assert.Equal(SublistType.Superlist, Sublist.Classify(list1, list2));
+        Assert.Equal(SublistType.Superlist, Sublist.Classify(new List<int>() {1, 2, 3}, new List<int>() { }));
     }
 
     [Fact(Skip = "Remove to run test")]
-    public void One_is_not_two()
+    public void List_equals_itself()
     {
-        var list1 = new List<int> { 1 };
-        var list2 = new List<int> { 2 };
-        Assert.Equal(SublistType.Unequal, Sublist.Classify(list1, list2));
+        Assert.Equal(SublistType.Equal, Sublist.Classify(new List<int>() {1, 2, 3}, new List<int>() {1, 2, 3}));
     }
 
     [Fact(Skip = "Remove to run test")]
-    public void Compare_larger_equal_lists()
+    public void Different_lists()
     {
-        var list1 = new List<char>(Enumerable.Repeat('x', 1000));
-        var list2 = new List<char>(Enumerable.Repeat('x', 1000));
-        Assert.Equal(SublistType.Equal, Sublist.Classify(list1, list2));
+        Assert.Equal(SublistType.Unequal, Sublist.Classify(new List<int>() {1, 2, 3}, new List<int>() {2, 3, 4}));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void False_start()
+    {
+        Assert.Equal(SublistType.Sublist, Sublist.Classify(new List<int>() {1, 2, 5}, new List<int>() {0, 1, 2, 3, 1, 2, 5, 6}));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Consecutive()
+    {
+        Assert.Equal(SublistType.Sublist, Sublist.Classify(new List<int>() {1, 1, 2}, new List<int>() {0, 1, 1, 1, 2, 1, 2}));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Sublist_at_start()
     {
-        var list1 = new List<int> { 1, 2, 3 };
-        var list2 = new List<int> { 1, 2, 3, 4, 5 };
-        Assert.Equal(SublistType.Sublist, Sublist.Classify(list1, list2));
+        Assert.Equal(SublistType.Sublist, Sublist.Classify(new List<int>() {0, 1, 2}, new List<int>() {0, 1, 2, 3, 4, 5}));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Sublist_in_middle()
     {
-        var list1 = new List<int> { 4, 3, 2 };
-        var list2 = new List<int> { 5, 4, 3, 2, 1 };
-        Assert.Equal(SublistType.Sublist, Sublist.Classify(list1, list2));
+        Assert.Equal(SublistType.Sublist, Sublist.Classify(new List<int>() {2, 3, 4}, new List<int>() {0, 1, 2, 3, 4, 5}));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Sublist_at_end()
     {
-        var list1 = new List<int> { 3, 4, 5 };
-        var list2 = new List<int> { 1, 2, 3, 4, 5 };
-        Assert.Equal(SublistType.Sublist, Sublist.Classify(list1, list2));
+        Assert.Equal(SublistType.Sublist, Sublist.Classify(new List<int>() {3, 4, 5}, new List<int>() {0, 1, 2, 3, 4, 5}));
     }
 
     [Fact(Skip = "Remove to run test")]
-    public void Partially_matching_sublist_at_start()
+    public void At_start_of_superlist()
     {
-        var list1 = new List<int> { 1, 1, 2 };
-        var list2 = new List<int> { 1, 1, 1, 2 };
-        Assert.Equal(SublistType.Sublist, Sublist.Classify(list1, list2));
+        Assert.Equal(SublistType.Superlist, Sublist.Classify(new List<int>() {0, 1, 2, 3, 4, 5}, new List<int>() {0, 1, 2}));
     }
 
     [Fact(Skip = "Remove to run test")]
-    public void Sublist_early_in_huge_list()
+    public void In_middle_of_superlist()
     {
-        var list1 = new List<int> { 3, 4, 5 };
-        var list2 = new List<int>(Enumerable.Range(1, 1000000));
-        Assert.Equal(SublistType.Sublist, Sublist.Classify(list1, list2));
+        Assert.Equal(SublistType.Superlist, Sublist.Classify(new List<int>() {0, 1, 2, 3, 4, 5}, new List<int>() {2, 3}));
     }
 
     [Fact(Skip = "Remove to run test")]
-    public void Huge_sublist_not_in_huge_list()
+    public void At_end_of_superlist()
     {
-        var list1 = new List<int>(Enumerable.Range(10, 1000001));
-        var list2 = new List<int>(Enumerable.Range(1, 1000000));
-        Assert.Equal(SublistType.Unequal, Sublist.Classify(list1, list2));
+        Assert.Equal(SublistType.Superlist, Sublist.Classify(new List<int>() {0, 1, 2, 3, 4, 5}, new List<int>() {3, 4, 5}));
     }
 
     [Fact(Skip = "Remove to run test")]
-    public void Superlist_at_start()
+    public void First_list_missing_element_from_second_list()
     {
-        var list1 = new List<int> { 1, 2, 3, 4, 5 };
-        var list2 = new List<int> { 1, 2, 3 };
-        Assert.Equal(SublistType.Superlist, Sublist.Classify(list1, list2));
+        Assert.Equal(SublistType.Unequal, Sublist.Classify(new List<int>() {1, 3}, new List<int>() {1, 2, 3}));
     }
 
     [Fact(Skip = "Remove to run test")]
-    public void Superlist_in_middle()
+    public void Second_list_missing_element_from_first_list()
     {
-        var list1 = new List<int> { 5, 4, 3, 2, 1 };
-        var list2 = new List<int> { 4, 3, 2 };
-        Assert.Equal(SublistType.Superlist, Sublist.Classify(list1, list2));
+        Assert.Equal(SublistType.Unequal, Sublist.Classify(new List<int>() {1, 2, 3}, new List<int>() {1, 3}));
     }
 
     [Fact(Skip = "Remove to run test")]
-    public void Superlist_at_end()
+    public void Order_matters_to_a_list()
     {
-        var list1 = new List<int> { 1, 2, 3, 4, 5 };
-        var list2 = new List<int> { 3, 4, 5 };
-        Assert.Equal(SublistType.Superlist, Sublist.Classify(list1, list2));
+        Assert.Equal(SublistType.Unequal, Sublist.Classify(new List<int>() {1, 2, 3}, new List<int>() {3, 2, 1}));
     }
 
     [Fact(Skip = "Remove to run test")]
-    public void Partially_matching_superlist_at_start()
+    public void Same_digits_but_different_numbers()
     {
-        var list1 = new List<int> { 1, 1, 1, 2 };
-        var list2 = new List<int> { 1, 1, 2 };
-        Assert.Equal(SublistType.Superlist, Sublist.Classify(list1, list2));
-    }
-
-    [Fact(Skip = "Remove to run test")]
-    public void Superlist_early_in_huge_list()
-    {
-        var list1 = new List<int>(Enumerable.Range(1, 1000000));
-        var list2 = new List<int> { 3, 4, 5 };
-        Assert.Equal(SublistType.Superlist, Sublist.Classify(list1, list2));
-    }
-
-    [Fact(Skip = "Remove to run test")]
-    public void Recurring_values_sublist()
-    {
-        var list1 = new List<int> { 1, 2, 1, 2, 3 };
-        var list2 = new List<int> { 1, 2, 3, 1, 2, 1, 2, 3, 2, 1 };
-        Assert.Equal(SublistType.Sublist, Sublist.Classify(list1, list2));
-    }
-
-    [Fact(Skip = "Remove to run test")]
-    public void Recurring_values_unequal()
-    {
-        var list1 = new List<int> { 1, 2, 1, 2, 3 };
-        var list2 = new List<int> { 1, 2, 3, 1, 2, 3, 2, 3, 2, 1 };
-        Assert.Equal(SublistType.Unequal, Sublist.Classify(list1, list2));
+        Assert.Equal(SublistType.Unequal, Sublist.Classify(new List<int>() {1, 0, 1}, new List<int>() {10, 1}));
     }
 }
---
+++ generators/Exercises/Sublist.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Generators.Input;
+using Generators.Output;
+using Humanizer;
+using System.Linq;
+
+namespace Generators.Exercises
+{
+    public class Sublist : GeneratorExercise
+    {
+        protected override HashSet<string> AddAdditionalNamespaces() => new HashSet<string>() { typeof(System.Collections.Generic.IList<int>).Namespace };
+
+        private UnescapedValue inputValues(int[] list)
+        {
+            var template = (list != null) ? string.Join(", ", Array.ConvertAll<int, string>(list, (x) => { return $"{x}"; })) : " ";
+            return new UnescapedValue($"new List<int>() {{{template}}}");
+        }
+        protected override void UpdateCanonicalData(CanonicalData canonicalData)
+        {
+            foreach (var canonicalDataCase in canonicalData.Cases)
+            {              
+                var input = canonicalDataCase.Properties["input"] as System.Collections.Generic.Dictionary<string, object>;
+                input["listOne"] = inputValues (input["listOne"] as int[]);
+                input["listTwo"] = inputValues(input["listTwo"] as int[]);
+
+                canonicalDataCase.Property = "classify";
+                canonicalDataCase.Properties["expected"] = new UnescapedValue($"SublistType.{(canonicalDataCase.Properties["expected"] as string).Dehumanize()}");
+            }
+        }
+    }
+}
+
