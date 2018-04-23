228ef303ece834fa6f6d29d73de10e7b1d981bcf
ShamilS <Shamil.Salakhetdinov@gmail.com>
2018-04-23 17:01:30 +0300

    'sublist' problem test generator v.3

--- generators/Exercises/Sublist.cs
+++ generators/Exercises/Sublist.cs
@@ -1,18 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using Generators.Input;
 using Generators.Output;
 using Humanizer;
-using System.Linq;
 
 namespace Generators.Exercises
 {
     public class Sublist : GeneratorExercise
     {
-        protected override HashSet<string> AddAdditionalNamespaces() => new HashSet<string>() { typeof(System.Collections.Generic.IList<int>).Namespace };
+        protected override HashSet<string> AddAdditionalNamespaces() => new HashSet<string>() { typeof(IList<int>).Namespace };
 
-        private UnescapedValue inputValues(int[] list)
+        private UnescapedValue InputValues(int[] list)
         {
             var template = (list != null) ? string.Join(", ", Array.ConvertAll<int, string>(list, (x) => { return $"{x}"; })) : "";
             return new UnescapedValue($"new List<int>() {{ {template} }}".Replace("  ", " "));
@@ -21,9 +19,9 @@ namespace Generators.Exercises
         {
             foreach (var canonicalDataCase in canonicalData.Cases)
             {              
-                var input = canonicalDataCase.Properties["input"] as System.Collections.Generic.Dictionary<string, object>;
-                input["listOne"] = inputValues(input["listOne"] as int[]);
-                input["listTwo"] = inputValues(input["listTwo"] as int[]);
+                var input = canonicalDataCase.Properties["input"] as Dictionary<string, object>;
+                input["listOne"] = InputValues(input["listOne"] as int[]);
+                input["listTwo"] = InputValues(input["listTwo"] as int[]);
 
                 canonicalDataCase.Property = "classify";
                 canonicalDataCase.Properties["expected"] = new UnescapedValue($"SublistType.{(canonicalDataCase.Properties["expected"] as string).Dehumanize()}");
