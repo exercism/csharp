89d7ea496331d5d53cfbd1c1de50b95c26a7e7cd
Shamil Salakhetdinov <Shamil.Salakhetdinov@gmail.com>
2018-04-24 19:36:24 +0300

    'sublist' problem test generator v.1 (#562)
    * 'sublist' problem test generator v.1
    
    * 'sublist' problem test generator v.2
    
    * 'sublist' problem test generator v.3

---
+++ generators/Exercises/Sublist.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Generators.Input;
+using Generators.Output;
+using Humanizer;
+
+namespace Generators.Exercises
+{
+    public class Sublist : GeneratorExercise
+    {
+        protected override HashSet<string> AddAdditionalNamespaces() => new HashSet<string>() { typeof(IList<int>).Namespace };
+
+        private UnescapedValue InputValues(int[] list)
+        {
+            var template = (list != null) ? string.Join(", ", Array.ConvertAll<int, string>(list, (x) => { return $"{x}"; })) : "";
+            return new UnescapedValue($"new List<int>() {{ {template} }}".Replace("  ", " "));
+        }
+        protected override void UpdateCanonicalData(CanonicalData canonicalData)
+        {
+            foreach (var canonicalDataCase in canonicalData.Cases)
+            {              
+                var input = canonicalDataCase.Properties["input"] as Dictionary<string, object>;
+                input["listOne"] = InputValues(input["listOne"] as int[]);
+                input["listTwo"] = InputValues(input["listTwo"] as int[]);
+
+                canonicalDataCase.Property = "classify";
+                canonicalDataCase.Properties["expected"] = new UnescapedValue($"SublistType.{(canonicalDataCase.Properties["expected"] as string).Dehumanize()}");
+            }
+        }
+    }
+}
+
