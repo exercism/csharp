5a435602cec1fec36395bf647ff36c382e949abe
Erik Schierboom <erik_schierboom@hotmail.com>
2018-06-18 08:39:47 +0200

    generators: Use static class

--- generators/Output/IndentFilter.cs
+++ generators/Output/IndentFilter.cs
@@ -3,9 +3,9 @@ using System.Linq;
 
 namespace Generators.Output
 {
-    public class IndentFilter
+    public static class IndentFilter
     {
-        public static string Indent(string input) 
+        public static string Indent(string input)
             => string.Join(Environment.NewLine, input
                 .Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
                 .Select(x => x.Indent()));
