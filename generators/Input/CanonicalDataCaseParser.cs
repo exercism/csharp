ecf032e30ad0554c110525122cf3d4bfbe192c96
Erik Schierboom <erik_schierboom@hotmail.com>
2018-06-27 17:35:21 +0200

    generators: Move JToken conversion helpers to separate class

--- generators/Input/CanonicalDataCaseParser.cs
+++ generators/Input/CanonicalDataCaseParser.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using Exercism.CSharp.Helpers;
 using Newtonsoft.Json.Linq;
 
 namespace Exercism.CSharp.Input
@@ -20,131 +18,17 @@ namespace Exercism.CSharp.Input
             => new CanonicalDataCase(
                 index: index,
                 property: canonicalDataCaseJToken.Value<string>("property"),
-                input: ToReadOnlyDictionary(canonicalDataCaseJToken["input"]),
-                expected: ConvertJToken(canonicalDataCaseJToken["expected"]),
+                input: JTokenHelper.ConvertJToken(canonicalDataCaseJToken["input"]),
+                expected: JTokenHelper.ConvertJToken(canonicalDataCaseJToken["expected"]),
                 description: canonicalDataCaseJToken.Value<string>("description"),
                 descriptionPath: GetDescriptionPath(canonicalDataCaseJToken));
 
-        private static string[] GetDescriptionPath(JToken canonicalDataCaseToken) 
-            => canonicalDataCaseToken.ParentsAndSelf()
+        private static string[] GetDescriptionPath(JToken canonicalDataCaseJToken) 
+            => canonicalDataCaseJToken.ParentsAndSelf()
                 .Where(token => token.Type == JTokenType.Object)
                 .Select(token => token.Value<string>("description"))
                 .Where(description => description != null)
                 .Reverse()
                 .ToArray();
-
-        private static IReadOnlyDictionary<string, dynamic> ToReadOnlyDictionary(JToken jToken) => ConvertJToken(jToken);
-
-        private static dynamic ConvertJToken(JToken jToken)
-        {
-            switch (jToken?.Type)
-            {
-                case JTokenType.Object:
-                    return ConvertJObject((JObject)jToken);
-                case JTokenType.Array:
-                    return ConvertJArray((JArray)jToken);
-                case JTokenType.Property:
-                    return jToken.ToObject<IReadOnlyDictionary<string, dynamic>>();
-                case JTokenType.Integer:
-                    return ConvertIntegerJToken(jToken);
-                case JTokenType.Float:
-                    return jToken.ToObject<float>();
-                case JTokenType.String:
-                    return jToken.ToObject<string>();
-                case JTokenType.Boolean:
-                    return jToken.ToObject<bool>();
-                case JTokenType.Date:
-                    return jToken.ToObject<DateTime>();
-                case JTokenType.Raw:
-                    return jToken.ToObject<string>();
-                case JTokenType.Bytes:
-                    return jToken.ToObject<byte[]>();
-                case JTokenType.Guid:
-                    return jToken.ToObject<Guid>();
-                case JTokenType.Uri:
-                    return jToken.ToObject<Uri>();
-                case JTokenType.TimeSpan:
-                    return jToken.ToObject<TimeSpan>();
-                default:
-                    return null;
-            }
-        }
-
-        private static dynamic ConvertJObject(JObject jObject)
-        {
-            var properties = jObject.ToObject<IDictionary<string, dynamic>>();
-
-            for (var i = 0; i < properties.Count; i++)
-            {
-                var key = properties.Keys.ElementAt(i);
-                var value = properties[key];
-                properties[key] = value is JToken jToken ? ConvertJToken(jToken) : value;
-            }
-
-            return properties;
-        }
-
-        private static dynamic ConvertJArray(JArray jArray)
-        {
-            // We can't determine the type of the array if the array is empty
-            if (!jArray.Any())
-                return jArray;
-
-            // We can only convert when all values have the same type
-            if (jArray.Select(x => x.Type).Distinct().Count() != 1)
-                return jArray;
-
-            switch (jArray[0].Type)
-            {
-                case JTokenType.Object:
-                    return jArray.Select(ConvertJToken).ToArray();
-                case JTokenType.Integer:
-                    var strings = jArray.ToObject<string[]>();
-                    if (strings.All(str => int.TryParse(str, out _)))
-                        return jArray.ToObject<int[]>();
-
-                    if (strings.All(str => long.TryParse(str, out _)))
-                        return jArray.ToObject<long[]>();
-
-                    if (strings.All(str => ulong.TryParse(str, out _)))
-                        return jArray.ToObject<ulong[]>();
-
-                    return strings;
-                case JTokenType.Float:
-                    return jArray.ToObject<float[]>();
-                case JTokenType.String:
-                    return jArray.ToObject<string[]>();
-                case JTokenType.Boolean:
-                    return jArray.ToObject<bool[]>();
-                case JTokenType.Date:
-                    return jArray.ToObject<DateTime[]>();
-                case JTokenType.Bytes:
-                    return jArray.ToObject<byte[]>();
-                case JTokenType.Guid:
-                    return jArray.ToObject<Guid[]>();
-                case JTokenType.Uri:
-                    return jArray.ToObject<Uri[]>();
-                case JTokenType.TimeSpan:
-                    return jArray.ToObject<TimeSpan[]>();
-                default:
-                    return jArray;
-            }
-        }
-
-        private static dynamic ConvertIntegerJToken(JToken jToken)
-        {
-            var str = jToken.ToObject<string>();
-
-            if (int.TryParse(str, out var i))
-                return i;
-
-            if (long.TryParse(str, out var l))
-                return l;
-
-            if (ulong.TryParse(str, out var ul))
-                return ul;
-
-            return str;
-        }
     }
 }
---
+++ generators/Input/JTokenHelper.cs
@@ -0,0 +1,131 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace Exercism.CSharp.Input
+{
+    public static class JTokenHelper
+    {
+        public static IEnumerable<JToken> ParentsAndSelf(this JToken jToken)
+        {
+            while (jToken != null)
+            {
+                yield return jToken;
+                jToken = jToken.Parent;
+            }
+        }
+        
+        public static dynamic ConvertJToken(JToken jToken)
+        {
+            switch (jToken?.Type)
+            {
+                case JTokenType.Object:
+                    return ConvertJObject((JObject)jToken);
+                case JTokenType.Array:
+                    return ConvertJArray((JArray)jToken);
+                case JTokenType.Property:
+                    return jToken.ToObject<IReadOnlyDictionary<string, dynamic>>();
+                case JTokenType.Integer:
+                    return ConvertIntegerJToken(jToken);
+                case JTokenType.Float:
+                    return jToken.ToObject<float>();
+                case JTokenType.String:
+                    return jToken.ToObject<string>();
+                case JTokenType.Boolean:
+                    return jToken.ToObject<bool>();
+                case JTokenType.Date:
+                    return jToken.ToObject<DateTime>();
+                case JTokenType.Raw:
+                    return jToken.ToObject<string>();
+                case JTokenType.Bytes:
+                    return jToken.ToObject<byte[]>();
+                case JTokenType.Guid:
+                    return jToken.ToObject<Guid>();
+                case JTokenType.Uri:
+                    return jToken.ToObject<Uri>();
+                case JTokenType.TimeSpan:
+                    return jToken.ToObject<TimeSpan>();
+                default:
+                    return null;
+            }
+        }
+
+        private static dynamic ConvertJObject(JObject jObject)
+        {
+            var properties = jObject.ToObject<IDictionary<string, dynamic>>();
+
+            for (var i = 0; i < properties.Count; i++)
+            {
+                var key = properties.Keys.ElementAt(i);
+                var value = properties[key];
+                properties[key] = value is JToken jToken ? ConvertJToken(jToken) : value;
+            }
+
+            return properties;
+        }
+
+        private static dynamic ConvertJArray(JArray jArray)
+        {
+            // We can't determine the type of the array if the array is empty
+            if (!jArray.Any())
+                return jArray;
+
+            // We can only convert when all values have the same type
+            if (jArray.Select(x => x.Type).Distinct().Count() != 1)
+                return jArray;
+
+            switch (jArray[0].Type)
+            {
+                case JTokenType.Object:
+                    return jArray.Select(ConvertJToken).ToArray();
+                case JTokenType.Integer:
+                    var strings = jArray.ToObject<string[]>();
+                    if (strings.All(str => int.TryParse(str, out _)))
+                        return jArray.ToObject<int[]>();
+
+                    if (strings.All(str => long.TryParse(str, out _)))
+                        return jArray.ToObject<long[]>();
+
+                    if (strings.All(str => ulong.TryParse(str, out _)))
+                        return jArray.ToObject<ulong[]>();
+
+                    return strings;
+                case JTokenType.Float:
+                    return jArray.ToObject<float[]>();
+                case JTokenType.String:
+                    return jArray.ToObject<string[]>();
+                case JTokenType.Boolean:
+                    return jArray.ToObject<bool[]>();
+                case JTokenType.Date:
+                    return jArray.ToObject<DateTime[]>();
+                case JTokenType.Bytes:
+                    return jArray.ToObject<byte[]>();
+                case JTokenType.Guid:
+                    return jArray.ToObject<Guid[]>();
+                case JTokenType.Uri:
+                    return jArray.ToObject<Uri[]>();
+                case JTokenType.TimeSpan:
+                    return jArray.ToObject<TimeSpan[]>();
+                default:
+                    return jArray;
+            }
+        }
+
+        private static dynamic ConvertIntegerJToken(JToken jToken)
+        {
+            var str = jToken.ToObject<string>();
+
+            if (int.TryParse(str, out var i))
+                return i;
+
+            if (long.TryParse(str, out var l))
+                return l;
+
+            if (ulong.TryParse(str, out var ul))
+                return ul;
+
+            return str;
+        }
+    }
+}
