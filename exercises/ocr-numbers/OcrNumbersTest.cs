8257eb73b20ffc95c3c6c57980d0605071e600fc
Erik Schierboom <erik_schierboom@hotmail.com>
2018-07-03 14:01:30 +0200

    generators: Simplify ocr-numbers generator

--- exercises/ocr-numbers/OcrNumbersTest.cs
+++ exercises/ocr-numbers/OcrNumbersTest.cs
@@ -9,9 +9,9 @@ public class OcrNumbersTest
     public void Recognizes_0()
     {
         var rows = 
-            " _ " + "\n" +
-            "| |" + "\n" +
-            "|_|" + "\n" +
+            " _ \n" +
+            "| |\n" +
+            "|_|\n" +
             "   ";
         var actual = OcrNumbers.Convert(rows);
         Assert.Equal("0", actual);
@@ -21,9 +21,9 @@ public class OcrNumbersTest
     public void Recognizes_1()
     {
         var rows = 
-            "   " + "\n" +
-            "  |" + "\n" +
-            "  |" + "\n" +
+            "   \n" +
+            "  |\n" +
+            "  |\n" +
             "   ";
         var actual = OcrNumbers.Convert(rows);
         Assert.Equal("1", actual);
@@ -33,9 +33,9 @@ public class OcrNumbersTest
     public void Unreadable_but_correctly_sized_inputs_return_()
     {
         var rows = 
-            "   " + "\n" +
-            "  _" + "\n" +
-            "  |" + "\n" +
+            "   \n" +
+            "  _\n" +
+            "  |\n" +
             "   ";
         var actual = OcrNumbers.Convert(rows);
         Assert.Equal("?", actual);
@@ -45,8 +45,8 @@ public class OcrNumbersTest
     public void Input_with_a_number_of_lines_that_is_not_a_multiple_of_four_raises_an_error()
     {
         var rows = 
-            " _ " + "\n" +
-            "| |" + "\n" +
+            " _ \n" +
+            "| |\n" +
             "   ";
         Assert.Throws<ArgumentException>(() => OcrNumbers.Convert(rows));
     }
@@ -55,9 +55,9 @@ public class OcrNumbersTest
     public void Input_with_a_number_of_columns_that_is_not_a_multiple_of_three_raises_an_error()
     {
         var rows = 
-            "    " + "\n" +
-            "   |" + "\n" +
-            "   |" + "\n" +
+            "    \n" +
+            "   |\n" +
+            "   |\n" +
             "    ";
         Assert.Throws<ArgumentException>(() => OcrNumbers.Convert(rows));
     }
@@ -66,9 +66,9 @@ public class OcrNumbersTest
     public void Recognizes_110101100()
     {
         var rows = 
-            "       _     _        _  _ " + "\n" +
-            "  |  || |  || |  |  || || |" + "\n" +
-            "  |  ||_|  ||_|  |  ||_||_|" + "\n" +
+            "       _     _        _  _ \n" +
+            "  |  || |  || |  |  || || |\n" +
+            "  |  ||_|  ||_|  |  ||_||_|\n" +
             "                           ";
         var actual = OcrNumbers.Convert(rows);
         Assert.Equal("110101100", actual);
@@ -78,9 +78,9 @@ public class OcrNumbersTest
     public void Garbled_numbers_in_a_string_are_replaced_with_()
     {
         var rows = 
-            "       _     _           _ " + "\n" +
-            "  |  || |  || |     || || |" + "\n" +
-            "  |  | _|  ||_|  |  ||_||_|" + "\n" +
+            "       _     _           _ \n" +
+            "  |  || |  || |     || || |\n" +
+            "  |  | _|  ||_|  |  ||_||_|\n" +
             "                           ";
         var actual = OcrNumbers.Convert(rows);
         Assert.Equal("11?10?1?0", actual);
@@ -90,9 +90,9 @@ public class OcrNumbersTest
     public void Recognizes_2()
     {
         var rows = 
-            " _ " + "\n" +
-            " _|" + "\n" +
-            "|_ " + "\n" +
+            " _ \n" +
+            " _|\n" +
+            "|_ \n" +
             "   ";
         var actual = OcrNumbers.Convert(rows);
         Assert.Equal("2", actual);
@@ -102,9 +102,9 @@ public class OcrNumbersTest
     public void Recognizes_3()
     {
         var rows = 
-            " _ " + "\n" +
-            " _|" + "\n" +
-            " _|" + "\n" +
+            " _ \n" +
+            " _|\n" +
+            " _|\n" +
             "   ";
         var actual = OcrNumbers.Convert(rows);
         Assert.Equal("3", actual);
@@ -114,9 +114,9 @@ public class OcrNumbersTest
     public void Recognizes_4()
     {
         var rows = 
-            "   " + "\n" +
-            "|_|" + "\n" +
-            "  |" + "\n" +
+            "   \n" +
+            "|_|\n" +
+            "  |\n" +
             "   ";
         var actual = OcrNumbers.Convert(rows);
         Assert.Equal("4", actual);
@@ -126,9 +126,9 @@ public class OcrNumbersTest
     public void Recognizes_5()
     {
         var rows = 
-            " _ " + "\n" +
-            "|_ " + "\n" +
-            " _|" + "\n" +
+            " _ \n" +
+            "|_ \n" +
+            " _|\n" +
             "   ";
         var actual = OcrNumbers.Convert(rows);
         Assert.Equal("5", actual);
@@ -138,9 +138,9 @@ public class OcrNumbersTest
     public void Recognizes_6()
     {
         var rows = 
-            " _ " + "\n" +
-            "|_ " + "\n" +
-            "|_|" + "\n" +
+            " _ \n" +
+            "|_ \n" +
+            "|_|\n" +
             "   ";
         var actual = OcrNumbers.Convert(rows);
         Assert.Equal("6", actual);
@@ -150,9 +150,9 @@ public class OcrNumbersTest
     public void Recognizes_7()
     {
         var rows = 
-            " _ " + "\n" +
-            "  |" + "\n" +
-            "  |" + "\n" +
+            " _ \n" +
+            "  |\n" +
+            "  |\n" +
             "   ";
         var actual = OcrNumbers.Convert(rows);
         Assert.Equal("7", actual);
@@ -162,9 +162,9 @@ public class OcrNumbersTest
     public void Recognizes_8()
     {
         var rows = 
-            " _ " + "\n" +
-            "|_|" + "\n" +
-            "|_|" + "\n" +
+            " _ \n" +
+            "|_|\n" +
+            "|_|\n" +
             "   ";
         var actual = OcrNumbers.Convert(rows);
         Assert.Equal("8", actual);
@@ -174,9 +174,9 @@ public class OcrNumbersTest
     public void Recognizes_9()
     {
         var rows = 
-            " _ " + "\n" +
-            "|_|" + "\n" +
-            " _|" + "\n" +
+            " _ \n" +
+            "|_|\n" +
+            " _|\n" +
             "   ";
         var actual = OcrNumbers.Convert(rows);
         Assert.Equal("9", actual);
@@ -186,9 +186,9 @@ public class OcrNumbersTest
     public void Recognizes_string_of_decimal_numbers()
     {
         var rows = 
-            "    _  _     _  _  _  _  _  _ " + "\n" +
-            "  | _| _||_||_ |_   ||_||_|| |" + "\n" +
-            "  ||_  _|  | _||_|  ||_| _||_|" + "\n" +
+            "    _  _     _  _  _  _  _  _ \n" +
+            "  | _| _||_||_ |_   ||_||_|| |\n" +
+            "  ||_  _|  | _||_|  ||_| _||_|\n" +
             "                              ";
         var actual = OcrNumbers.Convert(rows);
         Assert.Equal("1234567890", actual);
@@ -198,17 +198,17 @@ public class OcrNumbersTest
     public void Numbers_separated_by_empty_lines_are_recognized_lines_are_joined_by_commas_()
     {
         var rows = 
-            "    _  _ " + "\n" +
-            "  | _| _|" + "\n" +
-            "  ||_  _|" + "\n" +
-            "         " + "\n" +
-            "    _  _ " + "\n" +
-            "|_||_ |_ " + "\n" +
-            "  | _||_|" + "\n" +
-            "         " + "\n" +
-            " _  _  _ " + "\n" +
-            "  ||_||_|" + "\n" +
-            "  ||_| _|" + "\n" +
+            "    _  _ \n" +
+            "  | _| _|\n" +
+            "  ||_  _|\n" +
+            "         \n" +
+            "    _  _ \n" +
+            "|_||_ |_ \n" +
+            "  | _||_|\n" +
+            "         \n" +
+            " _  _  _ \n" +
+            "  ||_||_|\n" +
+            "  ||_| _|\n" +
             "         ";
         var actual = OcrNumbers.Convert(rows);
         Assert.Equal("123,456,789", actual);
