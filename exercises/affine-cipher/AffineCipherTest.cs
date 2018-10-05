e7a8ac42f1941e8f558f2f77f84dea964318c8d6
Davide Lettieri <davide.lettieri@nyxgg.com>
2018-10-05 18:09:33 +0200

    regenerate test

--- exercises/affine-cipher/AffineCipherTest.cs
+++ exercises/affine-cipher/AffineCipherTest.cs
@@ -12,56 +12,56 @@ public class AffineCipherTest
         Assert.Equal("xbt", ac.Encode("yes",5,7));
     }
 
-    [Fact]
+    [Fact(Skip = "Remove to run test")]
     public void Encode_no()
     {
         var ac = new AffineCipher();
         Assert.Equal("fu", ac.Encode("no",15,18));
     }
 
-    [Fact]
+    [Fact(Skip = "Remove to run test")]
     public void Encode_omg()
     {
         var ac = new AffineCipher();
         Assert.Equal("lvz", ac.Encode("OMG",21,3));
     }
 
-    [Fact]
+    [Fact(Skip = "Remove to run test")]
     public void Encode_o_m_g()
     {
         var ac = new AffineCipher();
         Assert.Equal("hjp", ac.Encode("O M G",25,47));
     }
 
-    [Fact]
+    [Fact(Skip = "Remove to run test")]
     public void Encode_mindblowingly()
     {
         var ac = new AffineCipher();
         Assert.Equal("rzcwa gnxzc dgt", ac.Encode("mindblowingly",11,15));
     }
 
-    [Fact]
+    [Fact(Skip = "Remove to run test")]
     public void Encode_numbers()
     {
         var ac = new AffineCipher();
         Assert.Equal("jqgjc rw123 jqgjc rw", ac.Encode("Testing,1 2 3, testing.",3,4));
     }
 
-    [Fact]
+    [Fact(Skip = "Remove to run test")]
     public void Encode_deep_thought()
     {
         var ac = new AffineCipher();
         Assert.Equal("iynia fdqfb ifje", ac.Encode("Truth is fiction.",5,17));
     }
 
-    [Fact]
+    [Fact(Skip = "Remove to run test")]
     public void Encode_all_the_letters()
     {
         var ac = new AffineCipher();
         Assert.Equal("swxtj npvyk lruol iejdc blaxk swxmh qzglf", ac.Encode("The quick brown fox jumps over the lazy dog.",17,33));
     }
 
-    [Fact]
+    [Fact(Skip = "Remove to run test")]
     public void Encode_with_a_not_coprime_to_m()
     {
         var ac = new AffineCipher();
@@ -69,49 +69,49 @@ public class AffineCipherTest
         Assert.Equal("a and m must be coprime.", ex.Message);
     }
 
-    [Fact]
+    [Fact(Skip = "Remove to run test")]
     public void Decode_exercism()
     {
         var ac = new AffineCipher();
         Assert.Equal("exercism", ac.Decode("tytgn fjr",3,7));
     }
 
-    [Fact]
+    [Fact(Skip = "Remove to run test")]
     public void Decode_a_sentence()
     {
         var ac = new AffineCipher();
         Assert.Equal("anobstacleisoftenasteppingstone", ac.Decode("qdwju nqcro muwhn odqun oppmd aunwd o",19,16));
     }
 
-    [Fact]
+    [Fact(Skip = "Remove to run test")]
     public void Decode_numbers()
     {
         var ac = new AffineCipher();
         Assert.Equal("testing123testing", ac.Decode("odpoz ub123 odpoz ub",25,7));
     }
 
-    [Fact]
+    [Fact(Skip = "Remove to run test")]
     public void Decode_all_the_letters()
     {
         var ac = new AffineCipher();
         Assert.Equal("thequickbrownfoxjumpsoverthelazydog", ac.Decode("swxtj npvyk lruol iejdc blaxk swxmh qzglf",17,33));
     }
 
-    [Fact]
+    [Fact(Skip = "Remove to run test")]
     public void Decode_with_no_spaces_in_input()
     {
         var ac = new AffineCipher();
         Assert.Equal("thequickbrownfoxjumpsoverthelazydog", ac.Decode("swxtjnpvyklruoliejdcblaxkswxmhqzglf",17,33));
     }
 
-    [Fact]
+    [Fact(Skip = "Remove to run test")]
     public void Decode_with_too_many_spaces()
     {
         var ac = new AffineCipher();
         Assert.Equal("jollygreengiant", ac.Decode("vszzm    cly   yd cg    qdp",15,16));
     }
 
-    [Fact]
+    [Fact(Skip = "Remove to run test")]
     public void Decode_with_a_not_coprime_to_m()
     {
         var ac = new AffineCipher();
