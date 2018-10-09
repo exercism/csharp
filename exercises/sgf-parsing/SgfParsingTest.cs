b818f9541f70c15bac410377af93cbf272a6b10a
Denis Capeto <denis.capeto@luizalabs.com>
2018-10-09 15:47:18 -0300

    Implemented test generator for SgfParsing exercise with the corresponding example file updated

--- exercises/sgf-parsing/SgfParsingTest.cs
+++ exercises/sgf-parsing/SgfParsingTest.cs
@@ -1,148 +1,92 @@
-﻿using System;
+// This file was auto-generated based on version 1.0.0 of the canonical data.
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using Xunit;
 
 public class SgfParsingTest
 {
-    // This exercises requires you to do somewhat more complicated parsing.
-    // This is the perfect exercise to get to know a parser library, of which
-    // there are several available. A simple one is Sprache (https://github.com/sprache/Sprache),
-    // a more complex one is Antlr (http://www.antlr.org/). You can also build
-    // your own parser of course!
-
-    // A tree consists of two parts:
-    // - The node's data: a map of type Dictionary<string, string list> 
-    // - A list of children (which are itself trees)
-
-    private static SgfTree TreeWithChildren(IDictionary<string, string[]> data, SgfTree[] children) => new SgfTree(data, children);
-    private static SgfTree TreeWithSingleChild(IDictionary<string, string[]> data, SgfTree child) => new SgfTree(data, child);
-    private static SgfTree TreeWithNoChildren(IDictionary<string, string[]> data) => new SgfTree(data);
-
-    private static IDictionary<string, string[]> CreateData(string key, params string[] values) => new Dictionary<string, string[]> { [key] = values };
-    
     [Fact]
-    public void Empty_value()
+    public void Empty_input()
     {
-        const string input = "";
-        Assert.Throws<ArgumentException>(() => SgfParser.ParseTree(input));
+        var encoded = "";
+        Assert.Throws<ArgumentException>(() => SgfParser.ParseTree(encoded));
     }
 
-    [Fact(Skip = "Remove to run test")]
-    public void Tree_without_nodes()
+    [Fact]
+    public void Tree_with_no_nodes()
     {
-        const string input = "()";
-        Assert.Throws<ArgumentException>(() => SgfParser.ParseTree(input));
+        var encoded = "()";
+        Assert.Throws<ArgumentException>(() => SgfParser.ParseTree(encoded));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Node_without_tree()
     {
-        const string input = ";";
-        Assert.Throws<ArgumentException>(() => SgfParser.ParseTree(input));
+        var encoded = ";";
+        Assert.Throws<ArgumentException>(() => SgfParser.ParseTree(encoded));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Node_without_properties()
     {
-        const string input = "(;)";
-        var expected = TreeWithNoChildren(new Dictionary<string, string[]>());
-        Assert.Equal(expected, SgfParser.ParseTree(input), SgfTreeEqualityComparer.Instance);
+        var encoded = "(;)";
+        Assert.Equal(new SgfTree(new Dictionary<string, string[]>()), SgfParser.ParseTree(encoded));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Single_node_tree()
     {
-        const string input = "(;A[B])";
-        var expected = TreeWithNoChildren(CreateData("A", "B"));
-        Assert.Equal(expected, SgfParser.ParseTree(input), SgfTreeEqualityComparer.Instance);
+        var encoded = "(;A[B])";
+        Assert.Equal(new SgfTree(new Dictionary<string, string[]> { ["A"] = new[] { "B" } } ), SgfParser.ParseTree(encoded));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Properties_without_delimiter()
     {
-        const string input = "(;a)";
-        Assert.Throws<ArgumentException>(() => SgfParser.ParseTree(input));
+        var encoded = "(;A)";
+        Assert.Throws<ArgumentException>(() => SgfParser.ParseTree(encoded));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void All_lowercase_property()
     {
-        const string input = "(;a[b])";
-        Assert.Throws<ArgumentException>(() => SgfParser.ParseTree(input));
+        var encoded = "(;a[b])";
+        Assert.Throws<ArgumentException>(() => SgfParser.ParseTree(encoded));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Upper_and_lowercase_property()
     {
-        const string input = "(;Aa[b])";
-        Assert.Throws<ArgumentException>(() => SgfParser.ParseTree(input));
+        var encoded = "(;Aa[b])";
+        Assert.Throws<ArgumentException>(() => SgfParser.ParseTree(encoded));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Two_nodes()
     {
-        const string input = "(;A[B];B[C])";
-        var expected = TreeWithSingleChild(CreateData("A", "B"), TreeWithNoChildren(CreateData("B", "C")));
-        Assert.Equal(expected, SgfParser.ParseTree(input), SgfTreeEqualityComparer.Instance);
+        var encoded = "(;A[B];B[C])";
+        Assert.Equal(new SgfTree(new Dictionary<string, string[]> { ["A"] = new[] { "B" } } ,new SgfTree(new Dictionary<string, string[]> { ["B"] = new[] { "C" } } )), SgfParser.ParseTree(encoded));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Two_child_trees()
     {
-        const string input = "(;A[B](;B[C])(;C[D]))";
-        var expected = TreeWithChildren(CreateData("A", "B"), 
-                            new[]
-                            {
-                                TreeWithNoChildren(CreateData("B", "C")),
-                                TreeWithNoChildren(CreateData("C", "D"))
-                            });
-        Assert.Equal(expected, SgfParser.ParseTree(input), SgfTreeEqualityComparer.Instance);
+        var encoded = "(;A[B](;B[C])(;C[D]))";
+        Assert.Equal(new SgfTree(new Dictionary<string, string[]> { ["A"] = new[] { "B" } } ,new SgfTree(new Dictionary<string, string[]> { ["B"] = new[] { "C" } } ), new SgfTree(new Dictionary<string, string[]> { ["C"] = new[] { "D" } } )), SgfParser.ParseTree(encoded));
     }
 
-    [Fact(Skip = "Remove to run test")]
-    public void Multiple_properties()
+    [Fact]
+    public void Multiple_property_values()
     {
-        const string input = "(;A[b][c][d])";
-        var expected = TreeWithNoChildren(CreateData("A", "b", "c", "d"));
-        Assert.Equal(expected, SgfParser.ParseTree(input), SgfTreeEqualityComparer.Instance);
+        var encoded = "(;A[b][c][d])";
+        Assert.Equal(new SgfTree(new Dictionary<string, string[]> { ["A"] = new[] { "b", "c", "d" } } ), SgfParser.ParseTree(encoded));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Escaped_property()
     {
-        const string input = @"(;A[\]b\nc\nd\t\te \n\]])";
-        var expected = TreeWithNoChildren(CreateData("A", "]b\nc\nd  e \n]"));
-        Assert.Equal(expected, SgfParser.ParseTree(input), SgfTreeEqualityComparer.Instance);
-    }
-
-    private class SgfTreeEqualityComparer : IEqualityComparer<SgfTree>
-    {
-        public static readonly SgfTreeEqualityComparer Instance = new SgfTreeEqualityComparer();
-
-        public bool Equals(SgfTree x, SgfTree y)
-        {
-            return SgfDataEqualityComparer.Instance.Equals(x.Data, y.Data) && x.Children.SequenceEqual(y.Children, Instance);
-        }
-
-        public int GetHashCode(SgfTree obj)
-        {
-            throw new System.NotImplementedException();
-        }
-    }
-
-    private class SgfDataEqualityComparer : IEqualityComparer<IDictionary<string, string[]>>
-    {
-        public static readonly SgfDataEqualityComparer Instance = new SgfDataEqualityComparer();
-
-        public bool Equals(IDictionary<string, string[]> x, IDictionary<string, string[]> y)
-        {
-            return x.Keys.SequenceEqual(y.Keys) && x.Keys.All(key => x[key].SequenceEqual(y[key]));
-        }
-
-        public int GetHashCode(IDictionary<string, string[]> obj)
-        {
-            throw new System.NotImplementedException();
-        }
+        var encoded = "(;A[\\]b\\nc\\nd\\t\\te \\n\\]])";
+        Assert.Equal(new SgfTree(new Dictionary<string, string[]> { ["A"] = new[] { "]b\nc\nd  e \n]" } } ), SgfParser.ParseTree(encoded));
     }
 }
---
+++ generators/Exercises/Generators/SgfParsing.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Exercism.CSharp.Output;
+using Exercism.CSharp.Output.Rendering;
+using Newtonsoft.Json.Linq;
+
+namespace Exercism.CSharp.Exercises.Generators
+{
+    public class SgfParsing : GeneratorExercise
+    {
+        protected override void UpdateTestMethod(TestMethod testMethod)
+        {
+            if (testMethod.Expected is Dictionary<string, object> &&
+                (testMethod.Expected as Dictionary<string, object>).ContainsKey("error"))
+                testMethod.ExceptionThrown = typeof(ArgumentException);
+            else
+                testMethod.Expected = RenderTree(testMethod.Expected);
+
+            testMethod.TestedClass = "SgfParser";
+            testMethod.TestedMethod = "ParseTree";
+
+            testMethod.UseVariablesForInput = true;
+
+            testMethod.Input["encoded"] = testMethod.Input.FirstOrDefault().Value.Replace("\\","\\\\");
+
+        }
+
+        protected override void UpdateNamespaces(ISet<string> namespaces)
+        {
+            namespaces.Add(typeof(ArgumentException).Namespace);
+            namespaces.Add(typeof(Dictionary<string, string[]>).Namespace);
+        }
+
+        private UnescapedValue RenderTree(dynamic tree)
+        {
+            if (tree == null)
+            {
+                return null;
+            }
+
+            var label = Render.Object(tree["properties"]);
+            label = (label as string).Replace("object", "string[]");
+            if (tree.ContainsKey("children"))
+            {
+                var children = string.Empty;
+
+                if(tree["children"] is JArray)
+                    children = string.Join(",", ((JArray)tree["children"]).Select(RenderTree));
+
+                if (tree["children"] is object[])
+                    children = string.Join(", ", ((object[])tree["children"]).Select(RenderTree));
+
+                if (!string.IsNullOrEmpty(children))
+                    children = "," + children;
+
+                return new UnescapedValue($"new SgfTree({label} {children})");
+            }
+
+            return new UnescapedValue($"new SgfTree({label})");
+        }
+    }
+}
