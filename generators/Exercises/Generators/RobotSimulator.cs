628027764e21e02859415f7219048ffd73eee090
Erik Schierboom <erik_schierboom@hotmail.com>
2018-11-09 08:13:01 +0100

    robot-simulator: cleanup generator and remove unused methods from stub (#725)

--- generators/Exercises/Generators/RobotSimulator.cs
+++ generators/Exercises/Generators/RobotSimulator.cs
@@ -10,33 +10,24 @@ namespace Exercism.CSharp.Exercises.Generators
         protected override void UpdateTestMethod(TestMethod testMethod)
         {
             testMethod.Input["direction"] = RenderDirection(testMethod.Input["direction"]);
-            testMethod.Input["coordinate"] = RenderCreateCoordinate(testMethod.Input["position"]);
+            testMethod.Input["x"] = testMethod.Input["position"]["x"];
+            testMethod.Input["y"] = testMethod.Input["position"]["y"];
 
-            testMethod.ConstructorInputParameters = new[] { "direction", "coordinate" };
+            testMethod.ConstructorInputParameters = new[] { "direction", "x", "y" };
             testMethod.TestedMethodType = TestedMethodType.InstanceMethod;
             testMethod.TestMethodName = testMethod.TestMethodNameWithPath;
-            testMethod.TestedClass = "Robot";
 
             testMethod.Act = RenderAct(testMethod);
             testMethod.Assert = RenderAssert(testMethod);
         }
 
         private string RenderAct(TestMethod testMethod)
-        {
-            switch (testMethod.Property)
-            {
-                case "create": return null;
-                case "move": return RenderInstructionsAct(testMethod);
-                default: return RenderDefaultAct(testMethod);
-            }
-        }
+            => testMethod.Property == "move" ? RenderMoveAct(testMethod) : null;
 
-        private static string RenderDefaultAct(TestMethod testMethod) => $"sut.{testMethod.TestedMethod}();";
-
-        private string RenderInstructionsAct(TestMethod testMethod)
+        private string RenderMoveAct(TestMethod testMethod)
         {
             var actual = Render.Object(testMethod.Input["instructions"]);
-            return $"sut.Simulate({actual});";
+            return $"sut.{testMethod.TestedMethod}({actual});";
         }
 
         private string RenderAssert(TestMethod testMethod)
@@ -47,18 +38,12 @@ namespace Exercism.CSharp.Exercises.Generators
 
             var assert = new StringBuilder();
 
-            if (direction != null)
-            {
-                var expectedDirection = RenderDirection(direction);
-                assert.AppendLine(Render.AssertEqual(Render.Object(expectedDirection), "sut.Direction"));
-            }
-
-            if (position == null)
-                return assert.ToString();
+            var expectedDirection = RenderDirection(direction);
+            assert.AppendLine(Render.AssertEqual(Render.Object(expectedDirection), "sut.Direction"));
 
             var x = Render.Object(position["x"]);
             var y = Render.Object(position["y"]);
-                
+
             assert.AppendLine(Render.AssertEqual(x, "sut.X"));
             assert.AppendLine(Render.AssertEqual(y, "sut.Y"));
 
@@ -67,8 +52,5 @@ namespace Exercism.CSharp.Exercises.Generators
 
         private UnescapedValue RenderDirection(string direction)
             => Render.Enum("Direction", direction);
-
-        private static UnescapedValue RenderCreateCoordinate(dynamic coordinates)
-            => new UnescapedValue($"{coordinates["x"]}, {coordinates["y"]}");
     }
 }
