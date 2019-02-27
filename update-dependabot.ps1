62faabd2f5cf41bc061b988fda0f3a45f5ecc6e6
Rob Keim <robkeim@gmail.com>
2019-02-27 14:09:55 +0700

    Auto-generate dependabot's config.yml (#775)
    * Auto-generate dependabot's config.yml
    
    * Write to config file once at end of execution
    
    * Update new exercise template to include updating dependabot's projects to watch

---
+++ update-dependabot.ps1
@@ -0,0 +1,22 @@
+$output = [System.Text.StringBuilder]::new()
+
+[void]$output.AppendLine("version: 1")
+[void]$output.AppendLine("")
+[void]$output.AppendLine("update_configs:")
+
+$paths = Get-ChildItem -Recurse -Path *.csproj
+
+foreach ($path in $paths)
+{
+	$dir = $path.Directory
+	$relativePath = Resolve-Path -Relative $dir
+	$relativePath = $relativePath.Replace(".\", "/")
+	$relativePath = $relativePath.Replace("\", "/")
+	
+	[void]$output.AppendLine("    - package_manager: `"dotnet:nuget`"")
+	[void]$output.AppendLine("      directory: `"" + $relativePath + "`"")
+	[void]$output.AppendLine("      update_schedule: `"live`"")
+	[void]$output.AppendLine("")
+}
+
+Set-Content "./.dependabot/config.yml" $output.ToString()
