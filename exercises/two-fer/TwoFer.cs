e10ee93f61935c55388f7b510ad4852a684a96d4
Erik Schierboom <erik_schierboom@hotmail.com>
2019-03-19 10:51:18 +0100

    two-fer: fix indentation of example file (#1251)

--- exercises/two-fer/TwoFer.cs
+++ exercises/two-fer/TwoFer.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 
-  public static class TwoFer
-  {
-      public static string Name(string input = null)
-      {
-          throw new NotImplementedException("You need to implement this function.");
-      }
-  }
+public static class TwoFer
+{
+    public static string Name(string input = null)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
