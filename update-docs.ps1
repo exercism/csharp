9360edee6dd501c08bb30625835cabd8b5c9182d
Erik Schierboom <erik_schierboom@hotmail.com>
2019-04-13 12:27:36 +0200

    scripts: allow passing argument to configlet to regenerate a single exercise's README

--- update-docs.ps1
+++ update-docs.ps1
@@ -1,2 +1,5 @@
+# Usage: .\update-docs.ps1. This will regenerate the docs for all exercises.
+# Usage: .\update-docs.ps1 -o <exercise>. This will regenerate the docs for the specified exercise.
+
 .\bin\fetch-configlet
-.\bin\configlet generate .
+.\bin\configlet generate . $args
