f041a2e1fad68ce644a3bfbe05e1b193333a3ec0
Erik Schierboom <erik_schierboom@hotmail.com>
2019-04-13 13:16:53 +0200

    docs: don't depend on external problem-specifications repo

--- update-docs.ps1
+++ update-docs.ps1
@@ -1,5 +1,8 @@
 # Usage: .\update-docs.ps1. This will regenerate the docs for all exercises.
 # Usage: .\update-docs.ps1 -o <exercise>. This will regenerate the docs for the specified exercise.
 
+git submodule init
+git submodule update --remote
+
 .\bin\fetch-configlet
-.\bin\configlet generate . $args
+.\bin\configlet generate . -p problem-specifications $args
