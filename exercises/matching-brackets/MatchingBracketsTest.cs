e0321297554d89bd0fed0f1adfe81bfff9f61eaa
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2019-04-15 12:34:59 +0200

    bracket-push: rename to matching-brackets
    This rename was proposed in:
    exercism/problem-specifications#1501
    
    The rationale for the name is:
    
    * to name the exercise by its story, not by what it potentially teaches
    * to avoid unnecessarily biasing the solution space

--- exercises/matching-brackets/MatchingBracketsTest.cs
+++ exercises/matching-brackets/MatchingBracketsTest.cs
@@ -2,124 +2,124 @@
 
 using Xunit;
 
-public class BracketPushTest
+public class MatchingBracketsTest
 {
     [Fact]
     public void Paired_square_brackets()
     {
         var value = "[]";
-        Assert.True(BracketPush.IsPaired(value));
+        Assert.True(MatchingBrackets.IsPaired(value));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Empty_string()
     {
         var value = "";
-        Assert.True(BracketPush.IsPaired(value));
+        Assert.True(MatchingBrackets.IsPaired(value));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Unpaired_brackets()
     {
         var value = "[[";
-        Assert.False(BracketPush.IsPaired(value));
+        Assert.False(MatchingBrackets.IsPaired(value));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Wrong_ordered_brackets()
     {
         var value = "}{";
-        Assert.False(BracketPush.IsPaired(value));
+        Assert.False(MatchingBrackets.IsPaired(value));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Wrong_closing_bracket()
     {
         var value = "{]";
-        Assert.False(BracketPush.IsPaired(value));
+        Assert.False(MatchingBrackets.IsPaired(value));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Paired_with_whitespace()
     {
         var value = "{ }";
-        Assert.True(BracketPush.IsPaired(value));
+        Assert.True(MatchingBrackets.IsPaired(value));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Partially_paired_brackets()
     {
         var value = "{[])";
-        Assert.False(BracketPush.IsPaired(value));
+        Assert.False(MatchingBrackets.IsPaired(value));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Simple_nested_brackets()
     {
         var value = "{[]}";
-        Assert.True(BracketPush.IsPaired(value));
+        Assert.True(MatchingBrackets.IsPaired(value));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Several_paired_brackets()
     {
         var value = "{}[]";
-        Assert.True(BracketPush.IsPaired(value));
+        Assert.True(MatchingBrackets.IsPaired(value));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Paired_and_nested_brackets()
     {
         var value = "([{}({}[])])";
-        Assert.True(BracketPush.IsPaired(value));
+        Assert.True(MatchingBrackets.IsPaired(value));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Unopened_closing_brackets()
     {
         var value = "{[)][]}";
-        Assert.False(BracketPush.IsPaired(value));
+        Assert.False(MatchingBrackets.IsPaired(value));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Unpaired_and_nested_brackets()
     {
         var value = "([{])";
-        Assert.False(BracketPush.IsPaired(value));
+        Assert.False(MatchingBrackets.IsPaired(value));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Paired_and_wrong_nested_brackets()
     {
         var value = "[({]})";
-        Assert.False(BracketPush.IsPaired(value));
+        Assert.False(MatchingBrackets.IsPaired(value));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Paired_and_incomplete_brackets()
     {
         var value = "{}[";
-        Assert.False(BracketPush.IsPaired(value));
+        Assert.False(MatchingBrackets.IsPaired(value));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Too_many_closing_brackets()
     {
         var value = "[]]";
-        Assert.False(BracketPush.IsPaired(value));
+        Assert.False(MatchingBrackets.IsPaired(value));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Math_expression()
     {
         var value = "(((185 + 223.85) * 15) - 543)/2";
-        Assert.True(BracketPush.IsPaired(value));
+        Assert.True(MatchingBrackets.IsPaired(value));
     }
 
     [Fact(Skip = "Remove to run test")]
     public void Complex_latex_expression()
     {
         var value = "\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)";
-        Assert.True(BracketPush.IsPaired(value));
+        Assert.True(MatchingBrackets.IsPaired(value));
     }
 }
--- exercises/matching-brackets/README.md
+++ exercises/matching-brackets/README.md
@@ -1,4 +1,4 @@
-# Bracket Push
+# Matching Brackets
 
 Given a string containing brackets `[]`, braces `{}`, parentheses `()`,
 or any combination thereof, verify that any and all pairs are matched
@@ -10,8 +10,8 @@ To run the tests, run the command `dotnet test` from within the exercise directo
 
 Initially, only the first test will be enabled. This is to encourage you to solve the exercise one step at a time.
 Once you get the first test passing, remove the `Skip` property from the next test and work on getting that test passing.
-Once none of the tests are skipped and they are all passing, you can submit your solution 
-using `exercism submit BracketPush.cs`
+Once none of the tests are skipped and they are all passing, you can submit your solution
+using `exercism submit MatchingBrackets.cs`
 
 ## Further information
 
