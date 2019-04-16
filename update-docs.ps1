ca1fd68601eef90b5ebd2a41ddf9b23223b181f4
Erik Schierboom <erik_schierboom@hotmail.com>
2019-04-16 14:35:06 +0200

    scripts: allow passing argument to configlet to regenerate a single README (#1275)
    * scripts: allow passing argument to configlet to regenerate a single exercise's README
    
    * docs: don't depend on external problem-specifications repo

--- update-docs.ps1
+++ update-docs.ps1
@@ -1,2 +1,8 @@
+# Usage: .\update-docs.ps1. This will regenerate the docs for all exercises.
+# Usage: .\update-docs.ps1 -o <exercise>. This will regenerate the docs for the specified exercise.
+
+git submodule init
+git submodule update --remote
+
 .\bin\fetch-configlet
-.\bin\configlet generate .
+.\bin\configlet generate . -p problem-specifications $args
