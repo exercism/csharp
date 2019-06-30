c08c1d8c6d8f5f0a2819b6b80c6e0d6bee537076
Dominik Mahnkopf <22577070+dmahnkopf@users.noreply.github.com>
2019-06-30 16:23:26 +0200

    two-fer: fix compilation issues (#1290)
    * two-fer: fix compilation issues
    
    * two-fer: exchange parameter with comment
    
    * two-fer: adjust comment
    
    Making it more readable
    
    Co-Authored-By: Erik Schierboom <erik_schierboom@hotmail.com>

--- exercises/two-fer/TwoFer.cs
+++ exercises/two-fer/TwoFer.cs
@@ -1,5 +1,9 @@
-﻿public static class TwoFer
+﻿using System;
+
+public static class TwoFer
 {
+    // In order to get the tests running, first you need to make sure the Speak method 
+    // can be called both without any arguments and also by passing one string argument.
     public static string Speak()
     {
         throw new NotImplementedException("You need to implement this function.");
