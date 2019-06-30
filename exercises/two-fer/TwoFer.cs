14bfcb6a5e1058cfaad3243430b010d71466be98
Dominik Mahnkopf <22577070+dmahnkopf@users.noreply.github.com>
2019-06-30 16:21:22 +0200

    two-fer: adjust comment
    Making it more readable
    
    Co-Authored-By: Erik Schierboom <erik_schierboom@hotmail.com>

--- exercises/two-fer/TwoFer.cs
+++ exercises/two-fer/TwoFer.cs
@@ -2,7 +2,8 @@
 
 public static class TwoFer
 {
-    // In order to get the tests running, first you need to make sure the Speak method can be run both without any and also by passing one string argument.
+    // In order to get the tests running, first you need to make sure the Speak method 
+    // can be called both without any arguments and also by passing one string argument.
     public static string Speak()
     {
         throw new NotImplementedException("You need to implement this function.");
