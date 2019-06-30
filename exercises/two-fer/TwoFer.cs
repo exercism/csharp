4c2faa787cb1676e65e396cea59e9a459487efa8
Dominik Mahnkopf <22577070+dmahnkopf@users.noreply.github.com>
2019-06-30 16:13:50 +0200

    two-fer: exchange parameter with comment

--- exercises/two-fer/TwoFer.cs
+++ exercises/two-fer/TwoFer.cs
@@ -2,7 +2,8 @@
 
 public static class TwoFer
 {
-    public static string Speak(string input = "")
+    // In order to get the tests running, first you need to make sure the Speak method can be run both without any and also by passing one string argument.
+    public static string Speak()
     {
         throw new NotImplementedException("You need to implement this function.");
     }
