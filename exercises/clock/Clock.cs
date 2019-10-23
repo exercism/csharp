82353cd179d4a96dc462b2d7653852c4423b4731
Erik Schierboom <erik_schierboom@hotmail.com>
2019-10-23 18:25:31 +0200

    Update Clock.cs (#1328)

--- exercises/clock/Clock.cs
+++ exercises/clock/Clock.cs
@@ -7,33 +7,12 @@ public class Clock
         throw new NotImplementedException("You need to implement this function.");
     }
 
-    public int Hours
-    {
-        get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
-    }
-
-    public int Minutes
-    {
-        get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
-    }
-
     public Clock Add(int minutesToAdd)
     {
         throw new NotImplementedException("You need to implement this function.");
     }
 
     public Clock Subtract(int minutesToSubtract)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-
-    public override string ToString()
     {
         throw new NotImplementedException("You need to implement this function.");
     }
