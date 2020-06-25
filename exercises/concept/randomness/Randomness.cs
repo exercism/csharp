44823b55b732e629d3d816a9f5d37b97de109c66
Mike May <mikemay.mdamay@googlemail.com>
2020-06-25 11:37:13 +0100

    Add randomness Concept Exercise
    * randomness added files from v3-dashboard bot branch
    
    * randomness created tree and moved in files
    
    * [CI] Format C# code
    
    * randomness commit after messing up git.  All looks ok Example.cs and instructions.md complete
    
    * randomness complete except hints.md and csharp config
    
    * randomness prettier
    
    * reandomness tweaks
    
    * randomness proofing
    
    * randomness cut-paste error fixed
    
    * Update languages/exercises/concept/randomness/.meta/design.md
    
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>
    
    * randomness dropped thread safety as topic.
    
    * randomness improved RollDie test
    
    * Update languages/exercises/concept/randomness/RandomnessTests.cs
    
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>
    
    * randomness hints.md
    
    * randomness all files complete after review
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>

---
+++ exercises/concept/randomness/Randomness.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Player
+{
+    public int RollDie()
+    {
+        throw new NotImplementedException("Please implement the Player.RollDie() method");
+    }
+
+    public double GenerateSpellStrength()
+    {
+        throw new NotImplementedException("Please implement the Player.GenerateSpellStrength() method");
+    }
+}
