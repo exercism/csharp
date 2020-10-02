bfc53dcb241b96fbb85b4ef488ce05994ac1ef28
Ashley Stallings Rhodes <aesdev421@gmail.com>
2020-10-02 15:59:21 -0500

    resistor-color-duo - skip all but first test

--- exercises/resistor-color-duo/ResistorColorDuoTests.cs
+++ exercises/resistor-color-duo/ResistorColorDuoTests.cs
@@ -10,25 +10,25 @@ public class ResistorColorDuoTests
         Assert.Equal(10, ResistorColorDuo.Value(new[] { "brown", "black" }));
     }
 
-    [Fact()]
+    [Fact(Skip = "Remove this Skip property to run this test")]
     public void Blue_and_grey()
     {
         Assert.Equal(68, ResistorColorDuo.Value(new[] { "blue", "grey" }));
     }
 
-    [Fact()]
+    [Fact(Skip = "Remove this Skip property to run this test")]
     public void Yellow_and_violet()
     {
         Assert.Equal(47, ResistorColorDuo.Value(new[] { "yellow", "violet" }));
     }
 
-    [Fact()]
+    [Fact(Skip = "Remove this Skip property to run this test")]
     public void Orange_and_orange()
     {
         Assert.Equal(33, ResistorColorDuo.Value(new[] { "orange", "orange" }));
     }
 
-    [Fact()]
+    [Fact(Skip = "Remove this Skip property to run this test")]
     public void Ignore_additional_colors()
     {
         Assert.Equal(51, ResistorColorDuo.Value(new[] { "green", "brown", "orange" }));
