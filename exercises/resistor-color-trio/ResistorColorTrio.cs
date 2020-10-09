1d3f89cbe7d8c8dfbf24fee5015377ffcbba46b8
Valentin Pouget <3833193+valentin-p@users.noreply.github.com>
2020-10-09 21:14:30 +0200

    reset commit

--- exercises/resistor-color-trio/ResistorColorTrio.cs
+++ exercises/resistor-color-trio/ResistorColorTrio.cs
@@ -4,6 +4,6 @@ public static class ResistorColorTrio
 {
     public static string Label(string[] colors)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        throw new NotImplementedException("You  to implement this function.");
     }
 }
