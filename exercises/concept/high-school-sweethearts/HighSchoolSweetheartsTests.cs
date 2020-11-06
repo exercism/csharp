5bf0e1dfbd7fea2858afd757f205b06949d018ec
Mike May <mikemay.mdamay@googlemail.com>
2020-11-06 09:16:59 -0500

    rename high school sweethearts classes
    manual construction of about.md to avoid merge conflicts
    
    * high-school-sweethearts - changed class name

--- exercises/concept/high-school-sweethearts/HighSchoolSweetheartsTests.cs
+++ exercises/concept/high-school-sweethearts/HighSchoolSweetheartsTests.cs
@@ -24,20 +24,20 @@ public class StringFormattingTests
     public void DisplaySingleLine()
     {
         const string expected = "                  Lance Green ♡ Pat Riley                    ";
-        Assert.Equal(expected, HighSchoolSweetheart.DisplaySingleLine("Lance Green", "Pat Riley"));
+        Assert.Equal(expected, HighSchoolSweethearts.DisplaySingleLine("Lance Green", "Pat Riley"));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void DisplayBanner()
     {
-        string actualBanner = HighSchoolSweetheart.DisplayBanner("L. G. ", "P. R. ");
+        string actualBanner = HighSchoolSweethearts.DisplayBanner("L. G. ", "P. R. ");
         Assert.Equal(expectedBanner.Trim(), actualBanner.Trim());
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void DisplayGermanExchangeStudents()
     {
-        string actual = HighSchoolSweetheart.DisplayGermanExchangeStudents("Norbert", "Heidi",
+        string actual = HighSchoolSweethearts.DisplayGermanExchangeStudents("Norbert", "Heidi",
             new DateTime(2019, 1, 22), 1535.22f);
         Assert.Equal("Norbert and Heidi have been dating since 22.01.2019 - that's 1.535,22 hours"
             , actual);
