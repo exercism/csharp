cbe402a7169b96de3f3d8b2f9bb6a8085a23d9a8
Pramod Jangam <pramod.jangam@deserve.com>
2020-11-25 14:51:17 +0530

    Added test case for idempotency of `PersonalTopThree` method in `HighScoresTests` to generators

--- generators/Exercises/Generators/HighScores.cs
+++ generators/Exercises/Generators/HighScores.cs
@@ -30,6 +30,14 @@ public void Latest_score_should_not_change_after_calling_personal_best()
     var sut = new HighScores(new List<int> { 20, 10, 30, 3, 2, 1 });
     Assert.Equal(30, sut.PersonalBest());
     Assert.Equal(1, sut.Latest());
+}
+
+[Fact(Skip = ""Remove this Skip property to run this test"")]
+public void Latest_score_should_not_change_after_calling_personal_top_three()
+{
+    var sut = new HighScores(new List<int> { 20, 100, 30, 90, 2, 70 });
+    Assert.Equal(new List<int> { 100, 90, 70 }, sut.PersonalTopThree());
+    Assert.Equal(70, sut.Latest());
 }");
         }
     }
