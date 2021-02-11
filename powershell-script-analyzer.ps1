f56e5ba9547834be99bd2a9967f8d57703d11082
Valentin Pouget <3833193+valentin-p@users.noreply.github.com>
2021-02-11 08:12:48 +0100

    add comments and remove header for pssa

--- powershell-script-analyzer.ps1
+++ powershell-script-analyzer.ps1
@@ -1,3 +1,14 @@
+<#
+.SYNOPSIS
+    Test the PowerShell scripts in the folders.
+.DESCRIPTION
+    Test the PowerShell strict correctness with https://github.com/PowerShell/PSScriptAnalyzer.
+.EXAMPLE
+    The example below will verify all the scripts in the repository
+    PS C:\> ./powershell-script-analyzer.ps1
+#>
+
+# Install PSScriptAnalyzer only if not already available.
 if (!(Get-Module -ListAvailable -Name PSScriptAnalyzer)) {
     Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
     Install-Module PSScriptAnalyzer -Scope CurrentUser -Repository PSGallery -Force
@@ -10,8 +21,4 @@ $DETAILS
 $SUMMARY = ($PSSAResults | Group-Object -Property Severity -NoElement | Foreach-Object { "$($_.Count) $($_.Name)" }) -join ", "
 $SUMMARY
 
-if ($SUMMARY.Count -ne 0) {
-    throw
-}
-
-exit $LastExitCode
+exit $SUMMARY.Count
