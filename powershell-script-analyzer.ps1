3e9b727d39c7fb1c13f72a52be42ba68b31bac9f
valentin-p <3833193+valentin-p@users.noreply.github.com>
2021-02-11 11:03:52 +0100

    PowerShell script analyzer and CI (#1496)
    * ps1 script analyzer and yml ci
    
    * analyze and check package
    
    * add comments and remove header for pssa

---
+++ powershell-script-analyzer.ps1
@@ -0,0 +1,24 @@
+<#
+.SYNOPSIS
+    Test the PowerShell scripts in the folders.
+.DESCRIPTION
+    Test the PowerShell strict correctness with https://github.com/PowerShell/PSScriptAnalyzer.
+.EXAMPLE
+    The example below will verify all the scripts in the repository
+    PS C:\> ./powershell-script-analyzer.ps1
+#>
+
+# Install PSScriptAnalyzer only if not already available.
+if (!(Get-Module -ListAvailable -Name PSScriptAnalyzer)) {
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+    Install-Module PSScriptAnalyzer -Scope CurrentUser -Repository PSGallery -Force
+}
+
+Import-Module PSScriptAnalyzer
+$PSSAResults = Invoke-ScriptAnalyzer -Path . -ExcludeRule PSAvoidTrailingWhitespace -Recurse
+$DETAILS = ($PSSAResults | ForEach-Object { "{0,-11} [{1,-3}, {2,-3}] {3,-50} {4,-35} - {5}" -f $_.Severity, $_.Line, $_.Column, $_.ScriptName, $_.RuleName, $_.Message } | Out-String -Width 88).Trim()
+$DETAILS
+$SUMMARY = ($PSSAResults | Group-Object -Property Severity -NoElement | Foreach-Object { "$($_.Count) $($_.Name)" }) -join ", "
+$SUMMARY
+
+exit $SUMMARY.Count
