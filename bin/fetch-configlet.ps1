016adf632677683fa5979aa8882595e9240b5796
Valentin Pouget <3833193+valentin-p@users.noreply.github.com>
2021-02-11 12:02:53 +0100

    fix fetch configlet positional parameter

--- bin/fetch-configlet.ps1
+++ bin/fetch-configlet.ps1
@@ -1,19 +1,20 @@
 $ErrorActionPreference = "Stop"
 $ProgressPreference = "SilentlyContinue"
 
-function Get-DownloadUrl($FileName) {
+$requestOpts = @{
+    Headers           = If ($env:GITHUB_TOKEN) { @{ Authorization = "Bearer ${env:GITHUB_TOKEN}" } } Else { @{ } }
+    MaximumRetryCount = 3
+    RetryIntervalSec  = 1
+}
+
+function Get-DownloadUrl($FileName, $RequestOpts) {
     if ($PSVersionTable.PSVersion.Major -le 5) {
         # PreserveAuthorizationOnRedirect requires PowerShell 7+
         throw "Please upgrade to PowerShell Core. For example: 'dotnet tool install --global PowerShell'"
     }
-
-    $requestOpts = @{
-        Headers           = If ($env:GITHUB_TOKEN) { @{ Authorization = "Bearer ${env:GITHUB_TOKEN}" } } Else { @{ } }
-        MaximumRetryCount = 3
-        RetryIntervalSec  = 1
-    }
+    
     $latestUrl = "https://api.github.com/repos/exercism/configlet/releases/latest"
-    $assets = Invoke-RestMethod -Uri $latestUrl -PreserveAuthorizationOnRedirect @requestOpts | Select-Object -ExpandProperty assets
+    $assets = Invoke-RestMethod -Uri $latestUrl -PreserveAuthorizationOnRedirect @RequestOpts | Select-Object -ExpandProperty assets
     $assets | Where-Object { $_.browser_download_url -match $FileName } | Select-Object -ExpandProperty browser_download_url
 }
 
@@ -21,8 +22,8 @@ $outputDirectory = "bin"
 $arch = If ([Environment]::Is64BitOperatingSystem) { "64bit" } Else { "32bit" }
 $fileName = "configlet-windows-$arch.zip"
 
-$downloadUrl = Get-DownloadUrl $fileName
+$downloadUrl = Get-DownloadUrl $fileName $requestOpts
 $outputFile = Join-Path -Path $outputDirectory -ChildPath $fileName
-Invoke-WebRequest -Uri $downloadUrl -OutFile $outputFile @requestOpts
+Invoke-WebRequest -Uri $downloadUrl -OutFile $outputFile -PreserveAuthorizationOnRedirect @requestOpts
 Expand-Archive $outputFile -DestinationPath $outputDirectory -Force
 Remove-Item -Path $outputFile
