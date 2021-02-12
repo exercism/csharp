8592a9a57da7711c924135f3108e6c5ceb18fffd
Valentin Pouget <3833193+valentin-p@users.noreply.github.com>
2021-02-12 08:36:54 +0100

    find a better name

--- shared.ps1
+++ shared.ps1
@@ -4,16 +4,16 @@ $ErrorActionPreference = 'Stop'
 # There is a pending proposal to support this:
 # https://github.com/PowerShell/PowerShell-RFC/pull/88/files
 # https://github.com/PowerShell/PowerShell-RFC/pull/261
-function Invoke-CommandExecution ($Command) {
+function Invoke-ExpressionExitOnError ($Command) {
     <#
         .SYNOPSIS
             Run a native command.
         .PARAMETER Command
             The command to execute.
         .EXAMPLE
-            The example below runs the "./bin/configlet hint ." command
+            The example below runs the "./bin/configlet lint" command
 
-            Invoke-CommandExecution "./bin/configlet hint ."
+            Invoke-ExpressionExitOnError "./bin/configlet lint"
     #>
 
     Invoke-Expression -Command $Command
