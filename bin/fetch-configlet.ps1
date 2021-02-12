ba90d9044ec8138862526e51c6b0d93090cc8108
Valentin Pouget <3833193+valentin-p@users.noreply.github.com>
2021-02-12 08:34:48 +0100

    change fetch configlet

--- bin/fetch-configlet.ps1
+++ bin/fetch-configlet.ps1
@@ -1,26 +1,28 @@
 $ErrorActionPreference = "Stop"
 $ProgressPreference = "SilentlyContinue"
 
-$requestOpts = @{
-    Headers           = If ($env:GITHUB_TOKEN) { @{ Authorization = "Bearer ${env:GITHUB_TOKEN}" } } Else { @{ } }
-    MaximumRetryCount = 3
-    RetryIntervalSec  = 1
+if ($PSVersionTable.PSVersion.Major -le 5) {
+    # PreserveAuthorizationOnRedirect requires PowerShell Core 7+
+    # TODO create a tool that install pwsh in the directory https://docs.microsoft.com/en-us/dotnet/core/tools/local-tools-how-to-use
+    throw "Please run with PowerShell Core 'pwsh'. If you need to install pwsh globally: 'dotnet tool install --global PowerShell'"
 }
 
 function Get-DownloadUrl($FileName, $RequestOpts) {
-    if ($PSVersionTable.PSVersion.Major -le 5) {
-        # PreserveAuthorizationOnRedirect requires PowerShell 7+
-        throw "Please upgrade to PowerShell Core. For example: 'dotnet tool install --global PowerShell'"
-    }
-    
     $latestUrl = "https://api.github.com/repos/exercism/configlet/releases/latest"
     $assets = Invoke-RestMethod -Uri $latestUrl -PreserveAuthorizationOnRedirect @RequestOpts | Select-Object -ExpandProperty assets
     $assets | Where-Object { $_.browser_download_url -match $FileName } | Select-Object -ExpandProperty browser_download_url
 }
 
+# Use GITHUB_TOKEN to download the configlet metadata and binaries from github releases for github actions.
+$requestOpts = @{
+    Headers           = If ($env:GITHUB_TOKEN) { @{ Authorization = "Bearer ${env:GITHUB_TOKEN}" } } Else { @{ } }
+    MaximumRetryCount = 3
+    RetryIntervalSec  = 1
+}
 $outputDirectory = "bin"
 $arch = If ([Environment]::Is64BitOperatingSystem) { "64bit" } Else { "32bit" }
 $fileName = "configlet-windows-$arch.zip"
+
 
 $downloadUrl = Get-DownloadUrl $fileName $requestOpts
 $outputFile = Join-Path -Path $outputDirectory -ChildPath $fileName
