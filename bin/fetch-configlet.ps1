5cc8de18091863e20191c168b65b76376ce3b6b4
RadicalBeastGames <kushnikov.mikhail@gmail.com>
2021-02-27 22:30:33 +0200

    Canonical data syncer update(#1425)
    * updating ps1 script from the canonical-data-syncer repo

--- bin/fetch-configlet.ps1
+++ bin/fetch-configlet.ps1
@@ -1,34 +1,26 @@
 $ErrorActionPreference = "Stop"
 $ProgressPreference = "SilentlyContinue"
 
-if ($PSVersionTable.PSVersion.Major -le 5) {
-    # PreserveAuthorizationOnRedirect requires PowerShell Core 7+
-    # TODO create a tool that install pwsh in the directory https://docs.microsoft.com/en-us/dotnet/core/tools/local-tools-how-to-use
-    throw "Please run with PowerShell Core 'pwsh'. If you need to install pwsh globally: 'dotnet tool install --global PowerShell'"
-}
-
-function Get-DownloadUrl($FileName, $RequestOpts) {
-    $latestUrl = "https://api.github.com/repos/exercism/configlet/releases/latest"
-    $assets = Invoke-RestMethod -Uri $latestUrl -PreserveAuthorizationOnRedirect @RequestOpts | Select-Object -ExpandProperty assets
-    return $assets | Where-Object { $_.browser_download_url -match $FileName } | Select-Object -ExpandProperty browser_download_url -First 1
-}
-
-# Use GITHUB_TOKEN to download the configlet metadata and binaries from github releases for github actions.
 $requestOpts = @{
-    Headers           = If ($env:GITHUB_TOKEN) { @{ Authorization = "Bearer ${env:GITHUB_TOKEN}" } } Else { @{ } }
+    Headers = If ($env:GITHUB_TOKEN) { @{ Authorization = "Bearer ${env:GITHUB_TOKEN}" } } Else { @{ } }
     MaximumRetryCount = 3
-    RetryIntervalSec  = 1
+    RetryIntervalSec = 1
 }
-$outputDirectory = "bin"
+
 $arch = If ([Environment]::Is64BitOperatingSystem) { "64bit" } Else { "32bit" }
 $fileName = "configlet-windows-$arch.zip"
 
-Write-Output "Fetching configlet download URL"
-$downloadUrl = Get-DownloadUrl -FileName $fileName -RequestOpts $requestOpts
-$outputFile = Join-Path -Path $outputDirectory -ChildPath $fileName
+Function Get-DownloadUrl {
+    $latestUrl = "https://api.github.com/repos/exercism/configlet/releases/latest"
+    Invoke-RestMethod -Uri $latestUrl -PreserveAuthorizationOnRedirect @requestOpts
+    | Select-Object -ExpandProperty assets
+    | Where-Object { $_.browser_download_url -match $FileName }
+    | Select-Object -ExpandProperty browser_download_url
+}
 
-Write-Output "Fetching configlet binaries"
-# Use PreserveAuthorizationOnRedirect named parameter doesn't work on WSL
+$downloadUrl = Get-DownloadUrl
+$outputDirectory = "bin"
+$outputFile = Join-Path -Path $outputDirectory -ChildPath $fileName
 Invoke-WebRequest -Uri $downloadUrl -OutFile $outputFile @requestOpts
-Expand-Archive -Path $outputFile -DestinationPath $outputDirectory -Force
+Expand-Archive $outputFile -DestinationPath $outputDirectory -Force
 Remove-Item -Path $outputFile
