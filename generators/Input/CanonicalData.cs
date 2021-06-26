9e88eca228e5775fb04d8dddce69b1db2248404b
Erik Schierboom <erik_schierboom@hotmail.com>
2021-06-26 17:30:58 +0200

    More work

--- generators/Input/CanonicalData.cs
+++ generators/Input/CanonicalData.cs
@@ -1,7 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.IO;
-
-using Newtonsoft.Json.Linq;
 
 namespace Exercism.CSharp.Input
 {
@@ -9,34 +6,4 @@ namespace Exercism.CSharp.Input
     
     internal record CanonicalDataCase(int Index, string Property, IReadOnlyDictionary<string, dynamic> Input,
         dynamic Expected, string Description, IReadOnlyCollection<string> DescriptionPath);
-    
-    internal class CanonicalDataParser
-    {
-        private readonly Options _options;
-
-        private CanonicalDataParser(Options options) => _options = options;
-
-        public static CanonicalDataParser Create(Options options)
-        {
-            var propSpecsRepository = new PropSpecsRepository(options);
-            propSpecsRepository.SyncToLatest();
-
-            return new CanonicalDataParser(options);
-        }
-
-        public CanonicalData Parse(string exercise)
-        {   
-            var canonicalDataJson = ParseCanonicalData(exercise);
-            var exerciseName = canonicalDataJson.Value<string>("exercise");
-            var canonicalDataCases = CanonicalDataCaseParser.Parse((JArray)canonicalDataJson["cases"]);
-
-            return new CanonicalData(exerciseName, canonicalDataCases);
-        }
-
-        private JObject ParseCanonicalData(string exercise) =>
-            JObject.Parse(File.ReadAllText(ExerciseCanonicalDataPath(exercise)));
-
-        private string ExerciseCanonicalDataPath(string exercise) =>
-            Path.Combine(_options.ProbSpecsDir, "exercises", exercise, "canonical-data.json");
-    }
 }
