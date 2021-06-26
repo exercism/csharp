4f93351ac42e44d83813de62f5e7674c3a93aab3
Erik Schierboom <erik_schierboom@hotmail.com>
2021-06-26 17:33:42 +0200

    Add todo

--- generators/Options.cs
+++ generators/Options.cs
@@ -9,6 +9,7 @@ namespace Exercism.CSharp
         [Option('e', "exercise", Required = false, HelpText = "The exercise to generate.")]
         public string Exercise { get; set; }
 
+        // TODO: set to default value
         [Option('d', "dir", Required = false, HelpText = "The directory of the problem-specifications repo. If the not specified, the repo will be downloaded.")]
         public string ProbSpecsDir { get; set; }
 
