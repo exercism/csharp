08bb54cd535ef54bdcc98f647673abe87b06d015
Alvin Zhao <alvin.zhao@myob.com>
2021-08-18 23:28:54 +1000

    add test cast to test generator

--- generators/Exercises/Generators/Sieve.cs
+++ generators/Exercises/Generators/Sieve.cs
@@ -6,17 +6,17 @@ namespace Exercism.CSharp.Exercises.Generators
 {
     internal class Sieve : ExerciseGenerator
     {
-        protected override void UpdateTestMethod(TestMethod testMethod)
-        {
-            testMethod.UseVariableForExpected = true;
+        protected override void UpdateTestClass(TestClass testClass) =>
+            AddTestCase(testClass);
 
-            if (testMethod.Input["limit"] < 2)
-                testMethod.ExceptionThrown = typeof(ArgumentOutOfRangeException);
-            else
-                testMethod.Expected = RenderAsSingleLineArray(testMethod.Expected);
-        }
-        
-        private UnescapedValue RenderAsSingleLineArray(dynamic value) 
-            => new(Render.Array(value as int[] ?? Array.Empty<int>()));
+        private static readonly string throwExceptionTestCase = @"
+[Fact(Skip = ""Remove this Skip property to run this test"")]
+public void No_negative_numbers()
+{
+    Assert.Throws<ArgumentOutOfRangeException>(() => Sieve.Primes(-1));
+}";
+
+        private static void AddTestCase(TestClass testClass) =>
+            testClass.AdditionalMethods.Add(throwExceptionTestCase);
     }
 }
