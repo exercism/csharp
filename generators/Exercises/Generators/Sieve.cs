396bcc7b7350372f1a6d1cc621d66997069edc7a
Alvin Zhao <alvin.zhao@myob.com>
2021-08-24 17:54:49 +1000

    change sieve unit tests (#1566)
    Change sieve unit tests
    
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>

--- generators/Exercises/Generators/Sieve.cs
+++ generators/Exercises/Generators/Sieve.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Exercism.CSharp.Output;
 using Exercism.CSharp.Output.Rendering;
 
@@ -6,17 +7,20 @@ namespace Exercism.CSharp.Exercises.Generators
 {
     internal class Sieve : ExerciseGenerator
     {
-        protected override void UpdateTestMethod(TestMethod testMethod)
-        {
-            testMethod.UseVariableForExpected = true;
+        protected override void UpdateNamespaces(ISet<string> namespaces) =>
+            namespaces.Add(typeof(ArgumentOutOfRangeException).Namespace);
 
-            if (testMethod.Input["limit"] < 2)
-                testMethod.ExceptionThrown = typeof(ArgumentOutOfRangeException);
-            else
-                testMethod.Expected = RenderAsSingleLineArray(testMethod.Expected);
-        }
-        
-        private UnescapedValue RenderAsSingleLineArray(dynamic value) 
-            => new(Render.Array(value as int[] ?? Array.Empty<int>()));
+        protected override void UpdateTestClass(TestClass testClass) =>
+            AddTestCase(testClass);
+
+        private static readonly string throwExceptionTestCase = @"
+[Fact(Skip = ""Remove this Skip property to run this test"")]
+public void No_negative_numbers()
+{
+    Assert.Throws<ArgumentOutOfRangeException>(() => Sieve.Primes(-1));
+}";
+
+        private static void AddTestCase(TestClass testClass) =>
+            testClass.AdditionalMethods.Add(throwExceptionTestCase);
     }
 }
