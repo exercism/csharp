852023557ec54bb08f3ce9acaa064672f043ef00
Alvin Zhao <alvin99yz@gmail.com>
2021-09-24 00:19:53 +1000

    Update exercises/concept/log-analysis/LogAnalysis.cs
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>

--- exercises/concept/log-analysis/LogAnalysis.cs
+++ exercises/concept/log-analysis/LogAnalysis.cs
@@ -2,23 +2,11 @@ using System;
 
 public static class LogAnalysis 
 {
-    public static string SubstringAfter(this string str, string delimiter)
-    {
-        throw new NotImplementedException("Please implement the SubstringAfter() extension method for strings");
-    }
+    // TODO: define the 'SubstringAfter()' extension method on the `string` type
 
-    public static string SubstringBetween(this string str, string start, string stop)
-    {
-        throw new NotImplementedException("Please implement the SubstringBetween() extension method for strings");
-    }
+    // TODO: define the 'SubstringBetween()' extension method on the `string` type
+    
+    // TODO: define the 'Message()' extension method on the `string` type
 
-    public static string Message(this string log)
-    {
-        throw new NotImplementedException("Please implement the Message() extension method for strings");
-    }
-
-    public static string LogLevel(this string log)
-    {
-        throw new NotImplementedException("Please implement the LogLevel() extension method for strings");
-    }
+    // TODO: define the 'LogLevel()' extension method on the `string` type
 }
