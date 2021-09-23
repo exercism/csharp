240d9467b8d419c780dd7669f92c30b081d75e0c
Alvin Zhao <alvin99yz@gmail.com>
2021-09-24 00:44:27 +1000

    Update Exemplar.cs

--- exercises/concept/log-analysis/.meta/Exemplar.cs
+++ exercises/concept/log-analysis/.meta/Exemplar.cs
@@ -2,15 +2,23 @@ using System;
 
 public static class LogAnalysis 
 {
-    public static string SubstringAfter(this string str, string delimiter) =>
-        str.Substring(str.IndexOf(delimiter) + delimiter.Length);
+    public static string SubstringAfter(this string str, string delimiter)
+    {
+        return str.Substring(str.IndexOf(delimiter) + delimiter.Length);
+    }
+    
+    public static string SubstringBetween(this string str, string start, string stop)
+    {
+        return str.SubstringAfter(start).Substring(0, str.IndexOf(stop) - 1);
+    }
+    
+    public static string Message(this string log)
+    {
+        return log.SubstringAfter("]: ");
+    }
 
-    public static string SubstringBetween(this string str, string start, string stop) =>
-        str.SubstringAfter(start).Substring(0, str.IndexOf(stop) - 1);
-
-    public static string Message(this string log) =>
-        log.SubstringAfter("]: ");
-
-    public static string LogLevel(this string log) =>
-        log.SubstringBetween("[", "]");
+    public static string LogLevel(this string log)
+    {
+        return log.SubstringBetween("[", "]");
+    }
 }
