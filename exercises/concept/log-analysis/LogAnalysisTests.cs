152e0845f81de356b81989a6f2b34e5760a887f3
Alvin Zhao <alvin.zhao@myob.com>
2021-09-26 01:40:32 +1000

    add test case

--- exercises/concept/log-analysis/LogAnalysisTests.cs
+++ exercises/concept/log-analysis/LogAnalysisTests.cs
@@ -5,25 +5,31 @@ using System;
 public class LogAnalysisTests
 {
     [Fact]
-    public void SubstringAfter()
+    public void SubstringAfter_WithDelimeterOfLength1()
     {
-        Assert.Equal(" test", "I am the 1st test".SubstringAfter("1st"));
+        Assert.Equal(" am the 1st test", "I am the 1st test".SubstringAfter("I"));
     }
 
-    [Fact]
+    [Fact(Skip = "Remove this Skip property to run this test")]
+    public void SubstringAfter_WithDelimeterOfLengthLongerThan1()
+    {
+        Assert.Equal(" test", "I am the 2nd test".SubstringAfter("2nd"));
+    }
+
+    [Fact(Skip = "Remove this Skip property to run this test")]
     public void SubstringBetween()
     {
         Assert.Equal("INFO", "[INFO]: File Deleted.".SubstringBetween("[", "]"));
     }
 
-    [Fact]
+    [Fact(Skip = "Remove this Skip property to run this test")]
     public void Message()
     {
         var log = "[WARNING]: Library is deprecated.";
         Assert.Equal("Library is deprecated.", log.Message());
     }
 
-    [Fact]
+    [Fact(Skip = "Remove this Skip property to run this test")]
     public void LogLevel()
     {
         var log = "[WARNING]: Library is deprecated.";
