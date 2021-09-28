f5f681825535466e07c8d4da52e097a8b3e5441b
18-F-cali <63421973+18-F-cali@users.noreply.github.com>
2021-09-28 12:18:00 +0530

    weighing-machine: add instructions for default units to be Kilogram (#1650)
    Added instructions for default units to be Kilogram.
    
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>

--- exercises/concept/weighing-machine/WeighingMachineTests.cs
+++ exercises/concept/weighing-machine/WeighingMachineTests.cs
@@ -20,6 +20,14 @@ public class WeighingMachineTests
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
+    public void Default_Unit_Is_Kilogram()
+    {
+        var wm = new WeighingMachine();
+        
+        Assert.Equal(Unit.Kilograms, wm.Unit);
+    }
+
+    [Fact(Skip = "Remove this Skip property to run this test")]
     public void Get_us_display_weight_pounds()
     {
         var wm = new WeighingMachine();
