1f4ab1e11929ad60f7372ff8e945f4100b3c698c
Sander Ploegsma <sanderploegsma@gmail.com>
2021-10-01 09:48:12 +0200

    Improve calculator-conundrum concept exercise (#1665)
    * Move read-only code from calculator-conundrum stub
    
    Move the SimpleOperation static class to a separate file that is marked as an editor file in the exercise's config.json.
    
    Fixes #1641
    
    * Add build directory to .gitignore
    
    When running the test.ps1 script locally a `build/` directory is created, which should never be committed.
    
    * Move calculator-conundrum hints to `.docs/hints.md`
    
    Fixes #1462
    
    * Update instructions
    
    - Task 1 does not actually handle errors thrown within Calculate, so the title does not match what is expected
    - Task 2 mentions using a `try` block, but this is only needed in task 3
    - Task 2 is missing usage examples
    
    * Move throwing/catching exception hints to the appropriate tasks
    
    * Add myself as contributor
    
    * Remove DivideByZeroException from task 3 instructions
    
    * Add suggested clarification to DivideByZeroException hint
    
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>
    
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>

--- exercises/concept/calculator-conundrum/.meta/Exemplar.cs
+++ exercises/concept/calculator-conundrum/.meta/Exemplar.cs
@@ -34,21 +34,3 @@ public static class SimpleCalculator
         return $"{operand1} {operation} {operand2} = {result}";
     }
 }
-
-/**** Please do not modify the code below ****/
-public static class SimpleOperation
-{
-    public static int Division(int operand1, int operand2)
-    {
-        return operand1 / operand2;
-    }
-
-    public static int Multiplication(int operand1, int operand2)
-    {
-        return operand1 * operand2;
-    }
-    public static int Addition(int operand1, int operand2)
-    {
-        return operand1 + operand2;
-    }
-}
