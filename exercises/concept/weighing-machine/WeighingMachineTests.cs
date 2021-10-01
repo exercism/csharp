c6893ac886eb3248fb7e8a1764e6554e881161fb
18-F-Cali <63421973+18-F-cali@users.noreply.github.com>
2021-10-01 22:19:45 +0530

    Updated Tests.

--- exercises/concept/weighing-machine/WeighingMachineTests.cs
+++ exercises/concept/weighing-machine/WeighingMachineTests.cs
@@ -5,68 +5,49 @@ using Exercism.Tests;
 public class WeighingMachineTests
 {
     [Fact]
+    public void Get_Precision()
+    {
+        var wm = new WeighingMachine(precision:2);
+        Assert.Equal(2, wm.Precision);
+    }
+
+    [Fact(Skip = "Remove this Skip property to run this test")]
     public void Set_weight_and_get_weight()
     {
         var wm = new WeighingMachine();
-        wm.InputWeight = 60m;
-        Assert.Equal(60m, wm.InputWeight, precision: 3);
+        wm.Weight = 60m;
+        Assert.Equal(60m, wm.Weight, precision: 3);
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Negative_weight_is_invalid()
     {
         var wm = new WeighingMachine();
-        Assert.Throws<ArgumentOutOfRangeException>(() => wm.InputWeight = -10);
-    }
-
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Get_us_display_weight_pounds()
-    {
-        var wm = new WeighingMachine();
-        wm.InputWeight = 60m;
-        Assert.Equal(132, wm.USDisplayWeight.Pounds);
-    }
-
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Get_us_display_weight_ounces()
-    {
-        var wm = new WeighingMachine();
-        wm.InputWeight = 60m;
-        Assert.Equal(4, wm.USDisplayWeight.Ounces);
-    }
-
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Input_pounds_and_get_us_display_weight_pounds()
-    {
-        var wm = new WeighingMachine();
-        wm.Unit = Unit.Pounds;
-        wm.InputWeight = 175.5m;
-        Assert.Equal(175, wm.USDisplayWeight.Pounds);
-    }
-
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Input_pounds_and_get_is_display_weight_ounces()
-    {
-        var wm = new WeighingMachine();
-        wm.Unit = Unit.Pounds;
-        wm.InputWeight = 175.5m;
-        Assert.Equal(8, wm.USDisplayWeight.Ounces);
+        Assert.Throws<ArgumentOutOfRangeException>(() => wm.Weight = -10);
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Apply_tare_adjustment_and_get_display_weight()
     {
         var wm = new WeighingMachine();
-        wm.InputWeight = 100;
+        wm.Weight = 100;
         wm.TareAdjustment = 10;
         Assert.Equal(90, wm.DisplayWeight);
+    }
+
+     [Fact(Skip = "Remove this Skip property to run this test")]
+    public void Apply_Default_tare_adjustment_and_get_display_weight()
+    {
+        var wm = new WeighingMachine();
+        wm.Weight = 100;
+        Assert.Equal(95, wm.DisplayWeight);
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Apply_negative_tare_adjustment()
     {
         var wm = new WeighingMachine();
-        wm.InputWeight = 100;
+        wm.Weight = 100;
         wm.TareAdjustment = -10;
         Assert.Equal(110, wm.DisplayWeight);
     }
@@ -75,16 +56,8 @@ public class WeighingMachineTests
     public void Apply_large_tare_adjustment_to_allow_negative_display_weight()
     {
         var wm = new WeighingMachine();
-        wm.InputWeight = 100;
+        wm.Weight = 100;
         wm.TareAdjustment = 110;
         Assert.Equal(-10, wm.DisplayWeight);
-    }
-
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Default_Unit_Is_Kilogram()
-    {
-        var wm = new WeighingMachine();
-        
-        Assert.Equal(wm.Unit, Unit.Kilograms);
     }
 }
