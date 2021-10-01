bdae4f40702fc385455bd6d2243ed3fa2220cad5
Sander Ploegsma <sanderploegsma@gmail.com>
2021-10-01 09:22:04 +0200

    football-match-reports: move read-only classes to editor-only file

---
+++ exercises/concept/football-match-reports/OffFieldActivitiesAndCharacters.cs
@@ -0,0 +1,24 @@
+﻿public class Manager
+{
+    public string Name { get; }
+
+    public Manager(string name)
+    {
+        this.Name = name;
+    }
+}
+
+public class Incident
+{
+    public virtual string GetDescription() => "An incident happened.";
+}
+
+public class Foul : Incident
+{
+    public override string GetDescription() => "The referee deemed a foul.";
+}
+
+public class Injury : Incident
+{
+    public override string GetDescription() => "A player is injured.";
+}
