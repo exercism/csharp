55861b3881a698a606c7d9bf8cfcccb60e456407
CodeVelo <jeff.wilkes@hotmail.com>
2021-10-04 17:05:06 -0700

    Updated HyperiaForex.cs to remove build errors when starting development

--- exercises/concept/hyperia-forex/HyperiaForex.cs
+++ exercises/concept/hyperia-forex/HyperiaForex.cs
@@ -1,3 +1,5 @@
+using System;
+
 public struct CurrencyAmount
 {
     private decimal amount;
@@ -11,9 +13,64 @@ public struct CurrencyAmount
 
     // TODO: implement equality operators
 
+    public static bool operator ==(CurrencyAmount @this, CurrencyAmount other)
+    {
+        throw new NotImplementedException("Please implement the == operator.");
+    }
+
+    public static bool operator !=(CurrencyAmount @this, CurrencyAmount other)
+    {
+        throw new NotImplementedException("Please implement the != operator.");
+    }
+
     // TODO: implement comparison operators
+
+    public static bool operator >(CurrencyAmount @this, CurrencyAmount other)
+    {
+        throw new NotImplementedException("Please implement the > operator.");
+    }
+
+    public static bool operator <(CurrencyAmount @this, CurrencyAmount other)
+    {
+        throw new NotImplementedException("Please implement the <!=> operator.");
+    }
 
     // TODO: implement arithmetic operators
 
+    public static CurrencyAmount operator +(CurrencyAmount @this, CurrencyAmount other)
+    {
+        throw new NotImplementedException("Please implement the + operator.");
+    }
+
+    public static CurrencyAmount operator -(CurrencyAmount @this, CurrencyAmount other)
+    {
+        throw new NotImplementedException("Please implement the - operator.");
+    }
+
+    public static CurrencyAmount operator *(CurrencyAmount @this, decimal multiplier)
+    {
+        throw new NotImplementedException("Please implement the * operator.");
+    }
+
+    public static CurrencyAmount operator *(decimal multiplier, CurrencyAmount @this)
+    {
+        throw new NotImplementedException("Please implement the * operator.");
+    }
+
+    public static CurrencyAmount operator /(CurrencyAmount @this, decimal divisor)
+    {
+        throw new NotImplementedException("Please implement the / operator.");
+    }
+
     // TODO: implement type conversion operators
+
+    public static explicit operator double(CurrencyAmount @this)
+    {
+        throw new NotImplementedException("Please implement the cast to double operator.");
+    }
+
+    public static implicit operator decimal(CurrencyAmount @this)
+    {
+        throw new NotImplementedException("Please implement the cast to decimal operator.");
+    }
 }
