5e998d7bfeefcae5ca01c679a327f30e90bd92ba
Erik Schierboom <erik_schierboom@hotmail.com>
2022-10-16 14:42:51 +0200

    grade-schooi: fix tests

--- exercises/practice/grade-school/.meta/Example.cs
+++ exercises/practice/grade-school/.meta/Example.cs
@@ -1,123 +1,26 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 public class GradeSchool
 {
-    private readonly Dictionary<int, IList<string>> roster = new Dictionary<int, IList<string>>();
+    private readonly SortedDictionary<int, List<string>> _roster = new();
     
     public bool Add(string student, int grade)
     {
-        if (roster.ContainsKey(grade))
+        if (_roster.Values.Any(students => students.Contains(student)))
+            return false;
+
+        if (!_roster.TryAdd(grade, new List<string> { student }))
         {
-            if (roster[grade].Contains(student))
-            {
-                return false;
-            }
-
-            roster[grade].Add(student);
-            return true;
+            _roster[grade].Add(student);
+            _roster[grade].Sort();
         }
-        else
-        {
-            roster.Add(grade, new SortedList<string> { student });
-            return true;
-        }
+
+        return true;
     }
 
-   public IEnumerable<string> Roster()
-   {
-        var students = new List<string>();
-        
-        foreach (var item in roster.OrderBy(pair => pair.Key))
-        {
-            foreach (var student in item.Value)
-            {
-                students.Add(student); 
-            }
-        }
-        return students;
-   } 
+    public IEnumerable<string> Roster() => _roster.Values.SelectMany(grade => grade).ToList();
 
-    public IEnumerable<string> Grade(int grade)
-    {
-        IList<string> students;
-        if (roster.TryGetValue(grade, out students))
-            return students.AsEnumerable();
-        return Enumerable.Empty<string>();
-    }
-}
-
-public class SortedList<T> : IList<T>
-{
-    private readonly List<T> list = new List<T>();
-
-    public int IndexOf(T item)
-    {
-        return list.IndexOf(item);
-    }
-
-    public void Insert(int index, T item)
-    {
-        throw new System.NotSupportedException("Insert would ruin sort");
-    }
-
-    public void RemoveAt(int index)
-    {
-        list.RemoveAt(index);
-    }
-
-    public T this[int index]
-    {
-        get { return list[index]; }
-        set
-        {
-            list.RemoveAt(index);
-            Add(value);
-        }
-    }
-
-    public void Add(T item)
-    {
-        list.Insert(~list.BinarySearch(item), item);
-    }
-
-    public void Clear()
-    {
-        list.Clear();
-    }
-
-    public bool Contains(T item)
-    {
-        return list.Contains(item);
-    }
-
-    public void CopyTo(T[] array, int arrayIndex)
-    {
-        list.CopyTo(array, arrayIndex);
-    }
-
-    public int Count
-    {
-        get { return list.Count; }
-    }
-
-    public bool IsReadOnly
-    {
-        get { return false; }
-    }
-
-    public bool Remove(T item)
-    {
-        return list.Remove(item);
-    }
-
-    public IEnumerator<T> GetEnumerator()
-    {
-        return list.GetEnumerator();
-    }
-
-    System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
-    {
-        return list.GetEnumerator();
-    }
+    public IEnumerable<string> Grade(int grade) => _roster.TryGetValue(grade, out var students) ? students : new List<string>();
 }
