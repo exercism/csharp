64707923f19f5e901a618f0ea5cbe55991da4f4e
Erik Schierboom <erik_schierboom@hotmail.com>
2022-11-03 12:24:14 +0100

    Allow for lazy evaluation in series and sieve (#2011)
    * generator: allow forcing evaluation
    
    * series: allow for lazy evaluation
    
    * sieve: allow for lazy evaluation

--- exercises/practice/series/SeriesTests.cs
+++ exercises/practice/series/SeriesTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Xunit;
 
 public class SeriesTests
@@ -7,71 +8,71 @@ public class SeriesTests
     public void Slices_of_one_from_one()
     {
         var expected = new[] { "1" };
-        Assert.Equal(expected, Series.Slices("1", 1));
+        Assert.Equal(expected, Series.Slices("1", 1).ToArray());
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Slices_of_one_from_two()
     {
         var expected = new[] { "1", "2" };
-        Assert.Equal(expected, Series.Slices("12", 1));
+        Assert.Equal(expected, Series.Slices("12", 1).ToArray());
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Slices_of_two()
     {
         var expected = new[] { "35" };
-        Assert.Equal(expected, Series.Slices("35", 2));
+        Assert.Equal(expected, Series.Slices("35", 2).ToArray());
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Slices_of_two_overlap()
     {
         var expected = new[] { "91", "14", "42" };
-        Assert.Equal(expected, Series.Slices("9142", 2));
+        Assert.Equal(expected, Series.Slices("9142", 2).ToArray());
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Slices_can_include_duplicates()
     {
         var expected = new[] { "777", "777", "777", "777" };
-        Assert.Equal(expected, Series.Slices("777777", 3));
+        Assert.Equal(expected, Series.Slices("777777", 3).ToArray());
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Slices_of_a_long_series()
     {
         var expected = new[] { "91849", "18493", "84939", "49390", "93904", "39042", "90424", "04243" };
-        Assert.Equal(expected, Series.Slices("918493904243", 5));
+        Assert.Equal(expected, Series.Slices("918493904243", 5).ToArray());
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Slice_length_is_too_large()
     {
-        Assert.Throws<ArgumentException>(() => Series.Slices("12345", 6));
+        Assert.Throws<ArgumentException>(() => Series.Slices("12345", 6).ToArray());
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Slice_length_is_way_too_large()
     {
-        Assert.Throws<ArgumentException>(() => Series.Slices("12345", 42));
+        Assert.Throws<ArgumentException>(() => Series.Slices("12345", 42).ToArray());
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Slice_length_cannot_be_zero()
     {
-        Assert.Throws<ArgumentException>(() => Series.Slices("12345", 0));
+        Assert.Throws<ArgumentException>(() => Series.Slices("12345", 0).ToArray());
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Slice_length_cannot_be_negative()
     {
-        Assert.Throws<ArgumentException>(() => Series.Slices("123", -1));
+        Assert.Throws<ArgumentException>(() => Series.Slices("123", -1).ToArray());
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Empty_series_is_invalid()
     {
-        Assert.Throws<ArgumentException>(() => Series.Slices("", 1));
+        Assert.Throws<ArgumentException>(() => Series.Slices("", 1).ToArray());
     }
 }
--- generators/Exercises/Generators/Sieve.cs
+++ generators/Exercises/Generators/Sieve.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+
 using Exercism.CSharp.Output;
 using Exercism.CSharp.Output.Rendering;
 
@@ -7,8 +9,14 @@ namespace Exercism.CSharp.Exercises.Generators
 {
     internal class Sieve : ExerciseGenerator
     {
-        protected override void UpdateNamespaces(ISet<string> namespaces) =>
+        protected override void UpdateTestMethod(TestMethod testMethod) =>
+            testMethod.ForceEvaluation = true;
+
+        protected override void UpdateNamespaces(ISet<string> namespaces)
+        {
             namespaces.Add(typeof(ArgumentOutOfRangeException).Namespace!);
+            namespaces.Add(typeof(Enumerable).Namespace!);
+        }
 
         protected override void UpdateTestClass(TestClass testClass) =>
             AddTestCase(testClass);
@@ -17,7 +25,7 @@ namespace Exercism.CSharp.Exercises.Generators
 [Fact(Skip = ""Remove this Skip property to run this test"")]
 public void No_negative_numbers()
 {
-    Assert.Throws<ArgumentOutOfRangeException>(() => Sieve.Primes(-1));
+    Assert.Throws<ArgumentOutOfRangeException>(() => Sieve.Primes(-1).ToArray());
 }";
 
         private static void AddTestCase(TestClass testClass) =>
