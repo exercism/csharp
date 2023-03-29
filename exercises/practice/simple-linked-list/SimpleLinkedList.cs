afd60766d0d9a8467c01d9ec86dc48637d164baf
Michał Poręba <michalporeba@gmail.com>
2023-03-29 21:44:00 +0100

    Simple list with LIFO behaviour example

--- exercises/practice/simple-linked-list/SimpleLinkedList.cs
+++ exercises/practice/simple-linked-list/SimpleLinkedList.cs
@@ -1,48 +1,65 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
+using Xunit;
+using System.Collections.Generic;
 
-public class SimpleLinkedList<T> : IEnumerable<T>
+public class SimpleLinkedListTests
 {
-    public SimpleLinkedList(T value)
+    [Fact]
+    public void Single_item_list_value()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var list = new SimpleLinkedList<int>(1);
+        Assert.Equal(1, list.Remove());
     }
 
-    public SimpleLinkedList(IEnumerable<T> values)
+    [Fact]
+    public void Two_item_list_first_value()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var list = new SimpleLinkedList<int>(1, 2);
+        Assert.Equal(2, list.Remove());
     }
 
-    public T Value 
-    { 
-        get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        } 
-    }
-
-    public SimpleLinkedList<T> Next
-    { 
-        get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        } 
-    }
-
-    public SimpleLinkedList<T> Add(T value)
+    [Fact]
+    public void Two_item_list_second_value()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var list = new SimpleLinkedList<int>(1, 2);
+        list.Remove();
+        Assert.Equal(1, list.Remove());
     }
 
-    public IEnumerator<T> GetEnumerator()
+    [Fact]
+    public void Multivalue_initialisation()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var values = new SimpleLinkedList<int>(2, 1, 3);
+        Assert.Equal(new[] { 3, 1, 2 }, values);
     }
 
-    IEnumerator IEnumerable.GetEnumerator()
+    [Fact]
+    public void From_enumerable()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var list = new SimpleLinkedList<int>(new[] { 11, 7, 5, 3, 2 });
+        Assert.Equal(2, list.Remove());
+        Assert.Equal(3, list.Remove());
+        Assert.Equal(5, list.Remove());
+        Assert.Equal(7, list.Remove());
+        Assert.Equal(11, list.Remove());
+    }
+
+    [Fact]
+    public void Reverse_enumerable()
+    {
+        var values = Enumerable.Range(1, 5).ToArray();
+        var list = new SimpleLinkedList<int>(values);
+        var enumerable = Assert.IsAssignableFrom<IEnumerable<int>>(list);    
+        var reversed = enumerable.Reverse();
+        Assert.Equal(values, reversed);
+    }
+
+    [Fact]
+    public void Roundtrip()
+    {
+        var values = Enumerable.Range(1, 7);
+        var list = new SimpleLinkedList<int>(values.ToArray());
+        var enumerable = Assert.IsAssignableFrom<IEnumerable<int>>(list);    
+        Assert.Equal(values.Reverse(), enumerable);
     }
 }
