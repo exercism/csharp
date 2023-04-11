4eec924e7d548cb53882b54917db0d97a56ae744
Michał Poręba <michalporeba@gmail.com>
2023-04-11 23:16:03 +0100

    proposed tasks to lead the implementation

--- exercises/practice/simple-linked-list/SimpleLinkedListTests.cs
+++ exercises/practice/simple-linked-list/SimpleLinkedListTests.cs
@@ -1,10 +1,12 @@
 using System.Linq;
 using System.Collections.Generic;
+using Exercism.Tests;
 using Xunit;
 
 public class SimpleLinkedListTests
 {
     [Fact] 
+    [Task(1)]
     public void Empty_list_has_no_elements()
     {
         var list = new SimpleLinkedList<int>();
@@ -12,6 +14,7 @@ public class SimpleLinkedListTests
     }
 
     [Fact]
+    [Task(2)]
     public void Pushing_elements_to_the_list_increases_the_count()
     {
         var list = new SimpleLinkedList<int>();
@@ -22,6 +25,7 @@ public class SimpleLinkedListTests
     }
 
     [Fact]
+    [Task(3)]
     public void Popping_elements_from_the_list_decreases_the_count()
     {
         var list = new SimpleLinkedList<int>();
@@ -33,6 +37,7 @@ public class SimpleLinkedListTests
     }
 
     [Fact]
+    [Task(4)]
     public void Elements_pop_back_in_lifo_order()
     {
         var list = new SimpleLinkedList<int>();
@@ -45,6 +50,7 @@ public class SimpleLinkedListTests
     }
 
     [Fact]
+    [Task(5)]
     public void Singlevalue_initialisation()
     {
         var list = new SimpleLinkedList<int>(7);
@@ -53,6 +59,7 @@ public class SimpleLinkedListTests
     }
 
     [Fact]
+    [Task(5)]
     public void Multivalue_initialisation()
     {
         var list = new SimpleLinkedList<int>(2, 1, 3);
@@ -62,6 +69,7 @@ public class SimpleLinkedListTests
     }
 
     [Fact]
+    [Task(5)]
     public void From_enumerable()
     {
         var list = new SimpleLinkedList<int>(new[] { 11, 7, 5, 3, 2 });
@@ -73,6 +81,7 @@ public class SimpleLinkedListTests
     }
 
     [Fact]
+    [Task(6)]
     public void Reverse_enumerable()
     {
         var values = Enumerable.Range(1, 5).ToArray();
@@ -83,6 +92,7 @@ public class SimpleLinkedListTests
     }
 
     [Fact]
+    [Task(6)]
     public void Roundtrip()
     {
         var values = Enumerable.Range(1, 7);
