723ca3e637c239e38b74f118b5e432bc491f7bce
Scott Matloff <scott.matloff@gmail.com>
2023-08-19 17:57:15 -0700

    Did an editing pass on the docs to make them clearer to the reader by moving the examples closer to the text which refers to them. Also changed the suggested function names to be verb-based, rather than noun-based, as that is more common in C# projects.

--- exercises/practice/crypto-square/CryptoSquare.cs
+++ exercises/practice/crypto-square/CryptoSquare.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 
 public static class CryptoSquare
 {
-    public static string NormalizedPlaintext(string plaintext)
+    public static string NormalizePlaintext(string plaintext)
     {
         throw new NotImplementedException("You need to implement this function.");
     }
 
-    public static IEnumerable<string> PlaintextSegments(string plaintext)
+    public static (int rows, int columns) CalculateRequiredDimensions(string plaintext)
     {
         throw new NotImplementedException("You need to implement this function.");
     }
 
-    public static string Encoded(string plaintext)
+    public static IEnumerable<string> GeneratePlaintextSegments(string plaintext)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+
+    public static string Encode(string plaintext)
     {
         throw new NotImplementedException("You need to implement this function.");
     }
