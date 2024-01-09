4a0c7a33c4472e549bf99ddf7ce42997ff4a8459
Michał Poręba <michalporeba@gmail.com>
2024-01-09 10:43:15 +0000

    Update exercises/practice/binary-search-tree/BinarySearchTree.cs
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>

--- exercises/practice/binary-search-tree/BinarySearchTree.cs
+++ exercises/practice/binary-search-tree/BinarySearchTree.cs
@@ -2,8 +2,8 @@ using System;
 
 public class BinarySearchTree<T> where T : IComparable
 {
-    public int Count => throw new NotImplementedException("You need to implement this function.");
-    public int Depth => throw new NotImplementedException("You need to implement this function.");
+    public int Count => throw new NotImplementedException("You need to implement this property.");
+    public int Depth => throw new NotImplementedException("You need to implement this property.");
 
     public void Add(T value) => throw new NotImplementedException("You need to implement this function.");
     public bool Contains(T value) => throw new NotImplementedException("You need to implement this function.");
