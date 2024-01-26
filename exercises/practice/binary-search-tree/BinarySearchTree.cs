a8fd85846636f906ae794b2eb7f229f7e1d74436
Michał Poręba <michalporeba@gmail.com>
2024-01-26 23:20:45 +0000

    the new standard not implemented messages

--- exercises/practice/binary-search-tree/BinarySearchTree.cs
+++ exercises/practice/binary-search-tree/BinarySearchTree.cs
@@ -5,20 +5,17 @@ using Newtonsoft.Json;
 public class BinarySearchTree<T> where T : IComparable
 {
     public int Count
-    => throw new NotImplementedException("You need to implement this property.");
-
-    public int Depth
-    => throw new NotImplementedException("You need to implement this property.");
+    => throw new NotImplementedException("Please implement the Count property of the BinarySearchTree<T> class.");
 
     public void Add(T value)
-    => throw new NotImplementedException("You need to implement this method.");
+    => throw new NotImplementedException("Please implement the Add(T) method of the BinarySearchTree<T> class.");
 
     public bool Contains(T value)
-    => throw new NotImplementedException("You need to implement this method.");
+    => throw new NotImplementedException("Please implement the Contains(T) method of the BinarySearchTree<T> class.");
 
     public string ToJson()
-    => throw new NotImplementedException("You need to implement this property.");
+    => throw new NotImplementedException("Please implement the ToJson() method of the BinarySearchTree<T> class.");
 
-    public IEnumerable<T> GetOrderedValues() 
-    => throw new NotImplementedException("You need to implement this propertly.");
+    public IEnumerable<T> GetOrderedValues()
+    => throw new NotImplementedException("Please implement the GetOrderedValues() method of the BinarySearchTree<T> class.");
 }
