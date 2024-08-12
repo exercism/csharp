87829e5894264b8af9ffe2a10ea553c56b2a1730
Eric Willigers <keiravillekode@gmail.com>
2024-08-12 20:43:53 +1000

    Ukranian => Ukrainian

--- exercises/practice/zebra-puzzle/.meta/Example.cs
+++ exercises/practice/zebra-puzzle/.meta/Example.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 public enum Color { Red , Green , Ivory , Yellow , Blue }
-public enum Nationality { Englishman , Spaniard , Ukranian , Japanese , Norwegian }
+public enum Nationality { Englishman , Spaniard , Ukrainian , Japanese , Norwegian }
 public enum Pet { Dog , Snails , Fox , Horse , Zebra }
 public enum Drink { Coffee , Tea , Milk , OrangeJuice , Water }
 public enum Smoke { OldGold , Kools , Chesterfields , LuckyStrike , Parliaments }
@@ -61,10 +61,10 @@ public static class ZebraPuzzle
     private static bool MatchesDrinkRules(Color[] colors, Nationality[] nationalities, Drink[] drinks)
     {
         var coffeeDrunkInGreenHouse = IsIndexMatch(colors, Color.Green, drinks, Drink.Coffee); // #4
-        var ukranianDrinksTee = IsIndexMatch(nationalities, Nationality.Ukranian, drinks, Drink.Tea); // #5
+        var ukrainianDrinksTee = IsIndexMatch(nationalities, Nationality.Ukrainian, drinks, Drink.Tea); // #5
         var milkDrunkInMiddleHouse = drinks[2] == Drink.Milk; // #9
 
-        return coffeeDrunkInGreenHouse && ukranianDrinksTee && milkDrunkInMiddleHouse;
+        return coffeeDrunkInGreenHouse && ukrainianDrinksTee && milkDrunkInMiddleHouse;
     }
     
     private static bool MatchesSmokeRules(Color[] colors, Nationality[] nationalities, Drink[] drinks, Pet[] pets, Smoke[] smokes)
--- exercises/practice/zebra-puzzle/ZebraPuzzle.cs
+++ exercises/practice/zebra-puzzle/ZebraPuzzle.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 public enum Color { Red , Green , Ivory , Yellow , Blue }
-public enum Nationality { Englishman , Spaniard , Ukranian , Japanese , Norwegian }
+public enum Nationality { Englishman , Spaniard , Ukrainian , Japanese , Norwegian }
 public enum Pet { Dog , Snails , Fox , Horse , Zebra }
 public enum Drink { Coffee , Tea , Milk , OrangeJuice , Water }
 public enum Smoke { OldGold , Kools , Chesterfields , LuckyStrike , Parliaments }
