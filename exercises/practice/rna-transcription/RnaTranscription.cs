8f3cec75aba9023b49479e22858060fc16f8bf60
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2024-08-13 21:56:23 -0700

    Update rna-transcription parameter name

--- exercises/practice/rna-transcription/RnaTranscription.cs
+++ exercises/practice/rna-transcription/RnaTranscription.cs
@@ -2,7 +2,7 @@ using System;
 
 public static class RnaTranscription
 {
-    public static string ToRna(string nucleotide)
+    public static string ToRna(string strand)
     {
         throw new NotImplementedException("You need to implement this method.");
     }
