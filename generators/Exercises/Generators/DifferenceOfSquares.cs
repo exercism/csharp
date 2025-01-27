a0007abd3715f5b2880b07168f4aaab26833f173
Erik Schierboom <erik_schierboom@hotmail.com>
2025-01-27 09:07:19 +0100

    Remove deprecated generators

--- generators/Exercises/Generators/DifferenceOfSquares.cs
+++
@@ -1,8 +0,0 @@
-using Exercism.CSharp.Output;
-
-namespace Exercism.CSharp.Exercises.Generators;
-
-internal class DifferenceOfSquares : ExerciseGenerator
-{
-    protected override void UpdateTestMethod(TestMethod testMethod) => testMethod.TestedMethod = $"Calculate{testMethod.TestedMethod}";
-}
--- generators/Exercises/Generators/Leap.cs
+++
@@ -1,8 +0,0 @@
-﻿using Exercism.CSharp.Output;
-
-namespace Exercism.CSharp.Exercises.Generators;
-
-internal class Leap : ExerciseGenerator
-{
-    protected override void UpdateTestMethod(TestMethod testMethod) => testMethod.TestedMethod = "IsLeapYear";
-}
