632f1d22f69101a172abf910e8fb8a3719554a34
Erik Schierboom <erik_schierboom@hotmail.com>
2025-01-27 20:01:28 +0100

    Add square-root

---
+++ exercises/practice/square-root/.meta/Generator.tpl
@@ -0,0 +1,12 @@
+using Xunit;
+
+public class SquareRootTests
+{
+    {{#test_cases}}
+    [Fact{{#unless @first}}(Skip = "Remove this Skip property to run this test"){{/unless}}]
+    public void {{method_name path}}()
+    {
+        Assert.Equal({{expected}}, SquareRoot.Root({{input.radicand}}));
+    }
+    {{/test_cases}}
+}
--- exercises/practice/square-root/SquareRootTests.cs
+++ exercises/practice/square-root/SquareRootTests.cs
@@ -3,37 +3,37 @@ using Xunit;
 public class SquareRootTests
 {
     [Fact]
-    public void Root_of_1()
+    public void RootOf1()
     {
         Assert.Equal(1, SquareRoot.Root(1));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Root_of_4()
+    public void RootOf4()
     {
         Assert.Equal(2, SquareRoot.Root(4));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Root_of_25()
+    public void RootOf25()
     {
         Assert.Equal(5, SquareRoot.Root(25));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Root_of_81()
+    public void RootOf81()
     {
         Assert.Equal(9, SquareRoot.Root(81));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Root_of_196()
+    public void RootOf196()
     {
         Assert.Equal(14, SquareRoot.Root(196));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Root_of_65025()
+    public void RootOf65025()
     {
         Assert.Equal(255, SquareRoot.Root(65025));
     }
