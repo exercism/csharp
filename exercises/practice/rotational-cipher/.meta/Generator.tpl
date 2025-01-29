dcff887b78bb34db14d2b3d1eb500e491aa3d0ab
Erik Schierboom <erik_schierboom@hotmail.com>
2025-01-29 08:18:09 +0100

    Fix rot cipher

--- exercises/practice/rotational-cipher/.meta/Generator.tpl
+++ exercises/practice/rotational-cipher/.meta/Generator.tpl
@@ -6,7 +6,7 @@ public class RotationalCipherTests
     [Fact{{#unless @first}}(Skip = "Remove this Skip property to run this test"){{/unless}}]
     public void {{test_method_name}}()
     {
-        Assert.Equal({{expected}}, RotationalCipher.Rotate({{lit input.text}}, {{input.shiftKey}}));
+        Assert.Equal({{lit expected}}, RotationalCipher.Rotate({{lit input.text}}, {{input.shiftKey}}));
     }
     {{/test_cases}}
 }
--- exercises/practice/rotational-cipher/RotationalCipherTests.cs
+++ exercises/practice/rotational-cipher/RotationalCipherTests.cs
@@ -5,60 +5,60 @@ public class RotationalCipherTests
     [Fact]
     public void Rotate_a_by_0_same_output_as_input()
     {
-        Assert.Equal(a, RotationalCipher.Rotate("a", 0));
+        Assert.Equal("a", RotationalCipher.Rotate("a", 0));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Rotate_a_by_1()
     {
-        Assert.Equal(b, RotationalCipher.Rotate("a", 1));
+        Assert.Equal("b", RotationalCipher.Rotate("a", 1));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Rotate_a_by_26_same_output_as_input()
     {
-        Assert.Equal(a, RotationalCipher.Rotate("a", 26));
+        Assert.Equal("a", RotationalCipher.Rotate("a", 26));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Rotate_m_by_13()
     {
-        Assert.Equal(z, RotationalCipher.Rotate("m", 13));
+        Assert.Equal("z", RotationalCipher.Rotate("m", 13));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Rotate_n_by_13_with_wrap_around_alphabet()
     {
-        Assert.Equal(a, RotationalCipher.Rotate("n", 13));
+        Assert.Equal("a", RotationalCipher.Rotate("n", 13));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Rotate_capital_letters()
     {
-        Assert.Equal(TRL, RotationalCipher.Rotate("OMG", 5));
+        Assert.Equal("TRL", RotationalCipher.Rotate("OMG", 5));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Rotate_spaces()
     {
-        Assert.Equal(T R L, RotationalCipher.Rotate("O M G", 5));
+        Assert.Equal("T R L", RotationalCipher.Rotate("O M G", 5));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Rotate_numbers()
     {
-        Assert.Equal(Xiwxmrk 1 2 3 xiwxmrk, RotationalCipher.Rotate("Testing 1 2 3 testing", 4));
+        Assert.Equal("Xiwxmrk 1 2 3 xiwxmrk", RotationalCipher.Rotate("Testing 1 2 3 testing", 4));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Rotate_punctuation()
     {
-        Assert.Equal(Gzo 'n zvo, Bmviyhv!, RotationalCipher.Rotate("Let' s eat, Grandma!", 21));
+        Assert.Equal("Gzo'n zvo, Bmviyhv!", RotationalCipher.Rotate("Let's eat, Grandma!", 21));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Rotate_all_letters()
     {
-        Assert.Equal(Gur dhvpx oebja sbk whzcf bire gur ynml qbt., RotationalCipher.Rotate("The quick brown fox jumps over the lazy dog.", 13));
+        Assert.Equal("Gur dhvpx oebja sbk whzcf bire gur ynml qbt.", RotationalCipher.Rotate("The quick brown fox jumps over the lazy dog.", 13));
     }
 }
