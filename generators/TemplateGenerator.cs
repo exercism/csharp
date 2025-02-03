9cfec17a1bf0c28c503646d929912df97df175ea
Erik Schierboom <erik_schierboom@hotmail.com>
2025-02-03 15:19:41 +0100

    More generator work

--- generators/TemplateGenerator.cs
+++ generators/TemplateGenerator.cs
@@ -1,3 +1,8 @@
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+using Scriban;
+
 namespace Generators;
 
 internal static class TemplateGenerator
@@ -13,6 +18,66 @@ internal static class TemplateGenerator
         File.WriteAllText(Paths.TemplateFile(exercise), template);
     }
 
-    private static string RenderTemplate(CanonicalData canonicalData) =>
-        "test";
+    private static string RenderTemplate(CanonicalData canonicalData)
+    {
+        var error = canonicalData.TestCases.Where(ExpectsError).Any();
+        var testCase = canonicalData.TestCases.First(testCase => !ExpectsError(testCase));
+
+        HashSet<string> namespaces = ["Xunit"];
+        if (error) namespaces.Add("System");
+
+        var model = new { namespaces, error, assertion = Assertion(testCase), throwsAssertion = AssertThrows(testCase) };
+
+        var template = Template.Parse(GeneratorTemplate);
+        return template.Render(model).Trim() + Environment.NewLine;
+    }
+
+    private static string Expected(JsonNode testCase) =>
+        testCase.GetValueKind() == JsonValueKind.String
+            ? "{{test.expected | string.literal}}"
+            : "{{test.expected}}";
+
+    private static string Assertion(JsonNode testCase) =>
+        testCase["expected"]!.GetValueKind() switch
+        {
+            JsonValueKind.False or JsonValueKind.True => AssertBool(testCase),
+            _ => AssertEqual(testCase)
+        };
+
+    private static string TestedMethodCall(JsonNode testCase) =>
+        "{{testedClass}}.{{test.testedMethod}}({{test.input}}";
+
+    private static string AssertBool(JsonNode testCase) =>
+        $"Assert.{{{{test.expected ? \"True\" : \"False\"}}}}({TestedMethodCall(testCase)});";
+    
+    private static string AssertEqual(JsonNode testCase) =>
+        $"Assert.Equal({Expected(testCase)}, {TestedMethodCall(testCase)}));";
+    
+    private static string AssertThrows(JsonNode testCase) =>
+        $"Assert.Throws<ArgumentException>(() => {TestedMethodCall(testCase)}));";
+
+    private static bool ExpectsError(this JsonNode testCase) =>
+        testCase["expected"] is JsonObject jsonObject && jsonObject.ContainsKey("error");
+
+    private const string GeneratorTemplate = @"
+{{for namespace in namespaces}}
+using {{namespace}};
+{{end}}
+public class {%{{{testClass}}}%}
+{
+    {%{{{for test in tests}}}%}
+    [Fact{%{{{if !for.first}}}%}(Skip = ""Remove this Skip property to run this test""){%{{{end}}}%}]
+    public void {%{{{test.testMethod}}}%}()
+    {
+        {{-if error}}
+        {%{{{if test.expected.error}}}%}
+        {{throwsAssertion}}
+        {%{{{else}}}%}
+        {{assertion}}
+        {%{{{end}}}%}
+        {{else}}
+        {{assertion}}
+        {{-end}}
+    }
+}";
 }
