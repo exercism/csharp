5c257fcb9c503e6373f749eef17c03ea8ac020ce
Erik Schierboom <erik_schierboom@hotmail.com>
2025-02-04 11:41:40 +0100

    secret-handshake: add generator

--- exercises/practice/secret-handshake/SecretHandshakeTests.cs
+++ exercises/practice/secret-handshake/SecretHandshakeTests.cs
@@ -5,66 +5,77 @@ public class SecretHandshakeTests
     [Fact]
     public void Wink_for_1()
     {
-        Assert.Equal(new[] { "wink" }, SecretHandshake.Commands(1));
+        string[] expected = ["wink"];
+        Assert.Equal(expected, SecretHandshake.Commands(1));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Double_blink_for_10()
     {
-        Assert.Equal(new[] { "double blink" }, SecretHandshake.Commands(2));
+        string[] expected = ["double blink"];
+        Assert.Equal(expected, SecretHandshake.Commands(2));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Close_your_eyes_for_100()
     {
-        Assert.Equal(new[] { "close your eyes" }, SecretHandshake.Commands(4));
+        string[] expected = ["close your eyes"];
+        Assert.Equal(expected, SecretHandshake.Commands(4));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Jump_for_1000()
     {
-        Assert.Equal(new[] { "jump" }, SecretHandshake.Commands(8));
+        string[] expected = ["jump"];
+        Assert.Equal(expected, SecretHandshake.Commands(8));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Combine_two_actions()
     {
-        Assert.Equal(new[] { "wink", "double blink" }, SecretHandshake.Commands(3));
+        string[] expected = ["wink", "double blink"];
+        Assert.Equal(expected, SecretHandshake.Commands(3));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Reverse_two_actions()
     {
-        Assert.Equal(new[] { "double blink", "wink" }, SecretHandshake.Commands(19));
+        string[] expected = ["double blink", "wink"];
+        Assert.Equal(expected, SecretHandshake.Commands(19));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Reversing_one_action_gives_the_same_action()
     {
-        Assert.Equal(new[] { "jump" }, SecretHandshake.Commands(24));
+        string[] expected = ["jump"];
+        Assert.Equal(expected, SecretHandshake.Commands(24));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Reversing_no_actions_still_gives_no_actions()
     {
-        Assert.Empty(SecretHandshake.Commands(16));
+        string[] expected = [];
+        Assert.Equal(expected, SecretHandshake.Commands(16));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void All_possible_actions()
     {
-        Assert.Equal(new[] { "wink", "double blink", "close your eyes", "jump" }, SecretHandshake.Commands(15));
+        string[] expected = ["wink", "double blink", "close your eyes", "jump"];
+        Assert.Equal(expected, SecretHandshake.Commands(15));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Reverse_all_possible_actions()
     {
-        Assert.Equal(new[] { "jump", "close your eyes", "double blink", "wink" }, SecretHandshake.Commands(31));
+        string[] expected = ["jump", "close your eyes", "double blink", "wink"];
+        Assert.Equal(expected, SecretHandshake.Commands(31));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Do_nothing_for_zero()
     {
-        Assert.Empty(SecretHandshake.Commands(0));
+        string[] expected = [];
+        Assert.Equal(expected, SecretHandshake.Commands(0));
     }
 }
