28372e0cebbb2e651300e4c7c64d8ed3600e010c
Erik Schierboom <erik_schierboom@hotmail.com>
2025-02-05 20:03:53 +0100

    beer-song: add generator

--- exercises/practice/beer-song/BeerSongTests.cs
+++ exercises/practice/beer-song/BeerSongTests.cs
@@ -3,54 +3,54 @@ using Xunit;
 public class BeerSongTests
 {
     [Fact]
-    public void First_generic_verse()
+    public void Verse_single_verse_first_generic_verse()
     {
-        var expected = 
+        var expected =
             "99 bottles of beer on the wall, 99 bottles of beer.\n" +
             "Take one down and pass it around, 98 bottles of beer on the wall.";
         Assert.Equal(expected, BeerSong.Recite(99, 1));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Last_generic_verse()
+    public void Verse_single_verse_last_generic_verse()
     {
-        var expected = 
+        var expected =
             "3 bottles of beer on the wall, 3 bottles of beer.\n" +
             "Take one down and pass it around, 2 bottles of beer on the wall.";
         Assert.Equal(expected, BeerSong.Recite(3, 1));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Verse_with_2_bottles()
+    public void Verse_single_verse_verse_with_2_bottles()
     {
-        var expected = 
+        var expected =
             "2 bottles of beer on the wall, 2 bottles of beer.\n" +
             "Take one down and pass it around, 1 bottle of beer on the wall.";
         Assert.Equal(expected, BeerSong.Recite(2, 1));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Verse_with_1_bottle()
+    public void Verse_single_verse_verse_with_1_bottle()
     {
-        var expected = 
+        var expected =
             "1 bottle of beer on the wall, 1 bottle of beer.\n" +
             "Take it down and pass it around, no more bottles of beer on the wall.";
         Assert.Equal(expected, BeerSong.Recite(1, 1));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Verse_with_0_bottles()
+    public void Verse_single_verse_verse_with_0_bottles()
     {
-        var expected = 
+        var expected =
             "No more bottles of beer on the wall, no more bottles of beer.\n" +
             "Go to the store and buy some more, 99 bottles of beer on the wall.";
         Assert.Equal(expected, BeerSong.Recite(0, 1));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
-    public void First_two_verses()
+    public void Lyrics_multiple_verses_first_two_verses()
     {
-        var expected = 
+        var expected =
             "99 bottles of beer on the wall, 99 bottles of beer.\n" +
             "Take one down and pass it around, 98 bottles of beer on the wall.\n" +
             "\n" +
@@ -60,9 +60,9 @@ public class BeerSongTests
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Last_three_verses()
+    public void Lyrics_multiple_verses_last_three_verses()
     {
-        var expected = 
+        var expected =
             "2 bottles of beer on the wall, 2 bottles of beer.\n" +
             "Take one down and pass it around, 1 bottle of beer on the wall.\n" +
             "\n" +
@@ -75,9 +75,9 @@ public class BeerSongTests
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
-    public void All_verses()
+    public void Lyrics_multiple_verses_all_verses()
     {
-        var expected = 
+        var expected =
             "99 bottles of beer on the wall, 99 bottles of beer.\n" +
             "Take one down and pass it around, 98 bottles of beer on the wall.\n" +
             "\n" +
