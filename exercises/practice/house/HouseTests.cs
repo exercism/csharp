6b5fbd3ec4a3214d567b9ea7ee0cd224f06a24f2
Erik Schierboom <erik_schierboom@hotmail.com>
2025-02-05 11:20:19 +0100

    Add songs

--- exercises/practice/house/HouseTests.cs
+++ exercises/practice/house/HouseTests.cs
@@ -89,8 +89,7 @@ public class HouseTests
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Multiple_verses()
     {
-        var expected = 
-            "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n" +
+        var expected = "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n" +
             "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n" +
             "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n" +
             "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n" +
@@ -101,8 +100,7 @@ public class HouseTests
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Full_rhyme()
     {
-        var expected = 
-            "This is the house that Jack built.\n" +
+        var expected = "This is the house that Jack built.\n" +
             "This is the malt that lay in the house that Jack built.\n" +
             "This is the rat that ate the malt that lay in the house that Jack built.\n" +
             "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n" +
