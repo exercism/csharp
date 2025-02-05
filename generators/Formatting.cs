9b553510e241a50931179258a4326d68bb9d8075
Erik Schierboom <erik_schierboom@hotmail.com>
2025-02-05 08:42:44 +0100

    Simplify formatting (#2367)

--- generators/Formatting.cs
+++ generators/Formatting.cs
@@ -1,22 +1,16 @@
-using System.Globalization;
-
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.Formatting;
 
 namespace Generators;
 
 internal static class Formatting
 {
-    private static readonly AdhocWorkspace AdhocWorkspace = new();
-
-    internal static string FormatCode(string code) =>
-        Formatter.Format(Parse(code).NormalizeWhitespace(), AdhocWorkspace)
-            .ToFullString() + Environment.NewLine;
+    internal static string FormatCode(string code) => 
+        Parse(code).NormalizeWhitespace().AddTrailingNewLine().ToFullString();
 
     private static SyntaxNode Parse(string code) =>
         CSharpSyntaxTree.ParseText(code).GetRoot();
 
-    internal static string FormatLiteral(object obj) =>
-        SymbolDisplay.FormatLiteral(Convert.ToString(obj, CultureInfo.InvariantCulture)!, obj is string);
+    private static SyntaxNode AddTrailingNewLine(this SyntaxNode node) =>
+        node.WithTrailingTrivia(SyntaxFactory.ElasticEndOfLine(Environment.NewLine));
 }
