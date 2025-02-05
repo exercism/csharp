6abdfe5b645619fe6b557adc820f2db1f51ccbf2
Erik Schierboom <erik_schierboom@hotmail.com>
2025-02-05 15:38:07 +0100

    queen-attack: add generator

--- exercises/practice/queen-attack/QueenAttackTests.cs
+++ exercises/practice/queen-attack/QueenAttackTests.cs
@@ -6,7 +6,7 @@ public class QueenAttackTests
     [Fact]
     public void Queen_with_a_valid_position()
     {
-        var actual = QueenAttack.Create(2, 2);
+        var queen = QueenAttack.Create(2, 2);
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
@@ -36,64 +36,64 @@ public class QueenAttackTests
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Cannot_attack()
     {
-        var whiteQueen = QueenAttack.Create(2,4);
-        var blackQueen = QueenAttack.Create(6,6);
+        var whiteQueen = QueenAttack.Create(2, 4);
+        var blackQueen = QueenAttack.Create(6, 6);
         Assert.False(QueenAttack.CanAttack(whiteQueen, blackQueen));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Can_attack_on_same_row()
     {
-        var whiteQueen = QueenAttack.Create(2,4);
-        var blackQueen = QueenAttack.Create(2,6);
+        var whiteQueen = QueenAttack.Create(2, 4);
+        var blackQueen = QueenAttack.Create(2, 6);
         Assert.True(QueenAttack.CanAttack(whiteQueen, blackQueen));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Can_attack_on_same_column()
     {
-        var whiteQueen = QueenAttack.Create(4,5);
-        var blackQueen = QueenAttack.Create(2,5);
+        var whiteQueen = QueenAttack.Create(4, 5);
+        var blackQueen = QueenAttack.Create(2, 5);
         Assert.True(QueenAttack.CanAttack(whiteQueen, blackQueen));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Can_attack_on_first_diagonal()
     {
-        var whiteQueen = QueenAttack.Create(2,2);
-        var blackQueen = QueenAttack.Create(0,4);
+        var whiteQueen = QueenAttack.Create(2, 2);
+        var blackQueen = QueenAttack.Create(0, 4);
         Assert.True(QueenAttack.CanAttack(whiteQueen, blackQueen));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Can_attack_on_second_diagonal()
     {
-        var whiteQueen = QueenAttack.Create(2,2);
-        var blackQueen = QueenAttack.Create(3,1);
+        var whiteQueen = QueenAttack.Create(2, 2);
+        var blackQueen = QueenAttack.Create(3, 1);
         Assert.True(QueenAttack.CanAttack(whiteQueen, blackQueen));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Can_attack_on_third_diagonal()
     {
-        var whiteQueen = QueenAttack.Create(2,2);
-        var blackQueen = QueenAttack.Create(1,1);
+        var whiteQueen = QueenAttack.Create(2, 2);
+        var blackQueen = QueenAttack.Create(1, 1);
         Assert.True(QueenAttack.CanAttack(whiteQueen, blackQueen));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Can_attack_on_fourth_diagonal()
     {
-        var whiteQueen = QueenAttack.Create(1,7);
-        var blackQueen = QueenAttack.Create(0,6);
+        var whiteQueen = QueenAttack.Create(1, 7);
+        var blackQueen = QueenAttack.Create(0, 6);
         Assert.True(QueenAttack.CanAttack(whiteQueen, blackQueen));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Cannot_attack_if_falling_diagonals_are_only_the_same_when_reflected_across_the_longest_falling_diagonal()
     {
-        var whiteQueen = QueenAttack.Create(4,1);
-        var blackQueen = QueenAttack.Create(2,5);
+        var whiteQueen = QueenAttack.Create(4, 1);
+        var blackQueen = QueenAttack.Create(2, 5);
         Assert.False(QueenAttack.CanAttack(whiteQueen, blackQueen));
     }
 }
