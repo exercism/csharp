318b914bba7906ff173267526e93bd78ec2bc60a
Erik Schierboom <erik_schierboom@hotmail.com>
2025-02-06 13:42:55 +0100

    kindergarten-garden: add generator

---
+++ exercises/practice/kindergarten-garden/.meta/Generator.tpl
@@ -0,0 +1,18 @@
+using Xunit;
+
+public class {{ testClass }}
+{
+    {{- for test in tests }}
+    [Fact{{ if !for.first }}(Skip = "Remove this Skip property to run this test"){{ end }}]
+    public void {{ test.testMethod }}()
+    {
+        var sut = new {{ testedClass }}({{ test.input.diagram | string.literal }});
+        Plant[] expected = [
+        {{- for expected in test.expected -}}
+            {{ expected | enum "Plant" }}{{- if !for.last }},{{ end -}}
+        {{- end -}}
+        ];
+        Assert.Equal(expected, sut.{{ test.testedMethod }}({{ test.input.student | string.literal }}));
+    }
+    {{ end -}}
+}
--- exercises/practice/kindergarten-garden/KindergartenGardenTests.cs
+++ exercises/practice/kindergarten-garden/KindergartenGardenTests.cs
@@ -6,118 +6,135 @@ public class KindergartenGardenTests
     public void Partial_garden_garden_with_single_student()
     {
         var sut = new KindergartenGarden("RC\nGG");
-        Assert.Equal(new[] { Plant.Radishes, Plant.Clover, Plant.Grass, Plant.Grass }, sut.Plants("Alice"));
+        Plant[] expected = [Plant.Radishes, Plant.Clover, Plant.Grass, Plant.Grass];
+        Assert.Equal(expected, sut.Plants("Alice"));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Partial_garden_different_garden_with_single_student()
     {
         var sut = new KindergartenGarden("VC\nRC");
-        Assert.Equal(new[] { Plant.Violets, Plant.Clover, Plant.Radishes, Plant.Clover }, sut.Plants("Alice"));
+        Plant[] expected = [Plant.Violets, Plant.Clover, Plant.Radishes, Plant.Clover];
+        Assert.Equal(expected, sut.Plants("Alice"));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Partial_garden_garden_with_two_students()
     {
         var sut = new KindergartenGarden("VVCG\nVVRC");
-        Assert.Equal(new[] { Plant.Clover, Plant.Grass, Plant.Radishes, Plant.Clover }, sut.Plants("Bob"));
+        Plant[] expected = [Plant.Clover, Plant.Grass, Plant.Radishes, Plant.Clover];
+        Assert.Equal(expected, sut.Plants("Bob"));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Partial_garden_multiple_students_for_the_same_garden_with_three_students_second_students_garden()
+    public void Partial_garden_multiple_students_for_the_same_garden_with_three_students_second_student_s_garden()
     {
         var sut = new KindergartenGarden("VVCCGG\nVVCCGG");
-        Assert.Equal(new[] { Plant.Clover, Plant.Clover, Plant.Clover, Plant.Clover }, sut.Plants("Bob"));
+        Plant[] expected = [Plant.Clover, Plant.Clover, Plant.Clover, Plant.Clover];
+        Assert.Equal(expected, sut.Plants("Bob"));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Partial_garden_multiple_students_for_the_same_garden_with_three_students_third_students_garden()
+    public void Partial_garden_multiple_students_for_the_same_garden_with_three_students_third_student_s_garden()
     {
         var sut = new KindergartenGarden("VVCCGG\nVVCCGG");
-        Assert.Equal(new[] { Plant.Grass, Plant.Grass, Plant.Grass, Plant.Grass }, sut.Plants("Charlie"));
+        Plant[] expected = [Plant.Grass, Plant.Grass, Plant.Grass, Plant.Grass];
+        Assert.Equal(expected, sut.Plants("Charlie"));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Full_garden_for_alice_first_students_garden()
+    public void Full_garden_for_alice_first_student_s_garden()
     {
         var sut = new KindergartenGarden("VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV");
-        Assert.Equal(new[] { Plant.Violets, Plant.Radishes, Plant.Violets, Plant.Radishes }, sut.Plants("Alice"));
+        Plant[] expected = [Plant.Violets, Plant.Radishes, Plant.Violets, Plant.Radishes];
+        Assert.Equal(expected, sut.Plants("Alice"));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Full_garden_for_bob_second_students_garden()
+    public void Full_garden_for_bob_second_student_s_garden()
     {
         var sut = new KindergartenGarden("VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV");
-        Assert.Equal(new[] { Plant.Clover, Plant.Grass, Plant.Clover, Plant.Clover }, sut.Plants("Bob"));
+        Plant[] expected = [Plant.Clover, Plant.Grass, Plant.Clover, Plant.Clover];
+        Assert.Equal(expected, sut.Plants("Bob"));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Full_garden_for_charlie()
     {
         var sut = new KindergartenGarden("VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV");
-        Assert.Equal(new[] { Plant.Violets, Plant.Violets, Plant.Clover, Plant.Grass }, sut.Plants("Charlie"));
+        Plant[] expected = [Plant.Violets, Plant.Violets, Plant.Clover, Plant.Grass];
+        Assert.Equal(expected, sut.Plants("Charlie"));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Full_garden_for_david()
     {
         var sut = new KindergartenGarden("VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV");
-        Assert.Equal(new[] { Plant.Radishes, Plant.Violets, Plant.Clover, Plant.Radishes }, sut.Plants("David"));
+        Plant[] expected = [Plant.Radishes, Plant.Violets, Plant.Clover, Plant.Radishes];
+        Assert.Equal(expected, sut.Plants("David"));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Full_garden_for_eve()
     {
         var sut = new KindergartenGarden("VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV");
-        Assert.Equal(new[] { Plant.Clover, Plant.Grass, Plant.Radishes, Plant.Grass }, sut.Plants("Eve"));
+        Plant[] expected = [Plant.Clover, Plant.Grass, Plant.Radishes, Plant.Grass];
+        Assert.Equal(expected, sut.Plants("Eve"));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Full_garden_for_fred()
     {
         var sut = new KindergartenGarden("VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV");
-        Assert.Equal(new[] { Plant.Grass, Plant.Clover, Plant.Violets, Plant.Clover }, sut.Plants("Fred"));
+        Plant[] expected = [Plant.Grass, Plant.Clover, Plant.Violets, Plant.Clover];
+        Assert.Equal(expected, sut.Plants("Fred"));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Full_garden_for_ginny()
     {
         var sut = new KindergartenGarden("VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV");
-        Assert.Equal(new[] { Plant.Clover, Plant.Grass, Plant.Grass, Plant.Clover }, sut.Plants("Ginny"));
+        Plant[] expected = [Plant.Clover, Plant.Grass, Plant.Grass, Plant.Clover];
+        Assert.Equal(expected, sut.Plants("Ginny"));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Full_garden_for_harriet()
     {
         var sut = new KindergartenGarden("VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV");
-        Assert.Equal(new[] { Plant.Violets, Plant.Radishes, Plant.Radishes, Plant.Violets }, sut.Plants("Harriet"));
+        Plant[] expected = [Plant.Violets, Plant.Radishes, Plant.Radishes, Plant.Violets];
+        Assert.Equal(expected, sut.Plants("Harriet"));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Full_garden_for_ileana()
     {
         var sut = new KindergartenGarden("VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV");
-        Assert.Equal(new[] { Plant.Grass, Plant.Clover, Plant.Violets, Plant.Clover }, sut.Plants("Ileana"));
+        Plant[] expected = [Plant.Grass, Plant.Clover, Plant.Violets, Plant.Clover];
+        Assert.Equal(expected, sut.Plants("Ileana"));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
     public void Full_garden_for_joseph()
     {
         var sut = new KindergartenGarden("VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV");
-        Assert.Equal(new[] { Plant.Violets, Plant.Clover, Plant.Violets, Plant.Grass }, sut.Plants("Joseph"));
+        Plant[] expected = [Plant.Violets, Plant.Clover, Plant.Violets, Plant.Grass];
+        Assert.Equal(expected, sut.Plants("Joseph"));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Full_garden_for_kincaid_second_to_last_students_garden()
+    public void Full_garden_for_kincaid_second_to_last_student_s_garden()
     {
         var sut = new KindergartenGarden("VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV");
-        Assert.Equal(new[] { Plant.Grass, Plant.Clover, Plant.Clover, Plant.Grass }, sut.Plants("Kincaid"));
+        Plant[] expected = [Plant.Grass, Plant.Clover, Plant.Clover, Plant.Grass];
+        Assert.Equal(expected, sut.Plants("Kincaid"));
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Full_garden_for_larry_last_students_garden()
+    public void Full_garden_for_larry_last_student_s_garden()
     {
         var sut = new KindergartenGarden("VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV");
-        Assert.Equal(new[] { Plant.Grass, Plant.Violets, Plant.Clover, Plant.Violets }, sut.Plants("Larry"));
+        Plant[] expected = [Plant.Grass, Plant.Violets, Plant.Clover, Plant.Violets];
+        Assert.Equal(expected, sut.Plants("Larry"));
     }
 }
