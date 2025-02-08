4a4f7b02385bd552caf11f9a0de0a2465685eeb6
Erik Schierboom <erik_schierboom@hotmail.com>
2025-02-08 14:14:23 +0100

    Remove deprecated generator (#2380)

--- generators.deprecated/Helpers/EnumerableExtensions.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Exercism.CSharp.Helpers;
-
-internal static class EnumerableExtensions
-{
-    public static SortedSet<TSource> ToSortedSet<TSource>(this IEnumerable<TSource> source) => new(source);
-}
