fcc0bf043163fea49574aad23993b33522b1c418
Erik Schierboom <erik_schierboom@hotmail.com>
2025-02-18 15:51:27 +0100

    hangman: fix

--- exercises/practice/hangman/HangmanTests.cs
+++ exercises/practice/hangman/HangmanTests.cs
@@ -49,8 +49,10 @@ public class HangmanTests : ReactiveTest
     public void Guess_changes_state()
     {
         var hangman = new Hangman("foo");
-        HangmanState actual = null;
+        HangmanState? actual = null;
         hangman.StateObservable.Subscribe(x => actual = x);
+        
+        Assert.NotNull(actual);
         var initial = actual;
 
         // +--x->
@@ -64,8 +66,10 @@ public class HangmanTests : ReactiveTest
     public void Wrong_guess_decrements_remaining_guesses()
     {
         var hangman = new Hangman("foo");
-        HangmanState actual = null;
+        HangmanState? actual = null;
         hangman.StateObservable.Subscribe(x => actual = x);
+        
+        Assert.NotNull(actual);
         var initial = actual;
 
         // +--x->
