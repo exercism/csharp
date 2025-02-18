d5c04db520becf4f0529685cdc6b31b4e59b3e23
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2025-02-17 17:48:42 -0800

    Add `bottle-song`

---
+++ exercises/practice/bottle-song/BottleSong.cs
@@ -0,0 +1,9 @@
+using System;
+
+public static class BottleSong
+{
+    public static string Recite(int startBottles, int takeDown)
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+}
