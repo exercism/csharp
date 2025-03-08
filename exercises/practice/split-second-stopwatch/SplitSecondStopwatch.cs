1fa9d9924814574d1b54e420bdd0e0db26cdcea9
Erik Schierboom <erik_schierboom@hotmail.com>
2025-03-08 18:56:13 +0100

    Add `last-lap` exercise

--- exercises/practice/split-second-stopwatch/SplitSecondStopwatch.cs
+++ exercises/practice/split-second-stopwatch/SplitSecondStopwatch.cs
@@ -1,32 +1,59 @@
 public enum StopwatchState
 {
     Ready,
-    Running,
-    Paused,
-    Stopped
+    Running
 }
 
 public class SplitSecondStopwatch(TimeProvider time)
 {
-    public StopwatchState State { get; private set; }
+    private readonly List<TimeSpan> _splits = new();
+    private DateTimeOffset? _splitStart;
+    
+    private TimeSpan PreviousSplits => _splits.Aggregate(TimeSpan.Zero, (total, split) => total + split);
+    private TimeSpan CurrentSplit => _splitStart is {} start ? time.GetUtcNow() - start : TimeSpan.Zero;
+
+    public StopwatchState State { get; private set; } = StopwatchState.Ready;
+    public List<TimeSpan> PreviousLaps { get; } = new();
+
+    public TimeSpan CurrentLap => CurrentSplit + PreviousSplits;
+    public TimeSpan Total => CurrentLap + PreviousLaps.Aggregate(TimeSpan.Zero, (total, split) => total + split);
 
     public void Start()
     {
-        throw new NotImplementedException();
+        if (State != StopwatchState.Ready)
+            throw new InvalidOperationException("Can't start a stopwatch that is not stopped.");
+        
+        _splitStart = time.GetUtcNow();
+        State = StopwatchState.Running;
     }
 
     public void Stop()
     {
-        throw new NotImplementedException();
-    }
+        if (State != StopwatchState.Running)
+            throw new InvalidOperationException("Can't stop a stopwatch that is not started.");
 
-    public void Split()
-    {
-        throw new NotImplementedException();
+        _splits.Add(CurrentSplit);
+        _splitStart = null;
+        State = StopwatchState.Ready;
     }
-
-    public void Pause()
+    
+    public void Reset()
     {
-        throw new NotImplementedException();
+        if (State != StopwatchState.Ready)
+            throw new InvalidOperationException("Can't reset a stopwatch that is not stopped.");
+        
+        _splits.Clear();
+        _splitStart = null;
+        State = StopwatchState.Ready;
+    }
+    
+    public void Lap()
+    {
+        if (State != StopwatchState.Running)
+            throw new InvalidOperationException("Can't lap a stopwatch that is not started stopped.");
+        
+        PreviousLaps.Add(CurrentLap);
+        _splits.Clear();
+        _splitStart = time.GetUtcNow();
     }
 }
--- exercises/practice/split-second-stopwatch/SplitSecondStopwatchTests.cs
+++ exercises/practice/split-second-stopwatch/SplitSecondStopwatchTests.cs
@@ -3,10 +3,31 @@ using Microsoft.Extensions.Time.Testing;
 public class SplitSecondStopwatchTests
 {
     [Fact]
-    public void InitalStateIsReady()       
+    public void StateIsInitiallyReady()       
     {
         var stopwatch = new SplitSecondStopwatch(new FakeTimeProvider());
         Assert.Equal(StopwatchState.Ready, stopwatch.State);
+    }
+    
+    [Fact]
+    public void PreviousLapsIsInitiallyEmpty()       
+    {
+        var stopwatch = new SplitSecondStopwatch(new FakeTimeProvider());
+        Assert.Empty(stopwatch.PreviousLaps);
+    }
+
+    [Fact]
+    public void CurrentLapIsInitiallyZero()       
+    {
+        var stopwatch = new SplitSecondStopwatch(new FakeTimeProvider());
+        Assert.Equal(TimeSpan.Zero, stopwatch.CurrentLap);
+    }
+
+    [Fact]
+    public void TotalIsInitiallyZero()       
+    {
+        var stopwatch = new SplitSecondStopwatch(new FakeTimeProvider());
+        Assert.Equal(TimeSpan.Zero, stopwatch.Total);
     }
 
     [Fact]
@@ -16,40 +37,204 @@ public class SplitSecondStopwatchTests
         stopwatch.Start();
         Assert.Equal(StopwatchState.Running, stopwatch.State);
     }
-
+    
     [Fact]
-    public void PauseChangesStateToPaused()       
+    public void StartDoesNotChangeLaps()       
     {
         var stopwatch = new SplitSecondStopwatch(new FakeTimeProvider());
         stopwatch.Start();
-        stopwatch.Pause();
-        Assert.Equal(StopwatchState.Paused, stopwatch.State);
+        Assert.Empty(stopwatch.PreviousLaps);
     }
-
+    
     [Fact]
-    public void SplitKeepsStateAsRunning()
+    public void StartStartsTimeTrackingInCurrentLap()       
+    {
+        var timeProvider = new FakeTimeProvider();
+        var stopwatch = new SplitSecondStopwatch(timeProvider);
+        stopwatch.Start();
+    
+        var elapsed = TimeSpan.FromSeconds(55.5);
+        timeProvider.Advance(elapsed);
+        
+        Assert.Equal(elapsed, stopwatch.CurrentLap);
+    }
+    
+    [Fact]
+    public void StartStartsTimeTrackingInTotal()       
+    {
+        var timeProvider = new FakeTimeProvider();
+        var stopwatch = new SplitSecondStopwatch(timeProvider);
+        stopwatch.Start();
+    
+        var elapsed = TimeSpan.FromSeconds(55.5);
+        timeProvider.Advance(elapsed);
+        
+        Assert.Equal(elapsed, stopwatch.CurrentLap);
+    }
+    
+    [Fact]
+    public void CannotStartFromRunningState()       
     {
         var stopwatch = new SplitSecondStopwatch(new FakeTimeProvider());
         stopwatch.Start();
-        stopwatch.Split();
-        Assert.Equal(StopwatchState.Running, stopwatch.State);
+        
+        Assert.Throws<InvalidOperationException>(() => stopwatch.Start());
     }
-
+    
     [Fact]
-    public void StopChangesStateToStop()       
+    public void StopFromRunningStateChangesStateToReady()       
+    {
+        var stopwatch = new SplitSecondStopwatch(new FakeTimeProvider());
+        stopwatch.Start();
+    
+        stopwatch.Stop();
+    
+        Assert.Equal(StopwatchState.Ready, stopwatch.State);
+    }
+    
+    [Fact]
+    public void StopDoesNotChangeLaps()       
+    {
+        var stopwatch = new SplitSecondStopwatch(new FakeTimeProvider());
+        stopwatch.Start();
+        
+        stopwatch.Stop();
+        
+        Assert.Empty(stopwatch.PreviousLaps);
+    }
+    
+    [Fact]
+    public void StopStopsTimeTrackingInCurrentLap()       
+    {
+        var timeProvider = new FakeTimeProvider();
+        var stopwatch = new SplitSecondStopwatch(timeProvider);
+        stopwatch.Start();
+    
+        var elapsed1 = TimeSpan.FromSeconds(22.5);
+        timeProvider.Advance(elapsed1);
+        
+        stopwatch.Stop();
+        
+        var elapsed2 = TimeSpan.FromSeconds(17.9);
+        timeProvider.Advance(elapsed2);
+        
+        Assert.Equal(elapsed1, stopwatch.CurrentLap);
+    }
+    
+    [Fact]
+    public void StopStopsTimeTrackingInTotal()       
+    {
+        var timeProvider = new FakeTimeProvider();
+        var stopwatch = new SplitSecondStopwatch(timeProvider);
+        stopwatch.Start();
+    
+        var elapsed1 = TimeSpan.FromSeconds(22.5);
+        timeProvider.Advance(elapsed1);
+        
+        stopwatch.Stop();
+        
+        var elapsed2 = TimeSpan.FromSeconds(17.9);
+        timeProvider.Advance(elapsed2);
+        
+        Assert.Equal(elapsed1, stopwatch.Total);
+    }
+    
+    [Fact]
+    public void CannotStopFromReadyState()       
+    {
+        var stopwatch = new SplitSecondStopwatch(new FakeTimeProvider());
+        
+        Assert.Throws<InvalidOperationException>(() => stopwatch.Stop());
+    }
+    
+    [Fact]
+    public void ResetClearsPreviousLaps()       
     {
         var stopwatch = new SplitSecondStopwatch(new FakeTimeProvider());
         stopwatch.Start();
         stopwatch.Stop();
-        Assert.Equal(StopwatchState.Stopped, stopwatch.State);
+        
+        stopwatch.Reset();
+        
+        Assert.Empty(stopwatch.PreviousLaps);
     }
-
+    
     [Fact]
-    public void StopAddsLapTime()       
+    public void ResetSetsCurrentLapToZero()       
     {
         var stopwatch = new SplitSecondStopwatch(new FakeTimeProvider());
         stopwatch.Start();
         stopwatch.Stop();
-        Assert.Equal(StopwatchState.Stopped, stopwatch.State);
+        
+        stopwatch.Reset();
+        
+        Assert.Equal(TimeSpan.Zero, stopwatch.CurrentLap);
+    }
+    
+    [Fact]
+    public void ResetSetsTotalToZero()       
+    {
+        var stopwatch = new SplitSecondStopwatch(new FakeTimeProvider());
+        stopwatch.Start();
+        stopwatch.Stop();
+        
+        stopwatch.Reset();
+        
+        Assert.Equal(TimeSpan.Zero, stopwatch.Total);
+    }
+    
+    [Fact]
+    public void LapAddsCurrentLapToPreviousLaps()       
+    {
+        var timeProvider = new FakeTimeProvider();
+        var stopwatch = new SplitSecondStopwatch(timeProvider);
+        stopwatch.Start();
+    
+        var elapsed1 = TimeSpan.FromSeconds(22.5);
+        timeProvider.Advance(elapsed1);
+        
+        stopwatch.Lap();
+        
+        var elapsed2 = TimeSpan.FromSeconds(17.9);
+        timeProvider.Advance(elapsed2);
+
+        var lap = Assert.Single(stopwatch.PreviousLaps);
+        Assert.Equal(elapsed1, lap);
+    }
+    
+    [Fact]
+    public void LapReSetsCurrentLapStartTimeToCurrentTime()       
+    {
+        var timeProvider = new FakeTimeProvider();
+        var stopwatch = new SplitSecondStopwatch(timeProvider);
+        stopwatch.Start();
+    
+        var elapsed1 = TimeSpan.FromSeconds(22.5);
+        timeProvider.Advance(elapsed1);
+        
+        stopwatch.Lap();
+        
+        var elapsed2 = TimeSpan.FromSeconds(17.9);
+        timeProvider.Advance(elapsed2);
+        
+        Assert.Equal(elapsed2, stopwatch.CurrentLap);
+    }
+    
+    [Fact]
+    public void LapDoesNotStopTimeTrackingInTotal()       
+    {
+        var timeProvider = new FakeTimeProvider();
+        var stopwatch = new SplitSecondStopwatch(timeProvider);
+        stopwatch.Start();
+    
+        var elapsed1 = TimeSpan.FromSeconds(22.5);
+        timeProvider.Advance(elapsed1);
+        
+        stopwatch.Lap();
+        
+        var elapsed2 = TimeSpan.FromSeconds(17.9);
+        timeProvider.Advance(elapsed2);
+        
+        Assert.Equal(elapsed1 + elapsed2, stopwatch.Total);
     }
 }
