9e7b3196ff3a3df5aef484898ffeb837988dbeed
Karan Chadha <72665232+KaranChadha10@users.noreply.github.com>
2025-10-05 21:42:20 +0530

    Improved test for roll_the_die (#2442)

--- exercises/concept/roll-the-die/RollTheDieTests.cs
+++ exercises/concept/roll-the-die/RollTheDieTests.cs
@@ -1,4 +1,5 @@
 using Exercism.Tests;
+using System.Collections.Generic;
 
 public class RollTheDieTests
 {
@@ -6,19 +7,33 @@ public class RollTheDieTests
     [Task(1)]
     public void RollDie()
     {
+        var rollCount = 1000;
+        var rolls = new HashSet<int>(rollCount);
         var player = new Player();
-        for (var i = 0; i < 100; i++)
+        for (var i = 0; i < rollCount; i++)
         {
+            var roll = player.RollDie();
+            rolls.Add(roll);
             Assert.InRange(player.RollDie(), 1, 18);
         }
+        Assert.Equal(18, rolls.Count);
     }
 
     [Fact]
     [Task(2)]
     public void GenerateSpellStrength()
     {
+        var rollCount = 100;
+        var minUniqueValues = rollCount - 5; // Allow up to 5 duplicates
+        var rolls = new HashSet<double>(rollCount);
         var player = new Player();
-        var strength = player.GenerateSpellStrength();
-        Assert.InRange(strength, 0.0, 100.0);
+        for (var i = 0; i < rollCount; i++)
+        {
+            var strength = player.GenerateSpellStrength();
+            rolls.Add(strength);
+            Assert.InRange(strength, 0.0, 100.0);
+        }
+        
+        Assert.InRange(rolls.Count, minUniqueValues, rollCount);
     }
 }
