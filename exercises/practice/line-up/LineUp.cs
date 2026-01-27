84fc88142fc9b6b7f72cc44f31d064b074839c6c
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2026-01-26 21:44:55 -0800

    Add `line-up`

---
+++ exercises/practice/line-up/LineUp.cs
@@ -0,0 +1,7 @@
+public static class LineUp
+{
+    public static string Format(string name, int number)
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+}
