b7dfec6850edf0e81b5588bd94cf12c6b506711e
Don Bru <dbruey@comcast.net>
2026-02-14 14:03:08 -0500

    Remove unimplemented methods from CryptoSquare class
    These methods are not required for tests to pass, and are not mentioned in the requirements.

--- exercises/practice/crypto-square/CryptoSquare.cs
+++ exercises/practice/crypto-square/CryptoSquare.cs
@@ -1,20 +1,5 @@
 public static class CryptoSquare
 {
-    public static string NormalizedPlaintext(string plaintext)
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
-
-    public static IEnumerable<string> PlaintextSegments(string plaintext)
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
-
-    public static string Encoded(string plaintext)
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
-
     public static string Ciphertext(string plaintext)
     {
         throw new NotImplementedException("You need to implement this method.");
