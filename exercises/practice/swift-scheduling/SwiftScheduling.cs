3eafc338fd421b6084e07468356ea1082f66d2e8
Andrea Carpino <carpocci@gmail.com>
2026-06-20 19:34:52 +0200

    Fix `SwiftScheduling.cs` syntax error

--- exercises/practice/swift-scheduling/SwiftScheduling.cs
+++ exercises/practice/swift-scheduling/SwiftScheduling.cs
@@ -1,6 +1,6 @@
 public static class SwiftScheduling
 {
-    public static DateTime DeliveryDate(DateTime meetingStart, string description) =>
+    public static DateTime DeliveryDate(DateTime meetingStart, string description)
     {
         throw new NotImplementedException("You need to implement this method.");
     }
